b0aa50e387b824b353db1b80f8b7893187eeba43
Will Stephenson <wstephenson@kde.org>
2004-05-20 23:32:34 +0000

    Use correct licensing for kdepim + libraries.
    Also remove incorrect copyright attribution on kimiface.h.  The original proof-of-concept KIMIFace was a straight rename of KopeteIface.  This abstract interface specification comes from a redesign and shares no code with the original.
    
    svn path=/trunk/kdepim/libkdepim/; revision=313100

--- libkdepim-copy/kimproxy.cpp
+++ libkdepim-copy/kimproxy.cpp
@@ -5,14 +5,20 @@
 	
 	Copyright (c) 2004 Will Stephenson   <lists@stevello.free-online.co.uk>
 
-    *************************************************************************
-    *                                                                       *
-    * This program is free software; you can redistribute it and/or modify  *
-    * it under the terms of the GNU General Public License as published by  *
-    * the Free Software Foundation; either version 2 of the License, or     *
-    * (at your option) any later version.                                   *
-    *                                                                       *
-    *************************************************************************
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
 */
 
 #include <dcopclient.h>
@@ -54,7 +60,7 @@ bool KIMProxy::initialize()
 {
 	if ( !m_im_client_stub )
 	{
-		// So there is no error from the a failed query when using kdelibs 3.2, which don't have this servicetype
+		// So there is no error from a failed query when using kdelibs 3.2, which don't have this servicetype
 		if ( KServiceType::serviceType( IM_SERVICE_TYPE ) ) 
 		{
 			// start/find an instance of DCOP/InstantMessenger
