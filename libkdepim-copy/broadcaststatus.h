c276ebfb53213397e43d03402ee076fbdd0bbcea
Till Adam <adam@kde.org>
2004-07-30 14:10:09 +0000

    Make setStatusMsg, setTransientStatusMsg and reset slots, so they can be hooked up to statusMsg() signals which are only connected sometimes, such as for the current folder only, in KMail.
    svn path=/trunk/kdepim/libkdepim/; revision=334163

--- libkdepim-copy/broadcaststatus.h
+++ libkdepim-copy/broadcaststatus.h
@@ -35,8 +35,7 @@ public:
 
   /** Return the instance of the singleton object for this class */
   static BroadcastStatus *instance();
-  /** Emit an update status bar signal */
-  void setStatusMsg( const QString& message );
+
   /** Return the last status message from setStatusMsg() */
   QString statusMsg() const { return mStatusMsg; }
   /** Sets a status bar message with timestamp */
@@ -55,6 +54,11 @@ public:
                                           int numBytesToRead = -1,
                                           bool mLeaveOnServer = false,
                                           KPIM::ProgressItem* progressItem = 0 ); // set the same status in this progress item
+
+public slots:
+  /** Emit an update status bar signal. It's a slot so it can be hooked up
+      to other signals. */
+  void setStatusMsg( const QString& message );
 
   /**
       Set a status message that will go away again with the next call of
