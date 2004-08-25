ba58b6727f2b3761d7e7bfc3c669a0fa657203a2
Antonio Larrosa Jimenez <larrosa@kde.org>
2004-08-25 20:02:08 +0000

    Usability suggestions by Ellen
    svn path=/trunk/kdepim/libkdepim/; revision=341102

--- libkdepim-copy/kpixmapregionselectorwidget.cpp
+++ libkdepim-copy/kpixmapregionselectorwidget.cpp
@@ -25,6 +25,8 @@
 #include <kimageeffect.h>
 #include <kdebug.h>
 #include <stdlib.h>
+#include <qcursor.h>
+#include <qapplication.h>
 
 using namespace KPIM;
 
@@ -130,11 +132,19 @@ bool KPixmapRegionSelectorWidget::eventFilter(QObject *obj, QEvent *ev)
       QMouseEvent *mev= (QMouseEvent *)(ev);
       //kdDebug() << QString("click at  %1,%2").arg( mev->x() ).arg( mev->y() ) << endl;
 
+      QCursor cursor;
       if ( m_selectedRegion.contains( mev->pos() ) 
           && m_selectedRegion!=m_originalPixmap.rect() )
+      {
          m_state=Moving;
+         cursor=QCursor(Qt::SizeAllCursor);
+      }
       else
+      {
          m_state=Resizing;
+         cursor=QCursor(Qt::CrossCursor);
+      }
+      QApplication::setOverrideCursor(cursor);
 
       m_tempFirstClick=mev->pos();
 
@@ -207,6 +217,7 @@ bool KPixmapRegionSelectorWidget::eventFilter(QObject *obj, QEvent *ev)
          resetSelection();
 
       m_state=None;
+      QApplication::restoreOverrideCursor();
 
       return TRUE;
    }
