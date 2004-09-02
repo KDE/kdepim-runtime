ee534dc7ffe5e25699adfbd81784444ea3c18846
Tobias Koenig <tokoe@kde.org>
2004-09-02 22:22:47 +0000

    Added the cached resource for libkabc, it has nearly the same API like KCal::ResourceCached and can be used as base class for a resource which wants to have a local cache for free.
    svn path=/trunk/kdepim/libkdepim/; revision=343611

---
+++ libkdepim-copy/kabcresourcecached.cpp
@@ -0,0 +1,210 @@
+/*
+    This file is part of libkdepim.
+    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include <qfile.h>
+
+#include <kabc/vcardconverter.h>
+#include <kdebug.h>
+#include <klocale.h>
+#include <kstandarddirs.h>
+
+#include "kabcresourcecached.h"
+
+using namespace KABC;
+
+ResourceCached::ResourceCached( const KConfig *config )
+  : KABC::Resource( config )
+{
+}
+
+ResourceCached::~ResourceCached()
+{
+}
+
+void ResourceCached::writeConfig( KConfig *config )
+{
+  KABC::Resource::writeConfig( config );
+}
+
+void ResourceCached::insertAddressee( const Addressee &addr )
+{
+  if ( mAddrMap.find( addr.uid() ) == mAddrMap.end() ) { // new contact
+    mAddrMap.insert( addr.uid(), addr );
+    mAddedAddressees.insert( addr.uid(), addr );
+  } else {
+    KABC::Addressee oldAddressee = mAddrMap.find( addr.uid() ).data();
+    if ( oldAddressee != addr ) {
+      mAddrMap.remove( addr.uid() );
+      mAddrMap.insert( addr.uid(), addr );
+      mChangedAddressees.insert( addr.uid(), addr );
+    }
+  }
+}
+
+void ResourceCached::removeAddressee( const Addressee &addr )
+{
+  if ( mDeletedAddressees.find( addr.uid() ) == mDeletedAddressees.end() )
+    mDeletedAddressees.insert( addr.uid(), addr );
+
+  mAddrMap.remove( addr.uid() );
+}
+
+void ResourceCached::loadCache()
+{
+  mUidMap.clear();
+  mAddrMap.clear();
+
+  // load uid map
+  QFile mapFile( uidMapFile() );
+  if ( mapFile.open( IO_ReadOnly ) ) {
+    QDataStream stream( &mapFile );
+    stream >> mUidMap;
+    mapFile.close();
+  } else {
+    kdError() << "Can't open uid map file '" << mapFile.name() << "'" << endl;
+  }
+
+  // load cache
+  QFile file( cacheFile() );
+  if ( !file.open( IO_ReadOnly ) )
+    return;
+
+
+  KABC::VCardConverter converter;
+  KABC::Addressee::List list = converter.parseVCards( QString::fromUtf8( file.readAll() ) );
+  KABC::Addressee::List::Iterator it;
+
+  for ( it = list.begin(); it != list.end(); ++it ) {
+    (*it).setResource( this );
+    (*it).setChanged( false );
+    mAddrMap.insert( (*it).uid(), *it );
+  }
+
+  file.close();
+}
+
+void ResourceCached::saveCache()
+{
+  // save uid map
+  QFile mapFile( uidMapFile() );
+  if ( mapFile.open( IO_WriteOnly ) ) {
+    QDataStream stream( &mapFile );
+    stream << mUidMap;
+    mapFile.close();
+  } else {
+    kdError() << "Can't open uid map file '" << mapFile.name() << "'" << endl;
+  }
+
+  // save cache
+  QFile file( cacheFile() );
+  if ( !file.open( IO_WriteOnly ) )
+    return;
+
+  KABC::Addressee::List list = mAddrMap.values();
+
+  KABC::VCardConverter converter;
+  QString vCard = converter.createVCards( list );
+  file.writeBlock( vCard.utf8() );
+  file.close();
+}
+
+void ResourceCached::setRemoteUid( const QString &localUid, const QString &remoteUid )
+{
+  mUidMap.insert( localUid, remoteUid );
+}
+
+void ResourceCached::removeRemoteUid( const QString &remoteUid )
+{
+  QMap<QString, QVariant>::Iterator it;
+  for ( it = mUidMap.begin(); it != mUidMap.end(); ++it )
+    if ( it.data().toString() == remoteUid ) {
+      mUidMap.remove( it );
+      return;
+    }
+}
+
+QString ResourceCached::remoteUid( const QString &localUid ) const
+{
+  QMap<QString, QVariant>::ConstIterator it;
+  it = mUidMap.find( localUid );
+
+  if ( it != mUidMap.end() )
+    return it.data().toString();
+  else
+    return QString::null;
+}
+
+QString ResourceCached::localUid( const QString &remoteUid ) const
+{
+  QMap<QString, QVariant>::ConstIterator it;
+  for ( it = mUidMap.begin(); it != mUidMap.end(); ++it )
+    if ( it.data().toString() == remoteUid )
+      return it.key();
+
+  return QString::null;
+}
+
+bool ResourceCached::hasChanges() const
+{
+  return !( mAddedAddressees.isEmpty() &&
+            mChangedAddressees.isEmpty() &&
+            mDeletedAddressees.isEmpty() );
+}
+
+void ResourceCached::clearChanges()
+{
+  mAddedAddressees.clear();
+  mChangedAddressees.clear();
+  mDeletedAddressees.clear();
+}
+
+void ResourceCached::clearChange( const KABC::Addressee &addr )
+{
+  mAddedAddressees.remove( addr.uid() );
+  mChangedAddressees.remove( addr.uid() );
+  mDeletedAddressees.remove( addr.uid() );
+}
+
+KABC::Addressee::List ResourceCached::addedAddressees() const
+{
+  return mAddedAddressees.values();
+}
+
+KABC::Addressee::List ResourceCached::changedAddressees() const
+{
+  return mChangedAddressees.values();
+}
+
+KABC::Addressee::List ResourceCached::deletedAddressees() const
+{
+  return mDeletedAddressees.values();
+}
+
+QString ResourceCached::cacheFile() const
+{
+  return locateLocal( "cache", "kabc/kresources/" + identifier() );
+}
+
+QString ResourceCached::uidMapFile() const
+{
+  return locateLocal( "data", "kabc/uidmaps/" + type() + "/" + identifier() );
+}
+
+#include "kabcresourcecached.moc"
---
+++ libkdepim-copy/kabcresourcecached.h
@@ -0,0 +1,99 @@
+/*
+    This file is part of libkdepim.
+    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KABC_RESOURCECACHED_H
+#define KABC_RESOURCECACHED_H
+
+#include <kabc/resource.h>
+
+namespace KABC {
+
+class ResourceCached : public Resource
+{
+  Q_OBJECT
+
+  public:
+    ResourceCached( const KConfig* );
+    ~ResourceCached();
+
+    /**
+      Writes the resource specific config to file.
+     */
+    virtual void writeConfig( KConfig *config );
+
+    /**
+      Insert an addressee into the resource.
+     */
+    virtual void insertAddressee( const Addressee& );
+
+    /**
+      Removes an addressee from resource.
+     */
+    virtual void removeAddressee( const Addressee& addr );
+
+    void loadCache();
+    void saveCache();
+
+    /**
+      Stores the remote uid for the given local uid.
+     */
+    void setRemoteUid( const QString &localUid, const QString &remoteUid );
+
+    /**
+      Removes the remote uid.
+     */
+    void removeRemoteUid( const QString &remoteUid );
+
+    /**
+      Returns the remote uid of the given local uid.
+     */
+    QString remoteUid( const QString &localUid ) const;
+
+    /**
+      Returns the local uid for the given remote uid.
+     */
+    QString localUid( const QString &remoteUid ) const;
+
+    bool hasChanges() const;
+    void clearChanges();
+    void clearChange( const KABC::Addressee& );
+
+    KABC::Addressee::List addedAddressees() const;
+    KABC::Addressee::List changedAddressees() const;
+    KABC::Addressee::List deletedAddressees() const;
+
+  protected:
+    virtual QString cacheFile() const;
+    virtual QString uidMapFile() const;
+
+  private:
+    QMap<QString, QVariant> mUidMap;
+
+    QMap<QString, KABC::Addressee> mAddedAddressees;
+    QMap<QString, KABC::Addressee> mChangedAddressees;
+    QMap<QString, KABC::Addressee> mDeletedAddressees;
+
+    class ResourceCachedPrivate;
+    ResourceCachedPrivate *d;
+};
+
+}
+
+#endif
