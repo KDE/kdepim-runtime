18c9a12fef4b21f192cdcbf02ed3bb625ac70d2e
Reinhold Kainhofer <reinhold@kainhofer.com>
2004-10-26 15:00:25 +0000

    CVS_SILENT: Add copyright information for myself.
    svn path=/trunk/kdepim/libkdepim/; revision=357945

--- libkdepim-copy/categoryselectdialog.cpp
+++ libkdepim-copy/categoryselectdialog.cpp
@@ -2,6 +2,7 @@
     This file is part of libkdepim.
 
     Copyright (c) 2000, 2001, 2002 Cornelius Schumacher <schumacher@kde.org>
+    Copyright (C) 2003-2004 Reinhold Kainhofer <reinhold@kainhofer.com>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
