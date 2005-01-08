9f0724a5955db1c8a3d3cc72d519fa738f56cd0b
David Faure <faure@kde.org>
2005-01-08 23:58:22 +0000

    This rather simply Qt-designer plugin allows to embed any KParts in a .ui file, given a URL and (preferrably) a mimetype. Idea by Reinhold. To be moved to kdelibs for 4.0.
    svn path=/trunk/kdepim/libkdepim/; revision=376670

---
+++ libkdepim-copy/kpartsdesignerplugin/kpartsdesignerplugin.cpp
@@ -0,0 +1,108 @@
+/*
+    Copyright (C) 2004, David Faure <faure@kde.org>
+    This file is part of the KDE project
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2, as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "kpartsdesignerplugin.h"
+
+#include <kparts/componentfactory.h>
+#include <kparts/part.h>
+#include <kmimetype.h>
+#include <qlayout.h>
+#include <kapplication.h>
+
+KPartsGenericPart::KPartsGenericPart( QWidget* parentWidget, const char* name )
+    : QWidget( parentWidget, name ), m_part( 0 )
+{
+    QVBoxLayout* layout = new QVBoxLayout( this );
+    layout->setAutoAdd( true );
+}
+
+void KPartsGenericPart::load()
+{
+    if ( m_mimetype.isEmpty() || m_url.isEmpty() )
+        return; // not enough info yet
+    // Here it crashes in KSycoca::openDatabase when trying to load the stuff from designer itself
+    // Not sure why - but it's not really needed anyway.
+    if ( !kapp )
+        return;
+    QString mimetype = m_mimetype;
+    if ( mimetype == "auto" )
+        mimetype == KMimeType::findByURL( m_url )->name();
+    if ( m_part ) {
+        delete m_part;
+    }
+    // "this" is both the parent widget and the parent object
+    m_part = KParts::ComponentFactory::createPartInstanceFromQuery<KParts::ReadOnlyPart>( mimetype, QString::null, this, 0, this, 0 );
+    if ( m_part ) {
+        m_part->openURL( m_url );
+        m_part->widget()->show();
+    }
+}
+
+////
+
+static const char* mykey = "KPartsGenericPart";
+
+QStringList KPartsWidgetPlugin::keys() const {
+    return QStringList() << mykey;
+}
+
+QWidget * KPartsWidgetPlugin::create( const QString & key, QWidget * parent, const char * name ) {
+    if ( key == mykey )
+        return new KPartsGenericPart( parent, name );
+    return 0;
+}
+
+QString KPartsWidgetPlugin::group( const QString & key ) const {
+    if ( key == mykey )
+        return "Display (KDE)";
+    return QString::null;
+}
+
+#if 0
+QIconSet KPartsWidgetPlugin::iconSet( const QString & key ) const {
+  return QIconSet();
+}
+#endif
+
+QString KPartsWidgetPlugin::includeFile( const QString & key ) const {
+    if ( key == mykey )
+        return "partplugin.h";
+    return QString::null;
+}
+
+QString KPartsWidgetPlugin::toolTip( const QString & key ) const {
+    if ( key == mykey )
+        return "Generic KParts viewer";
+    return QString::null;
+}
+
+QString KPartsWidgetPlugin::whatsThis( const QString & key ) const {
+    if ( key == mykey )
+        return "A widget to embed any KParts viewer, given a url and optionally a mimetype";
+    return QString::null;
+}
+
+bool KPartsWidgetPlugin::isContainer( const QString & /*key*/ ) const {
+    return false;
+}
+
+KDE_Q_EXPORT_PLUGIN( KPartsWidgetPlugin )
+
+#include "kpartsdesignerplugin.moc"
+
---
+++ libkdepim-copy/kpartsdesignerplugin/kpartsdesignerplugin.h
@@ -0,0 +1,70 @@
+/*
+    Copyright (C) 2004, David Faure <faure@kde.org>
+    This file is part of the KDE project
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2, as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+
+#ifndef DESIGNER_PARTPLUGIN_H
+#define DESIGNER_PARTPLUGIN_H
+
+#include <qwidgetplugin.h>
+#include <qwidget.h>
+namespace KParts { class ReadOnlyPart; }
+
+/**
+ * Generic part loader, able to view any kind of file for which
+ * a KParts::ReadOnlyPart is available
+ */
+class KPartsGenericPart : public QWidget {
+    Q_OBJECT
+    Q_PROPERTY( QString url READ url WRITE setURL )
+    Q_PROPERTY( QString mimetype READ mimetype WRITE setMimetype )
+public:
+    KPartsGenericPart( QWidget* parentWidget, const char* name );
+
+    QString url() const { return m_url; }
+    void setURL( const QString& url ) { m_url = url; load(); }
+
+    // The mimetype, or "auto" if unknown
+    QString mimetype() const { return m_mimetype; }
+    void setMimetype( const QString& mimetype ) { m_mimetype = mimetype; load(); }
+
+private:
+    void load();
+
+private:
+    QString m_mimetype;
+    QString m_url;
+    KParts::ReadOnlyPart* m_part;
+};
+
+/**
+ * Qt designer plugin for embedding a KParts using KPartsGenericPart
+ */
+class KPartsWidgetPlugin : public QWidgetPlugin {
+public:
+  QStringList keys() const;
+  QWidget * create( const QString & key, QWidget * parent, const char * name );
+  QString group( const QString & key ) const;
+  //QIconSet iconSet( const QString & key ) const;
+  QString includeFile( const QString & key ) const;
+  QString toolTip( const QString & key ) const;
+  QString whatsThis( const QString & key ) const;
+  bool isContainer( const QString & key ) const;
+};
+
+#endif
