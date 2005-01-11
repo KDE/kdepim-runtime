953649fa34dd9ffd56528c08526e18b19db5b1b8
Reinhold Kainhofer <reinhold@kainhofer.com>
2005-01-11 17:21:54 +0000

    Add support for custom pages in the incidence editors that display an attachment inline. If an attachment of the form ATTACH;FMTTYPE=text/html;X-CONTENT-DISPOSITION=inline;X-LABEL=Titel:http://reinhold.kainhofer.com/ is encountered in an incidence, a new tab is added to the incidence editor dialog, and the URL is shown there in a read-only part. This will be used to let a groupware server provide additional configuration pages for a given incidence.
    Problems that are still there:
    -) non-html content (e.g. pdf) will only be displayed when you open the dialog a second time (not that bad, we'll disable displaying all attachments with content-type other than text/html anyway)
    -) Pressing OK in the editor dialog in KOrganizer will cause the attachment to lose the X-.. fields
    -) The part is read-only, so clicking on a link won't have any effect whatsoever :-((( David, do you have an idea how I can make it start konqueror instead?
    
    CCMAIL: Helge Hess <helge.hess@opengroupware.org>, dfaure@kde.org
    
    svn path=/trunk/kdepim/libkdepim/; revision=377528

---
+++ libkdepim-copy/embeddedurlpage.cpp
@@ -0,0 +1,65 @@
+/*
+    This file is part of libkdepim.
+
+    Copyright (c) 2005 Reinhold Kainhofer <reinhold@kainhofer.com>
+    Part of loadContents() copied from the kpartsdesignerplugin:
+    Copyright (C) 2005, David Faure <faure@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "embeddedurlpage.h"
+#include <kparts/componentfactory.h>
+#include <kparts/part.h>
+#include <kmimetype.h>
+#include <klocale.h>
+#include <qlayout.h>
+#include <qlabel.h>
+
+using namespace KPIM;
+
+EmbeddedURLPage::EmbeddedURLPage( const QString &url, const QString &mimetype,
+                                  QWidget *parent, const char *name )
+  : QWidget( parent, name ), mUri(url), mMimeType( mimetype ), mPart( 0 )
+{
+  initGUI( url, mimetype );
+}
+
+void EmbeddedURLPage::initGUI( const QString &url, const QString &/*mimetype*/ )
+{
+  QVBoxLayout *layout = new QVBoxLayout( this );
+  layout->setAutoAdd( true );
+  new QLabel( i18n("Showing URL %1").arg( url ), this );
+}
+
+void EmbeddedURLPage::loadContents()
+{
+  if ( !mPart ) {
+    if ( mMimeType.isEmpty() || mUri.isEmpty() )
+        return;
+    QString mimetype = mMimeType;
+    if ( mimetype == "auto" )
+        mimetype == KMimeType::findByURL( mUri )->name();
+    // "this" is both the parent widget and the parent object
+    mPart = KParts::ComponentFactory::createPartInstanceFromQuery<KParts::ReadOnlyPart>( mimetype, QString::null, this, 0, this, 0 );
+    if ( mPart ) {
+        mPart->openURL( mUri );
+        mPart->widget()->show();
+    }
+  }
+}
+
+#include "embeddedurlpage.moc"
---
+++ libkdepim-copy/embeddedurlpage.h
@@ -0,0 +1,50 @@
+/*
+    This file is part of libkdepim.
+
+    Copyright (c) 2005 Reinhold Kainhofer <reinhold@kainhofer.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+#ifndef KPIM_EMBEDDEDURLPAGE_H
+#define KPIM_EMBEDDEDURLPAGE_H
+
+#include <qwidget.h>
+#include <kdepimmacros.h>
+
+namespace KParts { class ReadOnlyPart; }
+
+namespace KPIM {
+
+class KDE_EXPORT EmbeddedURLPage : public QWidget
+{
+    Q_OBJECT
+  public:
+    EmbeddedURLPage( const QString &url, const QString &mimetype,
+                     QWidget *parent, const char *name = 0 );
+
+  public slots:
+    void loadContents();
+  private:
+    void initGUI( const QString &url, const QString &mimetype );
+
+    QString mUri;
+    QString mMimeType;
+    KParts::ReadOnlyPart* mPart;
+};
+
+}
+
+#endif
