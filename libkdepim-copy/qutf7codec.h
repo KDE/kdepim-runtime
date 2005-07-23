b18939ec3ad38c10f040bd9b1c1d90123160f717
Reinhold Kainhofer <reinhold@kainhofer.com>
2005-07-23 19:53:57 +0000

    The FSF has moved, use the new address in all license files and headers ...
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=437980

--- libkdepim-copy/qutf7codec.h
+++ libkdepim-copy/qutf7codec.h
@@ -11,8 +11,8 @@
 
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-  02111-1307, US
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, US
 
   As a special exception, permission is granted to use this plugin
   with any version of Qt by TrollTech AS, Norway. In this case, the
