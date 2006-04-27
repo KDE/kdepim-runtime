ff97071f0736cec09610be31adcc3cf9f55f7354
Allen Winter <winter@kde.org>
2006-04-27 23:52:45 +0000

    Exporting Stuff for the kdepim libraries.
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=534884

---
+++ libkdepim-copy/kdepim_export.h
@@ -0,0 +1,36 @@
+/*
+    This file is part of libkdepim.
+    Copyright (c) 2006 Allen Winter <winter@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KDEPIM_EXPORT_H
+#define KDEPIM_EXPORT_H
+
+/* needed for KDE_EXPORT macros */
+#include <kdepimmacros.h>
+
+#if defined _WIN32 || defined _WIN64
+# include <kdepim_export_win.h>
+#else /* UNIX */
+
+/* export statements for unix */
+#define KHOLIDAYS_EXPORT KDE_EXPORT
+
+#endif
+
+#endif
