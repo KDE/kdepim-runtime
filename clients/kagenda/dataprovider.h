d885f41813015321782c7f56ada8b71e8014373c
Cornelius Schumacher <schumacher@kde.org>
2006-07-06 10:45:18 +0000

    Add decent license headers.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=558947

--- clients/kagenda/dataprovider.h
+++ clients/kagenda/dataprovider.h
@@ -1,5 +1,7 @@
 /*
-    This file is part of KDE.
+    This file is part of Akonadi.
+
+    Copyright (c) 2006 Cornelius Schumacher <schumacher@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
