9918a6366713d9f67283875a1b5421732b33b385
Tobias Koenig <tokoe@kde.org>
2007-01-13 10:07:58 +0000

    Changed the Interpreter to be type independend and add unit test
    svn path=/trunk/KDE/kdepim/akonadi/; revision=622835

--- searchproviders/interpreter.cpp
+++ searchproviders/interpreter.cpp
@@ -19,6 +19,88 @@
 
 #include "interpreter.h"
 
+InterpreterItem::InterpreterItem( const QString &key, const QString &comparator, const QString &pattern )
+  : mKey( key ), mComparator( comparator ), mPattern( pattern ), mIsLeaf( true )
+{
+}
+
+InterpreterItem::InterpreterItem( Relation relation )
+  : mRelation( relation ), mIsLeaf( false )
+{
+}
+
+void InterpreterItem::setChildItems( const QList<InterpreterItem*> items )
+{
+  mItems = items;
+}
+
+QList<InterpreterItem*> InterpreterItem::childItems() const
+{
+  return mItems;
+}
+
+InterpreterItem::~InterpreterItem()
+{
+  qDeleteAll( mItems );
+  mItems.clear();
+}
+
+bool InterpreterItem::isLeafItem() const
+{
+  return mIsLeaf;
+}
+
+InterpreterItem::Relation InterpreterItem::relation() const
+{
+  return mRelation;
+}
+
+QString InterpreterItem::key() const
+{
+  return mKey;
+}
+
+QString InterpreterItem::comparator() const
+{
+  return mComparator;
+}
+
+QString InterpreterItem::pattern() const
+{
+  return mPattern;
+}
+
+QString InterpreterItem::dump() const
+{
+  if ( mIsLeaf ) {
+    return QString( "(%1 %2 %3)" ).arg( mKey, mComparator, mPattern );
+  } else {
+    QString text = "(";
+    text += ( mRelation == And ? "&" : "|" );
+    for ( int i = 0; i < mItems.count(); ++i )
+      text += mItems[ i ]->dump();
+    text += ")";
+
+    return text;
+  }
+}
+
+InterpreterItem* Parser::parse( const QString &query ) const
+{
+  QString pattern( query.trimmed() );
+
+  if ( pattern.isEmpty() ) {
+    qDebug( "empty search pattern passed!" );
+    return 0;
+  }
+
+  const QStringList tokens = tokenize( pattern );
+
+  QStringListIterator it( tokens );
+
+  return parseInternal( it );
+}
+
 QStringListIterator Parser::balanced( QStringListIterator _it ) const
 {
   QStringListIterator it( _it );
@@ -122,4 +204,83 @@ QStringList Parser::tokenize( const QString &query ) const
     tokens.append( token );
 
   return tokens;
+}
+
+InterpreterItem* Parser::parseInternal( QStringListIterator &it ) const
+{
+  if ( !it.hasNext() )
+    return 0;
+
+  QString token = it.next();
+  if ( token == QLatin1String( "&" ) || token == QLatin1String( "|" ) ) {
+    /**
+     * We have a term like: (&( ... )( ... )( ... ))
+     */
+    QList<InterpreterItem*> childItems;
+
+    const QString operatorSign = token;
+
+    do {
+      QStringListIterator nextIt = balanced( it );
+      if ( !nextIt.hasNext() ) {
+        qDeleteAll( childItems );
+        return 0;
+      }
+      token = nextIt.next();
+
+      if ( !nextIt.hasPrevious() ) {
+        qDeleteAll( childItems );
+        return 0;
+      }
+      nextIt.previous();
+
+      InterpreterItem *item = parseInternal( it );
+      if ( item == 0 ) {
+        qDeleteAll( childItems );
+        return 0;
+      }
+
+      childItems.append( item );
+
+      if ( token[ 0 ] != '(' )
+        break;
+
+      it = nextIt;
+    } while ( true );
+
+    InterpreterItem *item = 0;
+
+    if ( operatorSign == QLatin1String( "&" ) )
+      item = new InterpreterItem( InterpreterItem::And );
+    else
+      item = new InterpreterItem( InterpreterItem::Or );
+
+    item->setChildItems( childItems );
+
+    return item;
+  } else if ( token[ 0 ] == '(' ) {
+    /**
+     * We have a term like: ( ... )
+     */
+    return parseInternal( it );
+  } else {
+    /**
+     * We have a term like: a = b
+     */
+    const QString key = token;
+
+    if ( !it.hasNext() )
+      return 0;
+
+    const QString comparator = it.next();
+
+    if ( !it.hasNext() )
+      return 0;
+
+    const QString pattern = it.next();
+
+    return new InterpreterItem( key, comparator, pattern );
+  }
+
+  return 0;
 }
--- searchproviders/interpreter.h
+++ searchproviders/interpreter.h
@@ -26,7 +26,6 @@
 /**
  * This class encapsulates solving nexted boolean constructs
  */
-template <typename T>
 class InterpreterItem
 {
   public:
@@ -39,80 +38,32 @@ class InterpreterItem
     /**
      * Creates a new solver item with the given key, comparator and pattern.
      */
-    InterpreterItem( const QString &key, const QString &comparator, const QString &pattern )
-      : mKey( key ), mComparator( comparator ), mPattern( pattern ), mIsLeaf( true )
-    {
-    }
+    InterpreterItem( const QString &key, const QString &comparator, const QString &pattern );
 
     /**
      * Creates a new solver item which combines the given child items by a given relation.
      *
      * Ownership of the child items is transfered to the item.
      */
-    InterpreterItem( Relation relation )
-      : mRelation( relation )
-    {
-    }
-
-    virtual void setChildItems( const QList<InterpreterItem<T>*> items )
-    {
-      mItems = items;
-    }
+    InterpreterItem( Relation relation );
 
     /**
      * Destroys the solver item and all its child items.
      */
-    virtual ~InterpreterItem()
-    {
-      qDeleteAll( mItems );
-      mItems.clear();
-    }
+    virtual ~InterpreterItem();
 
-    /**
-     * Starts the resolution of the boolean construct for a given object.
-     */
-    bool solve( const T* object ) const
-    {
-      if ( mIsLeaf )
-        return matches( mKey, mComparator, mPattern, object );
+    virtual void setChildItems( const QList<InterpreterItem*> items );
 
-      if ( mRelation == And ) {
-        for ( int i = 0; i < mItems.count(); ++i ) {
-          if ( !mItems[ i ]->solve( object ) )
-            return false;
-        }
+    QList<InterpreterItem*> childItems() const;
 
-        return true;
-      } else { // Or
-        for ( int i = 0; i < mItems.count(); ++i ) {
-          if ( mItems[ i ]->solve( object ) )
-            return true;
-        }
+    bool isLeafItem() const;
+    Relation relation() const;
 
-        return false;
-      }
-    }
+    QString key() const;
+    QString comparator() const;
+    QString pattern() const;
 
-    QString dump() const
-    {
-      if ( mIsLeaf ) {
-        return QString( "(%1 %2 %3)" ).arg( mKey, mComparator, mPattern );
-      } else {
-        QString text = "(";
-        text += ( mRelation == And ? "&" : "|" );
-        for ( int i = 0; i < mItems.count(); ++i )
-          text += mItems[ i ]->dump();
-        text += ")";
-
-        return text;
-      }
-    }
-
-  protected:
-    /**
-     * This method must be reimplemented to do the actual comparision for the specific type.
-     */
-    virtual bool matches( const QString &key, const QString &comparator, const QString &pattern, const T* object ) const = 0;
+    QString dump() const;
 
   private:
     QString mKey;
@@ -120,7 +71,7 @@ class InterpreterItem
     QString mPattern;
 
     Relation mRelation;
-    QList<InterpreterItem<T>*> mItems;
+    QList<InterpreterItem*> mItems;
 
     bool mIsLeaf;
 };
@@ -128,106 +79,13 @@ class InterpreterItem
 class Parser
 {
   public:
-    template <typename T>
-    T* parse( const QString &query ) const
-    {
-      QString pattern( query.trimmed() );
-
-      if ( pattern.isEmpty() ) {
-        qDebug( "empty search pattern passed!" );
-        return 0;
-      }
-
-      const QStringList tokens = tokenize( pattern );
-
-      QStringListIterator it( tokens );
-
-      return parseInternal<T>( it );
-    };
+    InterpreterItem* parse( const QString &query ) const;
 
   private:
     QStringList tokenize( const QString& ) const;
     QStringListIterator balanced( QStringListIterator ) const;
 
-    template <typename T>
-    T* parseInternal( QStringListIterator &it ) const
-    {
-      if ( !it.hasNext() )
-        return 0;
-
-      QString token = it.next();
-      if ( token == QLatin1String( "&" ) || token == QLatin1String( "|" ) ) {
-        /**
-         * We have a term like: (&( ... )( ... )( ... ))
-         */
-        QList<T*> childItems;
-
-        const QString operatorSign = token;
-
-        do {
-          QStringListIterator nextIt = balanced( it );
-          if ( !nextIt.hasNext() ) {
-            qDeleteAll( childItems );
-            return 0;
-          }
-          token = nextIt.next();
-
-          if ( !nextIt.hasPrevious() ) {
-            qDeleteAll( childItems );
-            return 0;
-          }
-          nextIt.previous();
-
-          T *item = parseInternal<T>( it );
-          if ( item == 0 ) {
-            qDeleteAll( childItems );
-            return 0;
-          }
-
-          childItems.append( item );
-
-          if ( token[ 0 ] != '(' )
-            break;
-
-          it = nextIt;
-        } while ( true );
-
-        T *item = 0;
-
-        if ( operatorSign == QLatin1String( "&" ) )
-          item = new T( T::And );
-        else
-          item = new T( T::Or );
-
-        item->setChildItems( childItems );
-
-        return item;
-      } else if ( token[ 0 ] == '(' ) {
-        /**
-         * We have a term like: ( ... )
-         */
-        return parseInternal<T>( it );
-      } else {
-        /**
-         * We have a term like: a = b
-         */
-        const QString key = token;
-
-        if ( !it.hasNext() )
-          return 0;
-
-        const QString comparator = it.next();
-
-        if ( !it.hasNext() )
-          return 0;
-
-        const QString pattern = it.next();
-
-        return new T( key, comparator, pattern );
-      }
-
-      return 0;
-    }
+    InterpreterItem* parseInternal( QStringListIterator &it ) const;
 };
 
 #endif
