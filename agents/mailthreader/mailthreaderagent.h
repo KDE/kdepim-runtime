632c97006972759d29b8a3adf05bab1f080cd4bc
Bruno Virlet <bruno.virlet@gmail.com>
2007-06-25 09:11:57 +0000

    Mailthreader agent base.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=679995

---
+++ agents/mailthreader/mailthreaderagent.h
@@ -0,0 +1,57 @@
+/*
+    Copyright (c) 2007 Bruno Virlet <bruno.virlet@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef MAILTHREADERAGENT_H
+#define MAILTHREADERAGENT_H
+
+#include <agentbase.h>
+
+/**
+ * This agent works on a mail collection an tries to thread
+ * mails related to the same discussion :
+ * 
+ * Mail
+ * + Reply1
+ *   + Reply11
+ *   + Reply12
+ * + Reply2
+ * ...
+ */
+class MailThreaderAgent : public Akonadi::AgentBase
+{
+  Q_OBJECT
+
+  public:
+    MailThreaderAgent( const QString &id );
+    ~MailThreaderAgent();
+
+  public Q_SLOTS:
+    virtual void configure();
+
+  protected:
+    virtual void aboutToQuit();
+
+    virtual void itemAdded( const Akonadi::Item &item, const Akonadi::Collection &collection );
+    virtual void itemChanged( const Akonadi::Item &item, const QStringList &parts );
+    virtual void itemRemoved( const Akonadi::DataReference &ref );
+
+  private:
+};
+
+#endif
