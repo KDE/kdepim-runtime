631eba6fc984b7a9db58c28cae9c7406134d7be7
Allen Winter <winter@kde.org>
2007-09-08 13:54:25 +0000

    Move KFolderTree into the KPIM namespace.
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=709896

--- libkdepim-copy/kfoldertree.h
+++ libkdepim-copy/kfoldertree.h
@@ -1,33 +1,37 @@
 /* -*- mode: C++; c-file-style: "gnu" -*-
 
-   This file is part of libkdepim.
+  This file is part of libkdepim.
 
-   Copyright (C) 2002 Carsten Burghardt <burghardt@kde.org>
-   Copyright (C) 2002 Marc Mutz <mutz@kde.org>
+  Copyright (C) 2002 Carsten Burghardt <burghardt@kde.org>
+  Copyright (C) 2002 Marc Mutz <mutz@kde.org>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License version 2 as published by the Free Software Foundation.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 #ifndef __KFOLDERTREE
 #define __KFOLDERTREE
+
+#include "kdepim_export.h"
+
+#include <k3listview.h>
 
 #include <QPixmap>
 #include <QMouseEvent>
 #include <QDropEvent>
 #include <QVector>
-#include <k3listview.h>
-#include <kdepim_export.h>
+
+namespace KPIM {
 
 class KFolderTree;
 
@@ -35,8 +39,7 @@ class KFolderTree;
 struct KPaintInfo {
 
   // Popup ids for toggle-able columns
-  enum ColumnIds
-  {
+  enum ColumnIds {
     COL_SIZE,
     COL_ATTACHMENT,
     COL_IMPORTANT,
@@ -51,40 +54,40 @@ struct KPaintInfo {
   };
 
   KPaintInfo() :
-    pixmapOn(false),
+    pixmapOn( false ),
 
-    showSize(false),
-    showAttachment(false),
-    showImportant(false),
+    showSize( false ),
+    showAttachment( false ),
+    showImportant( false ),
     showTodo( false ),
-    showSpamHam(false),
-    showWatchedIgnored(false),
-    showStatus(false),
-    showSigned(false),
-    showCrypto(false),
-    showReceiver(false),
-    showScore(false),
+    showSpamHam( false ),
+    showWatchedIgnored( false ),
+    showStatus( false ),
+    showSigned( false ),
+    showCrypto( false ),
+    showReceiver( false ),
+    showScore( false ),
 
-    scoreCol(-1),
-    flagCol(-1),
-    senderCol(-1),
-    receiverCol(-1),
-    subCol(-1),
-    dateCol(-1),
-    sizeCol(-1),
-    attachmentCol(-1),
-    importantCol(-1),
-    todoCol(-1),
-    spamHamCol(-1),
-    watchedIgnoredCol(-1),
-    statusCol(-1),
-    signedCol(-1),
-    cryptoCol(-1),
+    scoreCol( -1 ),
+    flagCol( -1 ),
+    senderCol( -1 ),
+    receiverCol( -1 ),
+    subCol( -1 ),
+    dateCol( -1 ),
+    sizeCol( -1 ),
+    attachmentCol( -1 ),
+    importantCol( -1 ),
+    todoCol( -1 ),
+    spamHamCol( -1 ),
+    watchedIgnoredCol( -1 ),
+    statusCol( -1 ),
+    signedCol( -1 ),
+    cryptoCol( -1 ),
 
-    orderOfArrival(false),
-    status(false),
-    showCryptoIcons(false),
-    showAttachmentIcon(false)
+    orderOfArrival( false ),
+    status( false ),
+    showCryptoIcons( false ),
+    showAttachmentIcon( false )
     {}
 
   bool pixmapOn;
@@ -164,16 +167,15 @@ class KDEPIM_EXPORT KFolderTreeItem : public K3ListViewItem
     };
 
     /** constructs a root-item */
-    KFolderTreeItem( KFolderTree *parent, const QString & label=QString(),
-        Protocol protocol=NONE, Type type=Root );
+    explicit KFolderTreeItem( KFolderTree *parent, const QString &label=QString(),
+                              Protocol protocol=NONE, Type type=Root );
 
     /** constructs a child-item */
-    KFolderTreeItem( KFolderTreeItem *parent, const QString & label=QString(),
-        Protocol protocol=NONE, Type type=Other, int unread=0, int total=0 );
+    explicit KFolderTreeItem( KFolderTreeItem *parent, const QString &label=QString(),
+                              Protocol protocol=NONE, Type type=Other, int unread=0, int total=0 );
 
     /** compare */
-    virtual int compare( Q3ListViewItem * i, int col,
-        bool ascending ) const;
+    virtual int compare( Q3ListViewItem *i, int col, bool ascending ) const;
 
     /** set/get the unread-count */
     int unreadCount() { return mUnread; }
@@ -201,11 +203,11 @@ class KDEPIM_EXPORT KFolderTreeItem : public K3ListViewItem
     virtual size_t recursiveFolderSize() const;
 
     /** paints the cell */
-    virtual void paintCell( QPainter * p, const QColorGroup & cg,
-        int column, int width, int align );
+    virtual void paintCell( QPainter *p, const QColorGroup &cg,
+                            int column, int width, int align );
 
     /** dnd */
-    virtual bool acceptDrag(QDropEvent* ) const { return true; }
+    virtual bool acceptDrag( QDropEvent * ) const { return true; }
 
     void setFolderIsCloseToQuota( bool );
     bool folderIsCloseToQuota() const;
@@ -237,33 +239,32 @@ class KDEPIM_EXPORT KFolderTree : public K3ListView
   Q_OBJECT
 
   public:
-    KFolderTree( QWidget *parent, const char *name=0 );
+    explicit KFolderTree( QWidget *parent, const char *name=0 );
 
     /** registers MIMETypes that are handled
       @param mimeType the name of the MIMEType
       @param outsideOk accept drops of this type even if
       the mouse cursor is not on top of an item */
-    virtual void addAcceptableDropMimetype( QString mimeType, bool outsideOk );
+    virtual void addAcceptableDropMimetype( const QString &mimeType, bool outsideOk );
 
     /** checks if the drag is acceptable */
-    virtual bool acceptDrag( QDropEvent* event ) const;
+    virtual bool acceptDrag( QDropEvent *event ) const;
 
     /** returns the KPaintInfo */
     KPaintInfo paintInfo() const { return mPaintInfo; }
 
     /** add/remove unread/total-columns */
-    virtual void addUnreadColumn( const QString & name, int width=70 );
+    virtual void addUnreadColumn( const QString &name, int width=70 );
     virtual void removeUnreadColumn();
-    virtual void addTotalColumn( const QString & name, int width=70 );
+    virtual void addTotalColumn( const QString &name, int width=70 );
     virtual void removeTotalColumn();
-    virtual void addSizeColumn( const QString & name, int width=70 );
+    virtual void addSizeColumn( const QString &name, int width=70 );
     virtual void removeSizeColumn();
-
 
     /** the current index of the unread/total column */
     int unreadIndex() const { return mUnreadIndex; }
-    int totalIndex() const { return mTotalIndex;  }
-    int sizeIndex() const { return mSizeIndex;  }
+    int totalIndex() const { return mTotalIndex; }
+    int sizeIndex() const { return mSizeIndex; }
 
     /** is the unread/total-column active? */
     bool isUnreadActive() const { return mUnreadIndex >= 0; }
@@ -276,14 +277,14 @@ class KDEPIM_EXPORT KFolderTree : public K3ListView
   protected:
     /** reimplemented in order to update the frame width in case of a changed
         GUI style */
-    void styleChange( QStyle& oldStyle );
+    void styleChange( QStyle &oldStyle );
 
     /** Set the width of the frame to a reasonable value for the current GUI
         style */
     void setStyleDependantFrameWidth();
 
-    virtual void drawContentsOffset( QPainter * p, int ox, int oy,
-        int cx, int cy, int cw, int ch );
+    virtual void drawContentsOffset( QPainter *p, int ox, int oy,
+                                     int cx, int cy, int cw, int ch );
 
     virtual void contentsMousePressEvent( QMouseEvent *e );
     virtual void contentsMouseReleaseEvent( QMouseEvent *e );
@@ -307,5 +308,7 @@ class KDEPIM_EXPORT KFolderTree : public K3ListView
     void slotSizeChanged( int section, int oldSize, int newSize );
 
 };
+
+}
 
 #endif
