fcf9ceca3b8e06729210ff8ddc578a7bc8d2f368
Allen Winter <winter@kde.org>
2007-09-13 14:47:18 +0000

    Put into the KPIM namespace Add our standard GPL license header minor coding style
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=712083

--- libkdepim-copy/kaddrbook.cpp
+++ libkdepim-copy/kaddrbook.cpp
@@ -1,43 +1,57 @@
-// -*- mode: C++; c-file-style: "gnu" -*-
-// kaddrbook.cpp
-// Author: Stefan Taferner <taferner@kde.org>
-// This code is under GPL
+/*
+  Simple Addressbook for KMail
+  Copyright Stefan Taferner <taferner@kde.org>
 
-#include <config.h> // KDEPIM_NEW_DISTRLISTS
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 
 #include "kaddrbook.h"
+#include "kaddressbookcore_interface.h"
 
+#include <config.h> // KDEPIM_NEW_DISTRLISTS
 #ifdef KDEPIM_NEW_DISTRLISTS
 #include "distributionlist.h"
 #else
 #include <kabc/distributionlist.h>
 #endif
-
-#include <kdebug.h>
-#include <klocale.h>
-#include <kmessagebox.h>
-#include <kdeversion.h>
 #include <kabc/resource.h>
 #include <kabc/stdaddressbook.h>
 #include <kabc/vcardconverter.h>
 #include <kabc/errorhandler.h>
 #include <kresources/selectdialog.h>
+
+#include <kdeversion.h>
+#include <kdefakes.h> // usleep
+#include <kdebug.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+#include <ktoolinvocation.h>
+
 #include <QtDBus/QDBusInterface>
 #include <QtDBus/QDBusConnection>
-
 #include <QApplication>
 #include <QEventLoop>
 #include <QRegExp>
 #include <QList>
 
 #include <unistd.h>
-#include <kdefakes.h> // usleep
-#include <ktoolinvocation.h>
-#include "kaddressbookcore_interface.h"
+
+using namespace KPIM;
 
 //-----------------------------------------------------------------------------
 void KAddrBookExternal::openEmail( const QString &email, const QString &addr, QWidget *) {
-  //QString email = KMMessage::getEmailAddr(addr);
   KABC::AddressBook *addressBook = KABC::StdAddressBook::self( true );
   KABC::Addressee::List addresseeList = addressBook->findByEmail(email);
 #ifdef __GNUC__
@@ -45,34 +59,35 @@ void KAddrBookExternal::openEmail( const QString &email, const QString &addr, QW
 #endif
   // DF: it comes from KUniqueApplication
 
-  QDBusInterface abinterface( "org.kde.KAddressbook", "/MainApplication", "org.kde.KUniqueApplication" );
+  QDBusInterface abinterface( "org.kde.KAddressbook", "/MainApplication",
+                              "org.kde.KUniqueApplication" );
+
   if ( abinterface.isValid() ) {
     //make sure kaddressbook is loaded, otherwise showContactEditor
     //won't work as desired, see bug #87233
     abinterface.call( "newInstance" );
-  }
-  else
+  } else {
     KToolInvocation::startServiceByDesktopName( "kaddressbook" );
-
-  OrgKdeKAddressbookCoreInterface interface("org.kde.KAddressbook", "/KAddressbook", QDBusConnection::sessionBus());
-  if( !addresseeList.isEmpty() ) {
-    interface.showContactEditor(addresseeList.first().uid() );
   }
-  else {
-    interface.addEmail(addr);
+
+  OrgKdeKAddressbookCoreInterface interface( "org.kde.KAddressbook",
+                                             "/KAddressbook",
+                                             QDBusConnection::sessionBus() );
+  if( !addresseeList.isEmpty() ) {
+    interface.showContactEditor( addresseeList.first().uid() );
+  } else {
+    interface.addEmail( addr );
   }
 }
 
 //-----------------------------------------------------------------------------
-void KAddrBookExternal::addEmail( const QString &addr, QWidget *parent)
+void KAddrBookExternal::addEmail( const QString &addr, QWidget *parent )
 {
   QString email;
   QString name;
 
   KABC::Addressee::parseEmailAddress( addr, name, email );
-
   KABC::AddressBook *ab = KABC::StdAddressBook::self( true );
-
   ab->setErrorHandler( new KABC::GuiErrorHandler( parent ) );
 
   // force a reload of the address book file so that changes that were made
@@ -87,28 +102,30 @@ void KAddrBookExternal::addEmail( const QString &addr, QWidget *parent)
     a.insertEmail( email, true );
 
     if ( KAddrBookExternal::addAddressee( a ) ) {
-      QString text = i18n("<qt>The email address <b>%1</b> was added to your "
-                          "addressbook; you can add more information to this "
-                          "entry by opening the addressbook.</qt>", addr );
+      QString text = i18n( "<qt>The email address <b>%1</b> was added to your "
+                           "addressbook; you can add more information to this "
+                           "entry by opening the addressbook.</qt>", addr );
       KMessageBox::information( parent, text, QString(), "addedtokabc" );
     }
   } else {
-    QString text = i18n("<qt>The email address <b>%1</b> is already in your "
-                        "addressbook.</qt>", addr );
-    KMessageBox::information( parent, text, QString(),
-                              "alreadyInAddressBook" );
+    QString text =
+      i18n( "<qt>The email address <b>%1</b> is already in your addressbook.</qt>", addr );
+    KMessageBox::information( parent, text, QString(), "alreadyInAddressBook" );
   }
   ab->setErrorHandler( 0 );
 }
 
-void KAddrBookExternal::openAddressBook( QWidget * ) {
+void KAddrBookExternal::openAddressBook( QWidget * )
+{
   KToolInvocation::startServiceByDesktopName( "kaddressbook" );
 }
 
 void KAddrBookExternal::addNewAddressee( QWidget * )
 {
-  KToolInvocation::startServiceByDesktopName("kaddressbook");
-  OrgKdeKAddressbookCoreInterface interface("org.kde.KAddressbook", "/KAddressbook", QDBusConnection::sessionBus());
+  KToolInvocation::startServiceByDesktopName( "kaddressbook" );
+  OrgKdeKAddressbookCoreInterface interface( "org.kde.KAddressbook",
+                                             "/KAddressbook",
+                                             QDBusConnection::sessionBus() );
   interface.newContact();
 }
 
@@ -117,24 +134,23 @@ bool KAddrBookExternal::addVCard( const KABC::Addressee &addressee, QWidget *par
   KABC::AddressBook *ab = KABC::StdAddressBook::self( true );
   bool inserted = false;
 
-   ab->setErrorHandler( new KABC::GuiErrorHandler( parent ) );
+  ab->setErrorHandler( new KABC::GuiErrorHandler( parent ) );
 
-   KABC::Addressee::List addressees =
-      ab->findByEmail( addressee.preferredEmail() );
+  KABC::Addressee::List addressees =
+    ab->findByEmail( addressee.preferredEmail() );
 
   if ( addressees.isEmpty() ) {
     if ( KAddrBookExternal::addAddressee( addressee ) ) {
-      QString text = i18n("The VCard was added to your addressbook; "
-                          "you can add more information to this "
-                          "entry by opening the addressbook.");
+      QString text = i18n( "The VCard was added to your addressbook; "
+                           "you can add more information to this "
+                           "entry by opening the addressbook." );
       KMessageBox::information( parent, text, QString(), "addedtokabc" );
       inserted = true;
     }
   } else {
-    QString text = i18n("The VCard's primary email address is already in "
-                        "your addressbook; however, you may save the VCard "
-                        "into a file and import it into the addressbook "
-                        "manually.");
+    QString text = i18n( "The VCard's primary email address is already in "
+                         "your addressbook; however, you may save the VCard "
+                         "into a file and import it into the addressbook manually." );
     KMessageBox::information( parent, text );
     inserted = true;
   }
@@ -164,12 +180,14 @@ bool KAddrBookExternal::addAddressee( const KABC::Addressee &addr )
     kabcResource = resIt.next();
     if ( !kabcResource->readOnly() ) {
       KRES::Resource *res = static_cast<KRES::Resource*>( kabcResource );
-      if ( res )
+      if ( res ) {
         kresResources.append( res );
+      }
     }
   }
 
-  kabcResource = static_cast<KABC::Resource*>( KRES::SelectDialog::getResource( kresResources, 0 ) );
+  kabcResource =
+    static_cast<KABC::Resource*>( KRES::SelectDialog::getResource( kresResources, 0 ) );
 
   KABC::Ticket *ticket = addressBook->requestSaveTicket( kabcResource );
   bool saved = false;
@@ -178,8 +196,9 @@ bool KAddrBookExternal::addAddressee( const KABC::Addressee &addr )
     addressee.setResource( kabcResource );
     addressBook->insertAddressee( addressee );
     saved = addressBook->save( ticket );
-    if ( !saved )
+    if ( !saved ) {
       addressBook->releaseSaveTicket( ticket );
+    }
   }
 
   addressBook->emitAddressBookChanged();
@@ -187,15 +206,17 @@ bool KAddrBookExternal::addAddressee( const KABC::Addressee &addr )
   return saved;
 }
 
-QString KAddrBookExternal::expandDistributionList( const QString& listName )
+QString KAddrBookExternal::expandDistributionList( const QString &listName )
 {
-  if ( listName.isEmpty() )
+  if ( listName.isEmpty() ) {
     return QString();
+  }
 
   const QString lowerListName = listName.toLower();
   KABC::AddressBook *addressBook = KABC::StdAddressBook::self( true );
 #ifdef KDEPIM_NEW_DISTRLISTS
-  KPIM::DistributionList distrList = KPIM::DistributionList::findByName( addressBook, lowerListName, false );
+  KPIM::DistributionList distrList =
+    KPIM::DistributionList::findByName( addressBook, lowerListName, false );
   if ( !distrList.isEmpty() ) {
     return distrList.emails( addressBook ).join( ", " );
   }
@@ -204,8 +225,7 @@ QString KAddrBookExternal::expandDistributionList( const QString& listName )
   manager.load();
   const QStringList listNames = manager.listNames();
 
-  for ( QStringList::ConstIterator it = listNames.begin();
-        it != listNames.end(); ++it) {
+  for ( QStringList::ConstIterator it = listNames.begin(); it != listNames.end(); ++it ) {
     if ( (*it).toLower() == lowerListName ) {
       const QStringList addressList = manager.list( *it )->emails();
       return addressList.join( ", " );
--- libkdepim-copy/kaddrbook.h
+++ libkdepim-copy/kaddrbook.h
@@ -1,30 +1,49 @@
-/* Simple Addressbook for KMail
- * Author: Stefan Taferner <taferner@kde.org>
- * This code is under GPL
- */
-#ifndef KAddrBook_h
-#define KAddrBook_h
+/*
+  Simple Addressbook for KMail
+  Copyright Stefan Taferner <taferner@kde.org>
 
-#include <QStringList>
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-#include <kdeversion.h>
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+#ifndef KPIM_KADDRBOOKEXTERNAL_H
+#define KPIM_KADDRBOOKEXTERNAL_H
+
+#include "kdepim_export.h"
 #include <kabc/addressee.h>
-#include <kdepim_export.h>
+#include <kdeversion.h>
+#include <QStringList>
 
 class QWidget;
 
-class KDEPIM_EXPORT KAddrBookExternal {
-public:
-  static void addEmail( const QString &addr, QWidget *parent );
-  static void addNewAddressee( QWidget* );
-  static void openEmail( const QString &email, const QString &addr, QWidget *parent );
-  static void openAddressBook( QWidget *parent );
+namespace KPIM {
 
-  static bool addVCard( const KABC::Addressee& addressee, QWidget *parent );
+class KDEPIM_EXPORT KAddrBookExternal
+{
+  public:
+    static void addEmail( const QString &addr, QWidget *parent );
+    static void addNewAddressee( QWidget * );
+    static void openEmail( const QString &email, const QString &addr, QWidget *parent );
+    static void openAddressBook( QWidget *parent );
 
-  static QString expandDistributionList( const QString& listName );
-private:
-  static bool addAddressee( const KABC::Addressee& addressee );
+    static bool addVCard( const KABC::Addressee &addressee, QWidget *parent );
+
+    static QString expandDistributionList( const QString &listName );
+
+  private:
+    static bool addAddressee( const KABC::Addressee &addressee );
 };
 
-#endif /*KAddrBook_h*/
+}
+
+#endif
