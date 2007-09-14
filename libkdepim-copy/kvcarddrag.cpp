6aa67740dee9ffcff68fe345930abd9c9d36a153
Allen Winter <winter@kde.org>
2007-09-14 19:55:12 +0000

    move into the KPIM namespace
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=712570

--- libkdepim-copy/kvcarddrag.cpp
+++ libkdepim-copy/kvcarddrag.cpp
@@ -1,28 +1,29 @@
 /*
-    This file is part of libkdepim.
+  This file is part of libkdepim.
 
-    Copyright (c) 2002 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2002 Tobias Koenig <tokoe@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
 */
 
 #include "kvcarddrag.h"
-
 #include <kabc/vcardconverter.h>
 #include <QMimeData>
+
+using namespace KPIM;
 
 QString KVCardDrag::mimeType()
 {
@@ -35,13 +36,13 @@ bool KVCardDrag::populateMimeData( QMimeData *md, const QByteArray &content )
   return true;
 }
 
-bool KVCardDrag::populateMimeData( QMimeData *md, const KABC::Addressee::List &adressees )
+bool KVCardDrag::populateMimeData( QMimeData *md, const KABC::Addressee::List &addressees )
 {
   KABC::VCardConverter converter;
-  QByteArray vcards = converter.createVCards( adressees );
+  QByteArray vcards = converter.createVCards( addressees );
   if ( !vcards.isEmpty() ) {
     return populateMimeData( md, vcards );
-  } else { 
+  } else {
     return false;
   }
 }
@@ -51,19 +52,20 @@ bool KVCardDrag::canDecode( const QMimeData *md )
   return md->hasFormat( mimeType() );
 }
 
-
 bool KVCardDrag::fromMimeData( const QMimeData *md, QByteArray &content )
 {
-  if ( !canDecode(md) ) 
+  if ( !canDecode(md) ) {
     return false;
+  }
   content = md->data( mimeType() );
   return true;
 }
 
-bool KVCardDrag::fromMimeData( const QMimeData *md, KABC::Addressee::List& addressees )
+bool KVCardDrag::fromMimeData( const QMimeData *md, KABC::Addressee::List &addressees )
 {
-  if ( !canDecode(md) ) 
+  if ( !canDecode( md ) ) {
     return false;
+  }
   addressees = KABC::VCardConverter().parseVCards( md->data( mimeType() ) );
   return true;
 }
