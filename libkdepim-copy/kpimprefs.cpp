42aa2dc9b0e9f1c9b5b34b450f224ff4246b019d
Allen Winter <winter@kde.org>
2007-09-15 15:52:21 +0000

    move KPimPrefs into the KPIM namespace
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=712888

--- libkdepim-copy/kpimprefs.cpp
+++ libkdepim-copy/kpimprefs.cpp
@@ -1,31 +1,26 @@
 /*
-    This file is part of libkdepim.
+  This file is part of libkdepim.
 
-    Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
-    Copyright (c) 2005 Rafal Rzepecki <divide@users.sourceforge.net>
+  Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2005 Rafal Rzepecki <divide@users.sourceforge.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
-#include <time.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
-
-#include <QString>
+#include "kpimprefs.h"
 
 #include <kstandarddirs.h>
 #include <kglobal.h>
@@ -34,7 +29,14 @@
 #include <ksystemtimezone.h>
 #include <kdebug.h>
 
-#include "kpimprefs.h"
+#include <QString>
+
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+using namespace KPIM;
 
 KPimPrefs::KPimPrefs( const QString &name )
   : KConfigSkeleton( name )
@@ -52,18 +54,17 @@ void KPimPrefs::usrSetDefaults()
 
 void KPimPrefs::usrReadConfig()
 {
-  kDebug(5300) <<"KPimPrefs::usrReadConfig()";
-
   KConfigGroup group( config(), "General" );
-  mCustomCategories = group.readEntry( "Custom Categories" , QStringList() );
-  if ( mCustomCategories.isEmpty() ) setCategoryDefaults();
+  mCustomCategories = group.readEntry( "Custom Categories", QStringList() );
+  if ( mCustomCategories.isEmpty() ) {
+    setCategoryDefaults();
+  }
   mCustomCategories.sort();
 }
 
 KDateTime::Spec KPimPrefs::timeSpec()
 {
   KTimeZone zone;
-	// FIXME: In KDE 4, use KSystemTimeZones::local instead of system and file system calls!
 
   // Read TimeZoneId from korganizerrc.
   KConfig korgcfg( KStandardDirs::locate( "config", "korganizerrc" ) );
@@ -71,15 +72,17 @@ KDateTime::Spec KPimPrefs::timeSpec()
   QString tz( group.readEntry( "TimeZoneId" ) );
   if ( !tz.isEmpty() ) {
     zone = KSystemTimeZones::zone( tz );
-    if ( zone.isValid() )
-      kDebug(5300) <<"timezone from korganizerrc is" << tz;
+    if ( zone.isValid() ) {
+      kDebug(5300) << "timezone from korganizerrc is" << tz;
+    }
   }
 
   // If timezone not found in KOrg, use the system's default timezone.
   if ( !zone.isValid() ) {
     zone = KSystemTimeZones::local();
-    if ( zone.isValid() )
-      kDebug(5300) <<"system timezone is" << zone.name();
+    if ( zone.isValid() ) {
+      kDebug(5300) << "system timezone is" << zone.name();
+    }
   }
 
   return zone.isValid() ? KDateTime::Spec( zone ) : KDateTime::ClockTime;
--- libkdepim-copy/kpimprefs.h
+++ libkdepim-copy/kpimprefs.h
@@ -1,34 +1,37 @@
 /*
-    This file is part of libkdepim.
+  This file is part of libkdepim.
 
-    Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
-    Copyright (c) 2005 Rafal Rzepecki <divide@users.sourceforge.net>
+  Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2005 Rafal Rzepecki <divide@users.sourceforge.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 #ifndef KPIMPREFS_H
 #define KPIMPREFS_H
 
-#include <QStringList>
+#include "kdepim_export.h"
 
 #include <kconfigskeleton.h>
 #include <kdatetime.h>
-#include "kdepim_export.h"
+
+#include <QStringList>
 
 class QString;
+
+namespace KPIM {
 
 class KDEPIM_EXPORT KPimPrefs : public KConfigSkeleton
 {
@@ -39,14 +42,14 @@ class KDEPIM_EXPORT KPimPrefs : public KConfigSkeleton
 
     /** Set preferences to default values */
     void usrSetDefaults();
-  
+
     /** Read preferences from config file */
     void usrReadConfig();
 
     /** Write preferences to config file */
     void usrWriteConfig();
 
-    /** 
+    /**
      * Get user's time zone.
      *
      * This will first look for whatever time zone is stored in KOrganizer's
@@ -59,12 +62,14 @@ class KDEPIM_EXPORT KPimPrefs : public KConfigSkeleton
 
   public:
     QStringList mCustomCategories;
-  
+
   protected:
     virtual void setCategoryDefaults() {}
-  
+
   public:
     static const QString categorySeparator;
 };
+
+}
 
 #endif
