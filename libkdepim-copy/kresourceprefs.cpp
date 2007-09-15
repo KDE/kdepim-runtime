12399855104ae14165947996ebe658cf7f824ac1
Allen Winter <winter@kde.org>
2007-09-15 16:28:25 +0000

    move into the KPIM namespace.
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=712905

--- libkdepim-copy/kresourceprefs.cpp
+++ libkdepim-copy/kresourceprefs.cpp
@@ -1,27 +1,28 @@
 /*
-    This file is part of libkdepim.
+  This file is part of libkdepim.
 
-    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
+#include "kresourceprefs.h"
 #include <QString>
 
-#include "kresourceprefs.h"
+using namespace KPIM;
 
 KResourcePrefs::KResourcePrefs( const QString &name )
   : KConfigSkeleton( name )
@@ -33,6 +34,7 @@ void KResourcePrefs::addGroupPrefix( const QString &prefix )
   KConfigSkeletonItem::List itemList = items();
   KConfigSkeletonItem::List::Iterator it;
 
-  for ( it = itemList.begin(); it != itemList.end(); ++it )
+  for ( it = itemList.begin(); it != itemList.end(); ++it ) {
     (*it)->setGroup( prefix + ':' + (*it)->group() );
+  }
 }
--- libkdepim-copy/kresourceprefs.h
+++ libkdepim-copy/kresourceprefs.h
@@ -1,31 +1,33 @@
 /*
-    This file is part of libkdepim.
+  This file is part of libkdepim.
 
-    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #ifndef KRESOURCEPREFS_H
 #define KRESOURCEPREFS_H
 
+#include "kdepim_export.h"
 #include <kconfigskeleton.h>
-#include <kdepim_export.h>
 
 class QString;
+
+namespace KPIM {
 
 /**
   This is a base class for all KPrefs objects, where multiple instances want
@@ -45,5 +47,7 @@ class KDEPIM_EXPORT KResourcePrefs : public KConfigSkeleton
      */
     void addGroupPrefix( const QString &prefix );
 };
+
+}
 
 #endif
