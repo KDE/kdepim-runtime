86c46f7be91030add144c4dcd6a2488bb5f09534
Brad Hards <bradh@frogmouth.net>
2007-12-14 23:15:47 +0000

    Add license, to help the GPLV3 zealots.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=748604

--- resources/openchange/lzfu.h
+++ resources/openchange/lzfu.h
@@ -1,3 +1,16 @@
+/*
+     Copyright (C) Brad Hards <bradh@frogmouth.net> 2007
+
+     This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+     You should have received a copy of the GNU General Public License
+     along with this program; if not, write to the Free Software Foundation,
+     Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
 #include <kdemacros.h>
 
 KDE_EXPORT QByteArray lzfuDecompress (const QByteArray &rtfcomp);
