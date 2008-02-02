a4fa9e621eb4d96b95f98b78cd2a25595c5161c4
Tobias Koenig <tokoe@kde.org>
2008-02-02 15:22:19 +0000

    Add nepomuk ontologies for contacts
    svn path=/trunk/KDE/kdepim/akonadi/; revision=769995

---
+++ nepomuk/nco.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Version=1.0
+Name=Nepomuk Contact Ontology
+Comment=Nepomuk Contact Ontology describes contact information, common in many places on the desktop.
+URL=http://www.semanticdesktop.org/ontologies/2007/03/22/nco#
+Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/nco.trig
+MimeType=application/x-trig
+Type=Data
