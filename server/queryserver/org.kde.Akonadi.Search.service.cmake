d268c4220d29c57cc6852daa0b9b21cb450b4e5a
Will Stephenson <wstephenson@kde.org>
2008-04-23 17:25:54 +0000

    Move Akonadi server and shared components to kdesupport.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=800213

--- server/queryserver/org.kde.Akonadi.Search.service.cmake
+++
@@ -1,3 +0,0 @@
-[D-BUS Service]
-Name=org.kde.Akonadi.Search
-Exec=${BIN_INSTALL_DIR}/akonadi_queryserver
--- server/src/storage/akonadi-mysql-client.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# connect to mysqld started by akonadi
-# useful for developing
-
-akonadisocket=$HOME/.local/share/akonadi/db_misc/mysql.socket
-mysql --socket=$akonadisocket akonadi
