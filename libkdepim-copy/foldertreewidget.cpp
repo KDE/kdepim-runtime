bb843fa0cdf3ae83fd65de7609547042fcbfe6a5
Szymon Stefanek <pragma@kvirc.net>
2008-04-28 01:46:37 +0000

    Added a preliminary native-Qt4-based FolderTreeWidget
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=801876

---
+++ libkdepim-copy/foldertreewidget.cpp
@@ -0,0 +1,297 @@
+/******************************************************************************
+ *
+ * This file is part of libkdepim.
+ *
+ * Copyright (C) 2008 Szymon Tomasz Stefanek <pragma@kvirc.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ *****************************************************************************/
+
+#include "foldertreewidget.h"
+
+#include <KMenu>
+#include <KConfig>
+#include <KConfigGroup>
+#include <KLocale>
+
+#include <QHeaderView>
+
+namespace KPIM {
+
+FolderTreeWidgetBase::FolderTreeWidgetBase( QWidget * parent, const char * name )
+: QTreeWidget( parent )
+{
+  setObjectName( name );
+  setColumnCount( 0 );
+
+  header()->setMovable( true );
+
+  setManualColumnHidingEnabled( true );
+}
+
+bool FolderTreeWidgetBase::saveLayout( KConfigGroup &group , const char * keyName )
+{
+  if( !keyName )
+    return false;
+
+  group.writeEntry( keyName , QVariant( header()->saveState() ) );
+
+  return true;
+}
+
+bool FolderTreeWidgetBase::saveLayout( KConfig * config , const char * groupName , const char * keyName )
+{
+  if( !config || !groupName || !keyName )
+    return false;
+
+  KConfigGroup group( config , groupName );
+  return saveLayout( group , keyName );
+}
+
+bool FolderTreeWidgetBase::restoreLayout( KConfigGroup &group , const char * keyName )
+{
+  if( !keyName )
+    return false;
+
+  // Restore the view column order, width and visibility
+  QByteArray state = group.readEntry(
+                             keyName , QVariant( QVariant::ByteArray )
+                         ).toByteArray();
+
+  if( state.isEmpty() )
+    return false;
+
+  // Qt writes also some options that can be set only
+  // programmatically. This can drive you crazy if you
+  // first setup the view and then restore the layout:
+  // it will overwrite your previous settings...
+
+  bool sectionsWereClickable = header()->isClickable();
+  bool sectionsWereMovable = header()->isMovable();
+  bool lastSectionWasStretched = header()->stretchLastSection();
+  bool sortIndicatorWasShown = header()->isSortIndicatorShown();
+  Qt::Alignment originalDefaultAlignment = header()->defaultAlignment();
+  int defaultSectionSize = header()->defaultSectionSize();
+  int minimumSectionSize = header()->minimumSectionSize();
+  bool cascadingSectionResizes = header()->cascadingSectionResizes();
+
+  if ( !header()->restoreState( state ) )
+    return false;
+
+
+  header()->setClickable( sectionsWereClickable );
+  header()->setMovable( sectionsWereMovable );
+  header()->setSortIndicatorShown( sortIndicatorWasShown );
+  header()->setDefaultAlignment( originalDefaultAlignment );
+  header()->setMinimumSectionSize( minimumSectionSize );
+  header()->setCascadingSectionResizes( cascadingSectionResizes );
+  // FIXME: This would cause the sections to be resized and thus
+  //        can't be reliably reset after the configuration
+  //        has been read. Can do nothing about that except warning
+  //        the user in the docs.
+  //header()->setDefaultSectionSize( defaultSectionSize );
+  //header()->setStretchLastSection( lastSectionWasStretched ); 
+
+  return true;
+}
+
+bool FolderTreeWidgetBase::restoreLayout( KConfig * config , const char * groupName , const char * keyName )
+{
+  if( !config || !groupName || !keyName )
+    return false;
+
+  if ( !config->hasGroup( groupName ) )
+    return false;
+
+  KConfigGroup group( config , groupName );
+  return restoreLayout( group , keyName );
+}
+
+void FolderTreeWidgetBase::setManualColumnHidingEnabled( bool enable )
+{
+  if( enable )
+  {
+     header()->setContextMenuPolicy( Qt::CustomContextMenu ); // make sure it's true
+     QObject::connect( header(), SIGNAL( customContextMenuRequested( const QPoint& ) ),
+                       this, SLOT( slotHeaderContextMenuRequested( const QPoint& ) ) );
+  } else {
+     if ( mEnableManualColumnHiding )
+         QObject::disconnect( header(), SIGNAL( customContextMenuRequested( const QPoint& ) ),
+                              this, SLOT( slotHeaderContextMenuRequested( const QPoint& ) ) );
+  }
+
+  mEnableManualColumnHiding = enable;
+}
+
+void FolderTreeWidgetBase::setColumnHidden( int logicalIndex, bool hide )
+{
+  header()->setSectionHidden( logicalIndex , hide );
+}
+
+bool FolderTreeWidgetBase::isColumnHidden( int logicalIndex ) const
+{
+  return header()->isSectionHidden( logicalIndex );
+}
+
+#define KFOLDERTREEWIDGETBASE_COLUMN_ACTION_ID_BASE 0x10000
+
+bool FolderTreeWidgetBase::fillHeaderContextMenu( KMenu * menu , const QPoint & )
+{
+  if ( !menu || !mEnableManualColumnHiding )
+    return false;
+
+  menu->addTitle( i18n("View Columns") );
+
+  QTreeWidgetItem * hitem = headerItem();
+  if ( !hitem )
+    return false; // oops..
+
+  // Dynamically build the menu and check the items for visible columns
+  int cc = hitem->columnCount();
+  if ( cc < 1 )
+    return false;
+
+  for ( int i = 1 ; i < cc; i++ )
+  {
+    QAction * act = menu->addAction( hitem->text( i ) );
+    act->setCheckable( true );
+    if ( !header()->isSectionHidden( i ) )
+        act->setChecked( true );
+    act->setData( QVariant( KFOLDERTREEWIDGETBASE_COLUMN_ACTION_ID_BASE + i ) );
+  }
+
+  return true;
+}
+
+void FolderTreeWidgetBase::slotToggleColumnActionTriggered( QAction *act )
+{
+  if ( !act )
+    return;
+
+  QVariant data = act->data();
+
+  bool ok;
+  int id = data.toInt( &ok );
+  if ( !ok )
+    return;
+
+  if ( id <= KFOLDERTREEWIDGETBASE_COLUMN_ACTION_ID_BASE )
+    return;
+
+  id -= KFOLDERTREEWIDGETBASE_COLUMN_ACTION_ID_BASE;
+
+  if ( id > columnCount() )
+    return;
+
+  header()->setSectionHidden( id , !act->isChecked() );
+}
+
+void FolderTreeWidgetBase::slotHeaderContextMenuRequested( const QPoint &clickPoint )
+{
+  KMenu menu( this );
+
+  if ( !fillHeaderContextMenu( &menu , clickPoint ) )
+    return;
+
+  connect( &menu , SIGNAL( triggered( QAction* ) ) ,
+           this , SLOT( slotToggleColumnActionTriggered( QAction* ) ) );
+
+  menu.exec( header()->mapToGlobal( clickPoint ) );
+}
+
+int FolderTreeWidgetBase::addColumn( const QString &label )
+{
+  QTreeWidgetItem * hitem = headerItem();
+  if ( !hitem )
+  {
+    // In fact this seems to never happen
+    hitem = new QTreeWidgetItem( this );
+    hitem->setText( 0 , label );
+    setHeaderItem( hitem ); // will set column count to 1
+    return 0;
+  }
+
+  int cc = columnCount();
+
+  setColumnCount( cc + 1 );
+  hitem->setText( cc , label );
+  return cc;
+}
+
+bool FolderTreeWidgetBase::setColumnText( int columnIndex , const QString &label )
+{
+  if ( columnIndex >= columnCount() )
+    return false;
+  QTreeWidgetItem * hitem = headerItem();
+  if ( !hitem )
+    return false;
+  hitem->setText( columnIndex , label );
+  return true;
+}
+
+
+
+
+
+
+FolderTreeWidget::FolderTreeWidget( QWidget *parent , const char *name )
+: FolderTreeWidgetBase( parent , name )
+{
+  setAlternatingRowColors( true );
+  setAcceptDrops( true );
+  setAllColumnsShowFocus( true );
+  setRootIsDecorated( true );
+  setSortingEnabled( true );
+  header()->setSortIndicatorShown( true );
+  header()->setClickable( true );
+  //setSelectionMode( Extended );
+}
+
+
+
+
+FolderTreeWidgetItem::FolderTreeWidgetItem(
+        FolderTreeWidget *parent,
+        const QString &label,
+        Protocol protocol,
+        FolderType folderType
+    ) : QTreeWidgetItem( parent ), mProtocol(protocol), mFolderType(folderType)
+{
+}
+
+FolderTreeWidgetItem::FolderTreeWidgetItem(
+        FolderTreeWidgetItem *parent,
+        const QString &label,
+        Protocol protocol,
+        FolderType folderType,
+        int unread,
+        int total
+    ) : QTreeWidgetItem( parent ), mProtocol(protocol), mFolderType(folderType)
+{
+}
+
+bool FolderTreeWidgetItem::operator < ( const QTreeWidgetItem &other ) const
+{
+  int sortCol = treeWidget()->sortColumn();
+  if ( sortCol < 0 )
+     return true; // just "yes" :D
+
+  // WIP
+
+  return text(sortCol) < other.text(sortCol);
+}
+
+};
---
+++ libkdepim-copy/foldertreewidget.h
@@ -0,0 +1,358 @@
+#ifndef __FOLDERTREEWIDGET_H__
+#define __FOLDERTREEWIDGET_H__
+
+/******************************************************************************
+ *
+ * This file is part of libkdepim.
+ *
+ * Copyright (C) 2008 Szymon Tomasz Stefanek <pragma@kvirc.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ *****************************************************************************/
+
+/**
+ * @file kfoldertreewidget.h A basic implementation of an UI for a tree of folders
+ */
+
+#include "kdepim_export.h"
+
+#include <QTreeWidget>
+
+class KMenu;
+class KConfig;
+class KConfigGroup;
+
+class QAction;
+
+
+namespace KPIM
+{
+
+#define FTWB_DEFAULT_CONFIG_KEY "TreeWidgetLayout"
+
+/**
+ * @brief A QTreeWidget with expanded capabilities
+ * 
+ * This class extends the functionality provided by the standard QTreeWidget
+ * by adding toggleable columns and a layout save/restore facility.
+ *
+ * @author: Szymon Tomasz Stefanek <pragma@kvirc.net>
+ */
+class KDEPIM_EXPORT FolderTreeWidgetBase : public QTreeWidget
+{
+  Q_OBJECT
+public:
+  /**
+   * Constructs a FolderTreeWidgetBase. Sets up the default values
+   * for properties.
+   */
+  explicit FolderTreeWidgetBase( QWidget *parent, const char *name = 0 );
+
+private:
+  bool mEnableManualColumnHiding; //< Is manual column hiding currently enabled ?
+
+public:
+
+  //
+  // Layout Save/Restore facility
+  //
+
+  /**
+   * Stores the layout of this tree view to the specified KConfigGroup.
+   * The stored data includes visible columns, column width and order.
+   *
+   * @param group The KConfigGroup to write the layout data to.
+   * @param keyName The key to use, "TreeWidgetLayout" by default
+   *
+   * @returns true on success and false on failure (if keyName is 0, for example).
+   */
+  bool saveLayout(
+      KConfigGroup &group,
+      const char *keyName = FTWB_DEFAULT_CONFIG_KEY
+    );
+
+  /**
+   * Stores the layout of this tree view to the specified key of 
+   * the specified config group.
+   * The stored data includes visible columns, column width and order.
+   * 
+   * @param configGroup The KConfig group to write the layout data to.
+   * @param keyName The key to use, "State" by default
+   *
+   * @returns true on success and false on failure (if you can't write
+   * to the configGroup).
+   */
+  bool saveLayout(
+      KConfig *config,
+      const char *groupName,
+      const char *keyName = FTWB_DEFAULT_CONFIG_KEY
+    );
+
+  /**
+   * Attempts to restore the layout of this tree from the specified
+   * key of the specified KConfigGroup. The layout must have been
+   * previously saved by a call to saveLayout().
+   *
+   * Please note that you must call this function after having added
+   * all the relevant columns to the view. On the other hand if you're
+   * setting some of the view properties programmatically (like last section
+   * stretching) then you should set them after restoring the layout
+   * because otherwise they might get overwritten by the stored data.
+   * The proper order is: add columns, restore layout, set header properties.
+   *
+   * @param group The KConfigGroup to read the layout data from.
+   * @param keyName The key to use, "State" by default
+   *
+   * @returns true if the layout data has been succesfully read and
+   *          restored. Returns false if the specified key of the config
+   *          group is empty, or the data contained inside is not valid.
+   */
+  bool restoreLayout(
+      KConfigGroup &group,
+      const char *keyName = FTWB_DEFAULT_CONFIG_KEY            
+    );
+
+  /**
+   * Attempts to restore the layout of this tree from the specified
+   * key of the specified group. The layout must have been
+   * previously saved by a call to saveLayout().
+   *
+   * Please note that you must call this function after having added
+   * all the relevant columns to the view. On the other hand if you're
+   * setting some of the view properties programmatically (like last section
+   * stretching) then you should set them after restoring the layout
+   * because otherwise they might get overwritten by the stored data.
+   * The proper order is: add columns, restore layout, set header properties.
+   *
+   * @param configGroup The name of the KConfig group to read the layout data from.
+   * @param keyName The key to use, "State" by default
+   *
+   * @returns true if the layout data has been succesfully read and
+   *          restored. Returns false if the specified key of the config
+   *          group is empty, or the data contained inside is not valid.
+   */
+  bool restoreLayout( KConfig *config, const char *groupName, const char *keyName = FTWB_DEFAULT_CONFIG_KEY );
+
+  //
+  // Manual and programmatic column hiding
+  //
+
+  /**
+   * Enables or disables manual column hiding by the means of a contextual menu.
+   * All columns but the first can be hidden by the user by toggling the
+   * check on the popup menu that is displayed by right clicking the header
+   * of this view.
+   *
+   * By default column hiding is enabled. 
+   */
+  void setManualColumnHidingEnabled( bool enable );
+
+  /**
+   * @returns true if manual column hiding is currently enabled, false otherwise.
+   */
+  bool manualColumnHidingEnabled() const
+    { return mEnableManualColumnHiding; };
+
+  /**
+   * Hides or shows the column specified by logicalIndex.
+   * The column is hidden if hide is true and shown otherwise.
+   */
+  void setColumnHidden( int logicalIndex, bool hide );
+
+  /**
+   * Returns true if the column specified by logicalIndex is actually hidden.
+   */
+  bool isColumnHidden( int logicalIndex ) const;
+
+  /**
+   * Called by this class to fill the contextual menu shown when the
+   * user right clicks on one of the header columns and column hiding
+   * is enabled.
+   *
+   * If you override this, remember to call the base class implementation
+   * if you want to keep column hiding working.
+   *
+   * You should return true if you want the menu to be shown or false
+   * if you don't want it to be shown. The default implementation returns
+   * true unless you have no columns at all, column hiding is disabled
+   * or you pass some weird parameter.
+   */
+  virtual bool fillHeaderContextMenu( KMenu * menu , const QPoint &clickPoint );
+
+  //
+  // Facilities for adding and manipulating columns
+  //
+
+  /**
+   * Convenience function that adds a column to this tree widget
+   * and returns its logical index. Please note that in fact
+   * this function manipulates the header item which in turn
+   * plays with the underlying data model.
+   *
+   * Anyway.. this is what you want to use 99,9% of the times :)
+   */
+  int addColumn( const QString &label );
+
+  /**
+   * Convenience function that changes the text of the specified column.
+   * Returns true if the text can be succesfully changed or false
+   * if the specified column index is out of range.
+   */
+  bool setColumnText( int columnIndex , const QString &label );
+
+protected Q_SLOTS:
+  /**
+   * Internal slot connected to the customContextMenuRequested()
+   * signal of the header().
+   */
+  void slotHeaderContextMenuRequested( const QPoint &clickPoint );
+
+  /**
+   * Internal slot connected to the show/hide actions in the
+   * header contextual menu.
+   */
+  void slotToggleColumnActionTriggered( QAction *act );
+
+};
+
+
+/**
+ * @brief A tree widget useful for displaying a tree of folders containing messages
+ *
+ *
+ * WIP Warning:
+ * 
+ * This widget is meant to be a Native-Qt4 polished replacement for
+ * KDEPIM::KFolderTreeWidget which is based on K3ListView.
+ * Necessary features are being added as existing clients are ported.
+ * This approach is taken in order to isolate features that are no
+ * longer necessary and can be dropped from the new implementation. Take care :)
+ *
+ * @author: Szymon Tomasz Stefanek <pragma@kvirc.net>
+ */
+class KDEPIM_EXPORT FolderTreeWidget : public FolderTreeWidgetBase
+{
+  Q_OBJECT
+public:
+  /**
+   * Constructs a FolderTreeWidget instance.
+   */
+  explicit FolderTreeWidget( QWidget *parent , const char *name = 0 );
+
+};
+
+
+/**
+ * @brief A folder tree node to be used with FolderTreeWidget
+ *
+ * @author: Szymon Tomasz Stefanek <pragma@kvirc.net>
+ */
+class KDEPIM_EXPORT FolderTreeWidgetItem : public QTreeWidgetItem
+{
+public:
+  /**
+   * Protocol information associated to the item.
+   */
+  enum Protocol {
+      Imap,
+      Local,
+      News,
+      CachedImap,
+      Search,
+      NONE
+  };
+
+  /**
+   * Folder type information
+   */
+  enum FolderType {
+      Inbox,
+      Outbox,
+      SentMail,
+      Trash,
+      Drafts,
+      Templates,
+      Root,
+      Calendar,
+      Tasks,
+      Journals,
+      Contacts,
+      Notes,
+      Other
+  };
+
+private:
+  Protocol mProtocol;         //< The protocol associated to the folder
+  FolderType mFolderType;     //< The type of the folder
+
+public:
+  /**
+   * Constructs a root-item
+   */
+  explicit FolderTreeWidgetItem(
+      FolderTreeWidget *parent,
+      const QString &label = QString(),
+      Protocol protocol = NONE,
+      FolderType folderType = Root
+    );
+
+  /**
+   * Constructs a child-item
+   */
+  explicit FolderTreeWidgetItem(
+      FolderTreeWidgetItem *parent,
+      const QString &label = QString(),
+      Protocol protocol = NONE,
+      FolderType folderType = Other,
+      int unread = 0,
+      int total = 0
+   );
+
+public:
+  /**
+   * Returns the protocol associated to the folder item
+   */
+  Protocol protocol() const
+    { return mProtocol; };
+
+  /**
+   * Sets the protocol associated to the folder item
+   */
+  void setProtocol( Protocol protocol )
+    { mProtocol = protocol; };
+
+  /**
+   * Returns the type of the folder
+   */
+  FolderType folderType() const
+    { return mFolderType; };
+
+  /**
+   * Sets the type of the folder
+   */
+  void setFolderType( FolderType folderType )
+    { mFolderType = folderType; };
+
+  /**
+   * Operator used for item sorting.
+   */
+  virtual bool operator < ( const QTreeWidgetItem &other ) const;
+
+};
+
+}; // namespace KPIM
+
+#endif //!__FOLDERTREEWIDGET_H__
