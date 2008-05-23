b62be90bcf26eb3a30d880f4ff47bef0b8ac41fc
Allen Winter <winter@kde.org>
2008-05-23 21:09:15 +0000

    As this code was licensed as "GPL", we insert our standard header for this license, as is our policy. CCMAIL: sanders@kde.org
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=811854

--- libkdepim-copy/broadcaststatus.cpp
+++ libkdepim-copy/broadcaststatus.cpp
@@ -1,13 +1,25 @@
 /*
   broadcaststatus.cpp
 
-  This file is part of KDEPIM.
+  This file is part of libkdepim.
 
   Author: Don Sanders <sanders@kde.org>
 
   Copyright (C) 2000 Don Sanders <sanders@kde.org>
 
-  License GPL
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #include "broadcaststatus.h"
@@ -19,16 +31,16 @@
 
 #include <QTime>
 
-KPIM::BroadcastStatus* KPIM::BroadcastStatus::instance_ = 0;
+KPIM::BroadcastStatus *KPIM::BroadcastStatus::instance_ = 0;
 static K3StaticDeleter<KPIM::BroadcastStatus> broadcastStatusDeleter;
 
 namespace KPIM {
 
-BroadcastStatus* BroadcastStatus::instance()
+BroadcastStatus *BroadcastStatus::instance()
 {
-  if (!instance_)
+  if ( !instance_ ) {
     broadcastStatusDeleter.setObject( instance_, new BroadcastStatus() );
-
+  }
   return instance_;
 }
 
@@ -42,108 +54,112 @@ BroadcastStatus::~BroadcastStatus()
   instance_ = 0;
 }
 
-void BroadcastStatus::setStatusMsg( const QString& message )
+void BroadcastStatus::setStatusMsg( const QString &message )
 {
   mStatusMsg = message;
-  if( !mTransientActive )
+  if( !mTransientActive ) {
     emit statusMsg( message );
+  }
 }
 
-void BroadcastStatus::setStatusMsgWithTimestamp( const QString& message )
+void BroadcastStatus::setStatusMsgWithTimestamp( const QString &message )
 {
-  KLocale* locale = KGlobal::locale();
-  setStatusMsg( i18nc( "%1 is a time, %2 is a status message", "[%1] %2" ,
-                  locale->formatTime( QTime::currentTime(),
-                                          true /* with seconds */ ) ,
-                  message ) );
+  KLocale *locale = KGlobal::locale();
+  setStatusMsg( i18nc( "%1 is a time, %2 is a status message", "[%1] %2",
+                       locale->formatTime( QTime::currentTime(), true ), message ) );
 }
 
 void BroadcastStatus::setStatusMsgTransmissionCompleted( int numMessages,
-                                                           int numBytes,
-                                                           int numBytesRead,
-                                                           int numBytesToRead,
-                                                           bool mLeaveOnServer,
-                                                           KPIM::ProgressItem* item )
+                                                         int numBytes,
+                                                         int numBytesRead,
+                                                         int numBytesToRead,
+                                                         bool mLeaveOnServer,
+                                                         KPIM::ProgressItem *item )
 {
   QString statusMsg;
-  if( numMessages > 0 ) {
-    if( numBytes != -1 ) {
-      if( ( numBytesToRead != numBytes ) && mLeaveOnServer )
+  if ( numMessages > 0 ) {
+    if ( numBytes != -1 ) {
+      if ( ( numBytesToRead != numBytes ) && mLeaveOnServer ) {
         statusMsg = i18np( "Transmission complete. %1 new message in %2 KB "
-                          "(%3 KB remaining on the server).",
-                          "Transmission complete. %1 new messages in %2 KB "
-                          "(%3 KB remaining on the server).",
-                          numMessages ,
-                      numBytesRead / 1024 ,
-                      numBytes / 1024 );
-      else
+                           "(%3 KB remaining on the server).",
+                           "Transmission complete. %1 new messages in %2 KB "
+                           "(%3 KB remaining on the server).",
+                           numMessages,
+                           numBytesRead / 1024,
+                           numBytes / 1024 );
+      } else {
         statusMsg = i18np( "Transmission complete. %1 message in %2 KB.",
-                         "Transmission complete. %1 messages in %2 KB.",
-                          numMessages ,
-                      numBytesRead / 1024 );
-    }
-    else
+                           "Transmission complete. %1 messages in %2 KB.",
+                           numMessages,
+                           numBytesRead / 1024 );
+      }
+    } else {
       statusMsg = i18np( "Transmission complete. %1 new message.",
-                        "Transmission complete. %1 new messages.",
-                        numMessages );
-  }
-  else
+                         "Transmission complete. %1 new messages.",
+                         numMessages );
+    }
+  } else {
     statusMsg = i18n( "Transmission complete. No new messages." );
+  }
 
   setStatusMsgWithTimestamp( statusMsg );
-  if ( item )
+
+  if ( item ) {
     item->setStatus( statusMsg );
+  }
 }
 
-void BroadcastStatus::setStatusMsgTransmissionCompleted( const QString& account,
-                                                           int numMessages,
-                                                           int numBytes,
-                                                           int numBytesRead,
-                                                           int numBytesToRead,
-                                                           bool mLeaveOnServer,
-                                                           KPIM::ProgressItem* item )
+void BroadcastStatus::setStatusMsgTransmissionCompleted( const QString &account,
+                                                         int numMessages,
+                                                         int numBytes,
+                                                         int numBytesRead,
+                                                         int numBytesToRead,
+                                                         bool mLeaveOnServer,
+                                                         KPIM::ProgressItem *item )
 {
   QString statusMsg;
-  if( numMessages > 0 ) {
-    if( numBytes != -1 ) {
-      if( ( numBytesToRead != numBytes ) && mLeaveOnServer )
+  if ( numMessages > 0 ) {
+    if ( numBytes != -1 ) {
+      if ( ( numBytesToRead != numBytes ) && mLeaveOnServer ) {
         statusMsg = i18np( "Transmission for account %4 complete. "
-                          "%1 new message in %2 KB "
-                          "(%3 KB remaining on the server).",
-                          "Transmission for account %4 complete. "
-                          "%1 new messages in %2 KB "
-                          "(%3 KB remaining on the server).",
-                          numMessages ,
-                      numBytesRead / 1024 ,
-                      numBytes / 1024 ,
+                           "%1 new message in %2 KB "
+                           "(%3 KB remaining on the server).",
+                           "Transmission for account %4 complete. "
+                           "%1 new messages in %2 KB "
+                           "(%3 KB remaining on the server).",
+                           numMessages,
+                      numBytesRead / 1024,
+                      numBytes / 1024,
                       account );
-      else
+      } else {
         statusMsg = i18np( "Transmission for account %3 complete. "
-                          "%1 message in %2 KB.",
-                          "Transmission for account %3 complete. "
-                          "%1 messages in %2 KB.",
-                          numMessages ,
-                      numBytesRead / 1024 ,
-                      account );
-    }
-    else
+                           "%1 message in %2 KB.",
+                           "Transmission for account %3 complete. "
+                           "%1 messages in %2 KB.",
+                           numMessages,
+                           numBytesRead / 1024,
+                           account );
+      }
+    } else {
       statusMsg = i18np( "Transmission for account %2 complete. "
-                        "%1 new message.",
-                        "Transmission for account %2 complete. "
-                        "%1 new messages.",
-                        numMessages ,
-                    account );
+                         "%1 new message.",
+                         "Transmission for account %2 complete. "
+                         "%1 new messages.",
+                         numMessages,
+                         account );
+    }
+  } else {
+    statusMsg = i18n( "Transmission for account %1 complete. No new messages.", account );
   }
-  else
-    statusMsg = i18n( "Transmission for account %1 complete. No new messages.",
-                  account );
 
   setStatusMsgWithTimestamp( statusMsg );
-  if ( item )
+
+  if ( item ) {
     item->setStatus( statusMsg );
+  }
 }
 
-void BroadcastStatus::setTransientStatusMsg( const QString& msg )
+void BroadcastStatus::setTransientStatusMsg( const QString &msg )
 {
   mTransientActive = true;
   emit statusMsg( msg );
