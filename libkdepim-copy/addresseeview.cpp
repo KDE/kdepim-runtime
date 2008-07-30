3068779ec97e4ca71f7d6010a1a1a57160d6ddbc
Tobias Koenig <tokoe@kde.org>
2008-07-30 10:15:23 +0000

    Show IM Address only when IM Address selected
    BUG: 167657
    
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=839547

--- libkdepim-copy/addresseeview.cpp
+++ libkdepim-copy/addresseeview.cpp
@@ -360,8 +360,8 @@ QString AddresseeView::vCardAsHTML( const KABC::Addressee& addr, ::KIMProxy*, Li
           QString key = customEntry.left( pos );
           const QString value = customEntry.mid( pos + 1 );
 
-          // blog is handled separated
-          if ( key == "BlogFeed" )
+          // blog and im address are handled separated
+          if ( key == "BlogFeed" || key == "IMAddress" )
             continue;
 
           const QMap<QString, QString>::ConstIterator keyIt = titleMap.find( key );
@@ -377,29 +377,39 @@ QString AddresseeView::vCardAsHTML( const KABC::Addressee& addr, ::KIMProxy*, Li
   QString name( Qt::escape( addr.realName() ) );
   QString role( Qt::escape( addr.role() ) );
   QString organization( Qt::escape( addr.organization() ) );
-/*
-  if ( proxy && (fieldMask & IMFields) ) {
-    if ( proxy->isPresent( addr.uid() ) && proxy->presenceNumeric( addr.uid() ) > 0 ) {
-      // set image source to either a QMimeSourceFactory key or a data:/ URL
-      QString imgSrc = pixmapAsDataUrl( proxy->presenceIcon( addr.uid() ) );
 
-      // make the status a link, if required
-      QString imStatus;
-      if ( linkMask & IMLinks )
-        imStatus = QString::fromLatin1( "<a href=\"im:\"><img src=\"%1\"> (%2)</a>" );
-      else
-        imStatus = QString::fromLatin1( "<img src=\"%1\"> (%2)" );
+  if ( fieldMask & IMFields ) {
 
-      // append our status to the rest of the dynamic part of the addressee
-      dynamicPart += rowFmtStr
-              .arg( i18n( "Presence" ) )
-              .arg( imStatus
-                        .arg( imgSrc )
-                        .arg( proxy->presenceString( addr.uid() ) )
-                  );
+    const QString imAddress = addr.custom( "KADDRESSBOOK", "X-IMAddress" );
+    if ( !imAddress.isEmpty() ) {
+      customData += rowFmtStr.arg( i18n( "IM Address" ) ).arg( Qt::escape( imAddress ) ) ;
     }
-  }
+
+/*
+    if ( proxy ) {
+      if ( proxy->isPresent( addr.uid() ) && proxy->presenceNumeric( addr.uid() ) > 0 ) {
+        // set image source to either a QMimeSourceFactory key or a data:/ URL
+        QString imgSrc = pixmapAsDataUrl( proxy->presenceIcon( addr.uid() ) );
+
+        // make the status a link, if required
+        QString imStatus;
+        if ( linkMask & IMLinks )
+          imStatus = QString::fromLatin1( "<a href=\"im:\"><img src=\"%1\"> (%2)</a>" );
+        else
+          imStatus = QString::fromLatin1( "<img src=\"%1\"> (%2)" );
+
+        // append our status to the rest of the dynamic part of the addressee
+        dynamicPart += rowFmtStr
+                .arg( i18n( "Presence" ) )
+                .arg( imStatus
+                          .arg( imgSrc )
+                          .arg( proxy->presenceString( addr.uid() ) )
+                    );
+      }
+    }
 */
+  }
+
   // @STYLE@ - construct the string by parts, substituting in
   // the styles first. There are lots of appends, but we need to
   // do it this way to avoid cases where the substituted string
@@ -767,7 +777,7 @@ QString AddresseeView::strippedNumber( const QString &number )
 
   for ( int i = 0; i < number.length(); ++i ) {
     QChar c = number[ i ];
-    if ( c.isDigit() || c == '*' || c == '#' || c == '+' && i == 0 )
+    if ( c.isDigit() || c == '*' || c == '#' || (c == '+' && i == 0) )
       retval.append( c );
   }
 
