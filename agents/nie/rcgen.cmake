7c85798216c05f8378b570b7ea305b9afc8b7084
Harald Fernengel <harry@kdevelop.org>
2008-08-09 16:17:25 +0000

    INTEGRATION:/branches/KDE/4.1/kdepim/akonadi/agents/nie 844405 fix the nepomuk-rcgen detection if kdepim is not installed into KDEDIR. Done with I-know-cmake Alex
    svn path=/trunk/KDE/kdepim/akonadi/agents/; revision=844408

--- agents/nie/rcgen.cmake
+++ agents/nie/rcgen.cmake
@@ -1,54 +1,45 @@
 # a CMake script. envoke with the -P option
 
 # must define
-# BIN_INSTALL_DIR
 # targetdir
 # ontofile1, ontofile2, ontofile3, ontofile4
+# _rcgen
 
-FIND_PROGRAM(_rcgen nepomuk-rcgen PATHS ${BIN_INSTALL_DIR} NO_DEFAULT_PATH)
+file(TO_NATIVE_PATH ${_rcgen} _rcgen)
 
-if(NOT _rcgen)
+execute_process(
+  COMMAND ${_rcgen} --listheaders --prefix ${targetdir}/ --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
+  OUTPUT_VARIABLE _out_headers
+  RESULT_VARIABLE _rcgen_result
+  ERROR_QUIET
+  )
+file(WRITE ${targetdir}/out_headers "${_out_headers}")
 
-  message(FATAL_ERROR "Failed to find the Nepomuk source generator. Did you install Soprano?" )
-
-else(NOT _rcgen)
-
-  FILE(TO_NATIVE_PATH ${_rcgen} _rcgen)
+# If the first call succeeds it is very very likely that the rest will, too
+if(${_rcgen_result} EQUAL 0)
 
   execute_process(
-    COMMAND ${_rcgen} --listheaders --prefix ${targetdir}/ --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
-    OUTPUT_VARIABLE _out_headers
-    RESULT_VARIABLE _rcgen_result
+    COMMAND ${_rcgen} --listsources --prefix ${targetdir}/ --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
+    OUTPUT_VARIABLE _out_sources
     ERROR_QUIET
     )
-  file(WRITE ${targetdir}/out_headers "${_out_headers}")
+  file(WRITE ${targetdir}/out_sources "${_out_sources}")
 
-  # If the first call succeeds it is very very likely that the rest will, too
-  if(${_rcgen_result} EQUAL 0)
+  execute_process(
+    COMMAND ${_rcgen} --listincludes --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
+    OUTPUT_VARIABLE _out_includes
+    ERROR_QUIET
+    )
+  file(WRITE ${targetdir}/out_includes "${_out_includes}")
 
-    execute_process(
-      COMMAND ${_rcgen} --listsources --prefix ${targetdir}/ --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
-      OUTPUT_VARIABLE _out_sources
-      ERROR_QUIET
-      )
-    file(WRITE ${targetdir}/out_sources "${_out_sources}")
+  execute_process(
+    COMMAND ${_rcgen} --writeall --templates "DUMMY" --target ${targetdir}/ --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
+    ERROR_QUIET
+    )
 
-    execute_process(
-      COMMAND ${_rcgen} --listincludes --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
-      OUTPUT_VARIABLE _out_includes
-      ERROR_QUIET
-      )
-    file(WRITE ${targetdir}/out_includes "${_out_includes}")
+else(${_rcgen_result} EQUAL 0)
 
-    execute_process(
-      COMMAND ${_rcgen} --writeall --templates "DUMMY" --target ${targetdir}/ --ontologies ${ontofile1} ${ontofile2} ${ontofile3} ${ontofile4}
-      ERROR_QUIET
-      )
+  message(FATAL_ERROR "Failed to generate Nepomuk resource classes. Do you have a recent Soprano version?")
 
-  else(${_rcgen_result} EQUAL 0)
+endif(${_rcgen_result} EQUAL 0)
 
-    message(FATAL_ERROR "Failed to generate Nepomuk resource classes. Do you have a recent Soprano version?")
-
-  endif(${_rcgen_result} EQUAL 0)
-
-endif(NOT _rcgen)
