4839922b4a5cc10320ad4d31b87082a9b80d9e4e
Sebastian Trueg <sebastian@trueg.de>
2008-10-08 20:51:43 +0000

    - Always perform an initial scan to make sure we never miss any addresses - Add the name as a label to each resource to have a proper value in Nepomuk::Resouce::genericLabel (we have no inference yet) - Added a simple DBus interface to forcefully update all addresses
    svn path=/trunk/KDE/kdepim/akonadi/agents/; revision=869367

--- agents/nepomuk_contact_feeder/nepomukcontactfeeder.cpp
+++ agents/nepomuk_contact_feeder/nepomukcontactfeeder.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (c) 2007 Tobias Koenig <tokoe@kde.org>
+                  2008 Sebastian Trueg <trueg@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
@@ -24,10 +25,15 @@
 #include <akonadi/changerecorder.h>
 #include <akonadi/item.h>
 #include <akonadi/itemfetchscope.h>
+#include <akonadi/itemfetchjob.h>
+#include <akonadi/collectionfetchjob.h>
 
 #include <nepomuk/resource.h>
+#include <nepomuk/resourcemanager.h>
 #include <nepomuk/variant.h>
 #include <kurl.h>
+
+#include <Soprano/Model>
 
 // ontology includes
 #include "bbsnumber.h"
@@ -45,14 +51,27 @@
 #include "videotelephonenumber.h"
 #include "voicephonenumber.h"
 
-using namespace Akonadi;
+#include <QtCore/QTimer>
+#include <QtDBus/QDBusConnection>
+
+#include <KDebug>
+
+
+namespace Akonadi {
 
 NepomukContactFeeder::NepomukContactFeeder( const QString &id )
-  : AgentBase( id )
+  : AgentBase( id ),
+    mForceUpdate( false )
 {
   changeRecorder()->itemFetchScope().fetchFullPayload();
   changeRecorder()->setMimeTypeMonitored( "text/directory" );
   changeRecorder()->setChangeRecordingEnabled( false );
+
+  // do the initial scan to make sure all items have been fed to nepomuk
+  QTimer::singleShot( 0, this, SLOT( slotInitialItemScan() ) );
+
+  // The line below is not necessary anymore once AgentBase also exports scriptable slots
+  QDBusConnection::sessionBus().registerObject( "/nepomukcontactfeeder", this, QDBusConnection::ExportScriptableSlots );
 }
 
 void NepomukContactFeeder::itemAdded( const Akonadi::Item &item, const Akonadi::Collection& )
@@ -62,12 +81,95 @@ void NepomukContactFeeder::itemAdded( const Akonadi::Item &item, const Akonadi::
 
 void NepomukContactFeeder::itemChanged( const Akonadi::Item &item, const QSet<QByteArray>& )
 {
-  if ( !item.hasPayload<KABC::Addressee>() )
-    return;
+  if ( item.hasPayload<KABC::Addressee>() )
+    updateItem( item );
+}
 
+void NepomukContactFeeder::itemRemoved( const Akonadi::Item &item )
+{
+  Nepomuk::PersonContact contact( item.url() );
+
+  QList<Nepomuk::PhoneNumber> numbers = contact.phoneNumbers();
+  Q_FOREACH( Nepomuk::PhoneNumber number, numbers ) {
+    number.remove();
+  }
+
+  QList<Nepomuk::EmailAddress> emails = contact.emailAddresses();
+  Q_FOREACH( Nepomuk::EmailAddress email, emails ) {
+    email.remove();
+  }
+
+  QList<Nepomuk::PostalAddress> addresses = contact.postalAddresses();
+  Q_FOREACH( Nepomuk::PostalAddress address, addresses ) {
+    address.remove();
+  }
+
+  contact.remove();
+}
+
+void NepomukContactFeeder::slotInitialItemScan()
+{
+  kDebug();
+  updateAll( false );
+}
+
+void NepomukContactFeeder::updateAll( bool force )
+{
+  mForceUpdate = force;
+
+  CollectionFetchJob *collectionFetch = new CollectionFetchJob( Collection::root(), CollectionFetchJob::Recursive );
+  if ( collectionFetch->exec() ) {
+    Collection::List collections = collectionFetch->collections();
+
+    foreach( const Collection &collection, collections) {
+      kDebug() << "checking collection" << collection.name();
+      if ( collection.contentMimeTypes().contains( QLatin1String( "text/directory" ) ) ) {
+        kDebug() << "fetching items from collection" << collection.name();
+        ItemFetchJob *itemFetch = new ItemFetchJob( collection );
+        itemFetch->fetchScope().fetchFullPayload();
+        connect( itemFetch, SIGNAL( itemsReceived( Akonadi::Item::List ) ),
+                 this, SLOT( slotItemsReceivedForInitialScan( Akonadi::Item::List ) ) );
+      }
+    }
+  }
+}
+
+void NepomukContactFeeder::slotItemsReceivedForInitialScan( const Akonadi::Item::List& items )
+{
+  kDebug() << items.count();
+  foreach( const Item &item, items ) {
+    // only update the item if it does not exist
+    if ( mForceUpdate ||
+         !Nepomuk::ResourceManager::instance()->mainModel()->containsAnyStatement( item.url(), Soprano::Node(), Soprano::Node() ) ) {
+      // FIXME: the idea is that we only fetch the full payload for items that need updating
+      //        However, the code below always returns an empty list!
+//       ItemFetchJob *itemFetch = new ItemFetchJob( item );
+//       itemFetch->fetchScope().fetchFullPayload();
+//       if ( itemFetch->exec() ) {
+//         Akonadi::Item::List fullItems = itemFetch->items();
+//         if ( fullItems.isEmpty() ) {
+//           kDebug() << "Failed to get full payload for item" << item.url();
+//         }
+//         else {
+//           updateItem( itemFetch->items().first() );
+//         }
+//       }
+      updateItem( item );
+    }
+  }
+  kDebug() << "done";
+}
+
+void NepomukContactFeeder::updateItem( const Akonadi::Item &item )
+{
   const KABC::Addressee addressee = item.payload<KABC::Addressee>();
 
   Nepomuk::PersonContact contact( item.url() );
+
+  // Nepomuk has no inference yet, thus having a label makes things easier
+  // when displaying resources generically, since it will be returned by
+  // Nepomuk::Resource::genericLabel()
+  contact.setLabel( addressee.name() );
 
   contact.setNameGivens( QStringList( addressee.givenName() ) );
   contact.setNameAdditionals( QStringList( addressee.additionalName() ) );
@@ -174,28 +276,8 @@ void NepomukContactFeeder::itemChanged( const Akonadi::Item &item, const QSet<QB
   }
 }
 
-void NepomukContactFeeder::itemRemoved( const Akonadi::Item &item )
-{
-  Nepomuk::PersonContact contact( item.url() );
+} // namespace Akonadi
 
-  QList<Nepomuk::PhoneNumber> numbers = contact.phoneNumbers();
-  Q_FOREACH( Nepomuk::PhoneNumber number, numbers ) {
-    number.remove();
-  }
-
-  QList<Nepomuk::EmailAddress> emails = contact.emailAddresses();
-  Q_FOREACH( Nepomuk::EmailAddress email, emails ) {
-    email.remove();
-  }
-
-  QList<Nepomuk::PostalAddress> addresses = contact.postalAddresses();
-  Q_FOREACH( Nepomuk::PostalAddress address, addresses ) {
-    address.remove();
-  }
-
-  contact.remove();
-}
-
-AKONADI_AGENT_MAIN( NepomukContactFeeder )
+AKONADI_AGENT_MAIN( Akonadi::NepomukContactFeeder )
 
 #include "nepomukcontactfeeder.moc"
--- agents/nepomuk_contact_feeder/nepomukcontactfeeder.h
+++ agents/nepomuk_contact_feeder/nepomukcontactfeeder.h
@@ -1,5 +1,6 @@
 /*
     Copyright (c) 2007 Tobias Koenig <tokoe@kde.org>
+                  2008 Sebastian Trueg <trueg@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
@@ -21,20 +22,34 @@
 #define AKONADI_NEPOMUK_CONTACT_FEEDER_H
 
 #include <akonadi/agentbase.h>
+#include <akonadi/item.h>
 
 namespace Akonadi {
 
 class NepomukContactFeeder : public AgentBase, public AgentBase::Observer
 {
   Q_OBJECT
+  Q_CLASSINFO( "D-Bus Interface", "org.kde.akonadi.NepomukContactFeeder" )
 
   public:
     NepomukContactFeeder( const QString &id );
+
+  public Q_SLOTS:
+    Q_SCRIPTABLE void updateAll( bool force = false );
 
   protected:
     void itemAdded( const Akonadi::Item &item, const Akonadi::Collection &collection );
     void itemChanged( const Akonadi::Item &item, const QSet<QByteArray> &partIdentifiers );
     void itemRemoved(const Akonadi::Item &item);
+
+  private Q_SLOTS:
+    void slotInitialItemScan();
+    void slotItemsReceivedForInitialScan( const Akonadi::Item::List& items );
+
+  private:
+    void updateItem( const Akonadi::Item &item );
+
+    bool mForceUpdate;
 };
 
 }
