331257e38a0de321e7aff28f7d23d0458fca78b0
Kevin Krammer <kevin.krammer@gmx.at>
2008-11-02 12:21:05 +0000

    Fixed typo in copyright statement
    svn path=/trunk/KDE/kdepim/akonadi/; revision=879048

--- plugins/akonadi_serializer_contactgroup.h
+++ plugins/akonadi_serializer_contactgroup.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 200( Kevin Krammer <kevin.krammer@gmx.at>
+    Copyright (c) 2008 Kevin Krammer <kevin.krammer@gmx.at>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
