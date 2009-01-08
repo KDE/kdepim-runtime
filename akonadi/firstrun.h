fab1adc749c891d5ad192886fdc73f5fc2476f50
Volker Krause <vkrause@kde.org>
2009-01-08 19:28:18 +0000

    Document the setup description files.
    svn path=/trunk/playground/pim/akonadi/; revision=907821

--- akonadi/firstrun.h
+++ akonadi/firstrun.h
@@ -33,6 +33,36 @@ namespace Akonadi {
 
 /**
  Takes care of setting up default resource agents when running Akonadi for the first time.
+
+ <h4>Defining your own default agent setups</h4>
+
+  To add an additional agent to the default Akonadi setup, add a file with the
+  agent setup description into $KDEDIR/share/akonadi/firstrun.
+
+  Such a file looks as follows:
+
+  @verbatim
+  [Agent]
+  Id=defaultaddressbook
+  Type=akonadi_vcard_resource
+  Name=My Addressbook
+
+  [Settings]
+  Path[$e]=~/.kde/share/apps/kabc/std.ics
+  AutosaveInterval=1
+  @endverbatim
+
+  The keys in the [Agent] group are mandatory:
+  <ul>
+  <li>Id: A unique identifier of the setup description, should never change to avoid the agent
+  being set up twice.</li>
+  <li>Type: The agent type</li>
+  <li>Nanme: The user visible name for this agent (only used for resource agents currently)</li>
+  </ul>
+
+  The [Settings] group is optional and contains agent-dependent settings.
+  For those settings to be applied, the agent needs to export its settings
+  via D-Bus using the KConfigXT &lt;-&gt; D-Bus bridge.
 */
 class Firstrun : public QObject
 {
