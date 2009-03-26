49956295d33447d2c712747c8bf2b6098f2aa349
Stephen Kelly <steveire@gmail.com>
2009-03-26 11:58:26 +0000

    Add a model for a tree of emails.
    svn path=/trunk/playground/pim/akonadi/; revision=944872

---
+++ akonadi/example_mail/mailmodel.h
@@ -0,0 +1,32 @@
+
+#ifndef MAILMODEL_H
+#define MAILMODEL_H
+
+#include "entitytreemodel.h"
+
+using namespace Akonadi;
+
+class MailModelPrivate;
+
+class MailModel : public EntityTreeModel
+{
+  Q_OBJECT
+public:
+
+  MailModel(Session *session, Monitor *monitor, QObject *parent = 0);
+  virtual ~MailModel();
+
+  virtual QVariant getData(Item item, int column, int role=Qt::DisplayRole) const;
+
+  virtual QVariant getData(Collection collection, int column, int role=Qt::DisplayRole) const;
+
+  virtual int columnCount(const QModelIndex &index = QModelIndex()) const;
+
+  virtual QVariant headerData( int section, Qt::Orientation orientation, int role ) const;
+
+private:
+    Q_DECLARE_PRIVATE(MailModel);
+    MailModelPrivate *d_ptr;
+};
+
+#endif
