e9ea039c276cb96c750e2fed430ddc176cd9630e
Tom Albers <toma@kde.org>
2009-04-04 09:15:44 +0000

    Documentation++
    svn path=/trunk/playground/pim/microblog/; revision=949001

--- resources/microblog/statusitem.h
+++ resources/microblog/statusitem.h
@@ -25,20 +25,58 @@
 #include <QHash>
 #include <QDateTime>
 
+/** 
+ * @class StatusItem
+ *
+ * This class is a representation of one Dent or Tweet. It is filled with 
+ * xml which the REST API from ident.ca or Teitter and parses it and gives
+ * back the values. There are also some convenience functions like 
+ */
 class StatusItem
 {
 public:
+    /** Constructor */
     StatusItem();
+
+    /** Constructor which takes the XML as argument. The data is parsed 
+      * instantly, so the other methods of the class are instantly usuable
+      */
     StatusItem( const QByteArray& );
+
+    /** Copy constructor */
     StatusItem( const StatusItem& );
+
+    /** Destructor */
     ~StatusItem();
+
+    /** Coparisation operator */
     StatusItem operator=( const StatusItem& );
+
+    /** The call to set the XML data. After this, the data is parsed
+      * instantly, so the other methods of the class are instantly usuable
+      */
     void setData( const QByteArray& );
+
+    /** Returns the unique id as given by the service */
     qlonglong id() const;
+
+    /** Returns the value of a certain key. The keys can be obtained via
+     *  keys()
+     */
     QString value( const QString& ) const;
-    QStringList keys() const;
+
+    /** Gives the text of the tweet or dent. The result is HTML where links
+     *  are hrefs and smileys are images.
+     */
     QString text() const;
+
+    /** Returns all the keys available */
+    QStringList keys() const;
+
+    /** Returns the date of the dent or tweet */
     QDateTime date() const;
+
+    /** Gives the raw xml data of the tweet or dent */
     QByteArray data() const;
 
 private:
