d2ae845f08526a3a8a849fdaeb6b6ca51f7bb8f6
Laurent Montel <montel@kde.org>
2009-04-09 09:31:10 +0000

    Backport: Don't search on ldap when we disable completion
    svn path=/trunk/KDE/kdepim/libkdepim/; revision=951423

--- libkdepim-copy/addresseelineedit.cpp
+++ libkdepim-copy/addresseelineedit.cpp
@@ -747,6 +747,12 @@ void AddresseeLineEdit::addCompletionItem( const QString &string, int weight,
 
 void AddresseeLineEdit::slotStartLDAPLookup()
 {
+  KGlobalSettings::Completion  mode = completionMode();
+
+  if ( mode == KGlobalSettings::CompletionNone ) {
+    return;
+  }
+
   if ( !s_LDAPSearch->isAvailable() ) {
     return;
   }
