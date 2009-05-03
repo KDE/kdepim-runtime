9a7385b6deb903fe72f0847a4e30952f6c7ac30e
Stephen Kelly <steveire@gmail.com>
2009-05-03 22:20:11 +0000

    Check in and use a FindGrantlee cmake module.
    svn path=/trunk/playground/pim/akonadi/; revision=963099

---
+++ akonadi/cmake/modules/FindGrantlee.cmake
@@ -0,0 +1,12 @@
+
+find_package(Qt4 REQUIRED)
+
+find_path(GRANTLEE_INCLUDE_DIR NAMES grantlee/grantlee.h )
+
+find_library(GRANTLEE_LIBRARY NAMES grantlee_core_library )
+
+if( GRANTLEE_LIBRARY AND GRANTLEE_INCLUDE_DIR )
+  set(Grantlee_FOUND true)
+  set(GRANTLEE_LIBRARIES ${GRANTLEE_LIBRARY} )
+endif( GRANTLEE_LIBRARY AND GRANTLEE_INCLUDE_DIR  )
+
