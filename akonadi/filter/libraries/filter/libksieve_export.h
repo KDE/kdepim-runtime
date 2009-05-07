a6cf7096da293925779aedfa8a9d85e4661fcb02
Szymon Stefanek <pragma@kvirc.net>
2009-05-07 11:28:57 +0000

    Fetch the ksieve headers via svn:externals as they aren't installed in the current state (because of unstable API). Add a fake libksieve_export.h which is generated by the libksieve build and thus not present on svn.
    svn path=/trunk/playground/pim/akonadi/; revision=964719

---
+++ akonadi/filter/libraries/filter/libksieve_export.h
@@ -0,0 +1,13 @@
+// This is a fake libksieve_export.h for the ksieve headers. It will be removed
+// once libksieve is really installed in the proper place or once this project
+// will be moved to a svn module that can access the real libksieve headers.
+
+#ifndef _LIBKSIEVE_EXPORT_H_
+#define _LIBKSIEVE_EXPORT_H_
+
+#include <kdemacros.h>
+
+#define KSIEVE_EXPORT KDE_IMPORT
+
+#endif //!_LIBKSIEVE_EXPORT_H_
+
