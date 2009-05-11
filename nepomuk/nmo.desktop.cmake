0e7f230e875bed35c1f2e5a46942cdc40a15403f
Sebastian Trueg <sebastian@trueg.de>
2009-05-11 08:29:58 +0000

    NMO and NCO are now part of kdebase
    svn path=/trunk/KDE/kdepim/akonadi/; revision=966438

--- nepomuk/nmo.desktop.cmake
+++
@@ -1,8 +0,0 @@
-[Desktop Entry]
-Version=1.0
-Name=Nepomuk Message Ontology
-Comment=The Nepomuk Message Ontology extends the NEPOMUK Information Element framework into the domain of messages. Kinds of messages covered by NMO include Emails and instant messages.
-URL=http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#
-Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/nmo.trig
-MimeType=application/x-trig
-Type=Data
