a9eeb9fdaf27093c5c813b4b3fdbd5dae6280638
Kevin Ottens <ervin@kde.org>
2009-06-04 13:41:11 +0000

    Now that we get the parts in itemChanged() make use of them to really update the mails. For IMAP though I have to APPEND the new content, and then mark the old mail deleted, hence why I had to adapt the onAppendDone() slot a bit.
    svn path=/trunk/KDE/kdepim/akonadi/resources/imap/; revision=977505

--- resources/imap/imapresource.cpp
+++ resources/imap/imapresource.cpp
@@ -239,7 +239,7 @@ void ImapResource::onAppendMessageDone( KJob *job )
 {
   KIMAP::AppendJob *append = qobject_cast<KIMAP::AppendJob*>( job );
 
-  Collection collection = job->property( "akonadiCollection" ).value<Collection>();
+  const QString collectionRemoteId = remoteIdForMailBox( append->mailBox() );
   Item item = job->property( "akonadiItem" ).value<Item>();
 
   if ( append->error() ) {
@@ -249,11 +249,29 @@ void ImapResource::onAppendMessageDone( KJob *job )
   qint64 uid = append->uid();
   Q_ASSERT( uid > 0 );
 
-  const QString remoteId =  collection.remoteId() + "-+-" + QString::number( uid );
+  const QString remoteId =  collectionRemoteId + "-+-" + QString::number( uid );
   kDebug() << "Setting remote ID to " << remoteId;
   item.setRemoteId( remoteId );
 
   changeCommitted( item );
+
+  // Check if it we got here because an itemChanged() call
+  // (since in IMAP you're forced to append+remove in this case)
+  qint64 oldUid = job->property( "oldUid" ).toLongLong();
+  if ( oldUid ) {
+    // OK it's indeed a content change, so we've to mark the old version as deleted
+    KIMAP::StoreJob *store = new KIMAP::StoreJob( m_account->session() );
+    store->setUidBased( true );
+    store->setSequenceSet( KIMAP::ImapSet( oldUid ) );
+    store->setFlags( QList<QByteArray>() << "\\Deleted" );
+    store->setMode( KIMAP::StoreJob::AppendFlags );
+    store->start();
+  }
+
+  Collection collection = job->property( "akonadiCollection" ).value<Collection>();
+  if ( !collection.isValid() ) {
+    collection = collectionFromRemoteId( collectionRemoteId );
+  }
 
   // Get the current uid next value and store it
   UidNextAttribute *uidAttr = 0;
@@ -282,23 +300,40 @@ void ImapResource::onAppendMessageDone( KJob *job )
 
 void ImapResource::itemChanged( const Item &item, const QSet<QByteArray> &parts )
 {
+  kDebug() << item.remoteId() << parts;
+
   const QString remoteId = item.remoteId();
   const QStringList temp = remoteId.split( "-+-" );
   const QString mailBox = mailBoxForRemoteId( temp[0] );
   const qint64 uid = temp[1].toLongLong();
 
-  KIMAP::SelectJob *select = new KIMAP::SelectJob( m_account->session() );
-  select->setMailBox( mailBox );
-  select->start();
-  KIMAP::StoreJob *store = new KIMAP::StoreJob( m_account->session() );
-  store->setProperty( "akonadiItem", QVariant::fromValue( item ) );
-  store->setProperty( "itemUid", uid );
-  store->setUidBased( true );
-  store->setSequenceSet( KIMAP::ImapSet( uid ) );
-  store->setFlags( item.flags().toList() );
-  store->setMode( KIMAP::StoreJob::SetFlags );
-  connect( store, SIGNAL( result( KJob* ) ), SLOT( onStoreFlagsDone( KJob* ) ) );
-  store->start();
+  if ( parts.contains( "PLD:RFC822" ) ) {
+    // save message to the server.
+    MessagePtr msg = item.payload<MessagePtr>();
+
+    KIMAP::AppendJob *job = new KIMAP::AppendJob( m_account->session() );
+    job->setProperty( "akonadiItem", QVariant::fromValue( item ) );
+    job->setProperty( "oldUid", uid ); // Will be used in onAppendMessageDone
+    job->setMailBox( mailBox );
+    job->setContent( msg->encodedContent( true ) );
+    job->setFlags( item.flags().toList() );
+    connect( job, SIGNAL( result( KJob* ) ), SLOT( onAppendMessageDone( KJob* ) ) );
+    job->start();
+
+  } else if ( parts.contains( "FLAGS" ) ) {
+    KIMAP::SelectJob *select = new KIMAP::SelectJob( m_account->session() );
+    select->setMailBox( mailBox );
+    select->start();
+    KIMAP::StoreJob *store = new KIMAP::StoreJob( m_account->session() );
+    store->setProperty( "akonadiItem", QVariant::fromValue( item ) );
+    store->setProperty( "itemUid", uid );
+    store->setUidBased( true );
+    store->setSequenceSet( KIMAP::ImapSet( uid ) );
+    store->setFlags( item.flags().toList() );
+    store->setMode( KIMAP::StoreJob::SetFlags );
+    connect( store, SIGNAL( result( KJob* ) ), SLOT( onStoreFlagsDone( KJob* ) ) );
+    store->start();
+  }
 }
 
 void ImapResource::onStoreFlagsDone( KJob *job )
