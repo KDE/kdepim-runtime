1fcb3006a279c183306566d1324bbcf3d3d68f72
Volker Krause <vkrause@kde.org>
2009-06-19 12:45:10 +0000

    There is no need to call exec() when we don't care for the result anyway. It just prevents the job scheduler from doing some optimizations and also disturbs change notification compression a bit.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=983909

--- resources/imap/imapresource.cpp
+++ resources/imap/imapresource.cpp
@@ -291,7 +291,6 @@ void ImapResource::onAppendMessageDone( KJob *job )
     }
 
     CollectionModifyJob *modify = new CollectionModifyJob( collection );
-    modify->exec();
   }
 }
 
@@ -737,7 +736,6 @@ void ImapResource::onGetAclDone( KJob *job )
   }
 
   CollectionModifyJob *modify = new CollectionModifyJob( collection );
-  modify->exec();
 }
 
 void ImapResource::onRightsReceived( KJob *job )
@@ -777,7 +775,6 @@ void ImapResource::onRightsReceived( KJob *job )
     collection.setRights( newRights );
 
     CollectionModifyJob *modify = new CollectionModifyJob( collection );
-    modify->exec();
   }
 }
 
@@ -818,7 +815,6 @@ void ImapResource::onQuotasReceived( KJob *job )
   }
 
   CollectionModifyJob *modify = new CollectionModifyJob( collection );
-  modify->exec();
 }
 
 void ImapResource::onGetMetaDataDone( KJob *job )
@@ -856,7 +852,6 @@ void ImapResource::onGetMetaDataDone( KJob *job )
   }
 
   CollectionModifyJob *modify = new CollectionModifyJob( collection );
-  modify->exec();
 }
 
 void ImapResource::onSelectDone( KJob *job )
@@ -928,7 +923,6 @@ void ImapResource::onSelectDone( KJob *job )
   }
 
   CollectionModifyJob *modify = new CollectionModifyJob( collection );
-  modify->exec();
 
   // First check the uidvalidity, if this has changed, it means the folder
   // has been deleted and recreated. So we wipe out the messages and
