7eff116fe202c28941e8fd9c86183f15bd8a6c8c
Till Adam <adam@kde.org>
2009-06-19 21:41:42 +0000

    Make the kabc model deal with the no-content and invalid collection cases as well.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=984076

--- kabc/kabcmodel.cpp
+++ kabc/kabcmodel.cpp
@@ -33,6 +33,12 @@ using namespace Akonadi;
 class KABCModel::Private
 {
   public:
+    bool collectionIsValid( const Collection& collection )
+    {
+      return !collection.isValid()
+          || collection.contentMimeTypes().contains( KABC::Addressee::mimeType() )
+          || collection.contentMimeTypes() == QStringList( QLatin1String("inode/directory") );
+    }
 };
 
 KABCModel::KABCModel( QObject *parent )
@@ -54,16 +60,17 @@ QStringList KABCModel::mimeTypes() const
       << KABC::Addressee::mimeType();
 }
 
+
 int KABCModel::rowCount( const QModelIndex& ) const
 {
-  if ( !collection().contentMimeTypes().contains( KABC::Addressee::mimeType() ) )
+  if ( !d->collectionIsValid( collection() ) )
     return 1;
   return ItemModel::rowCount();
 }
 
 int KABCModel::columnCount( const QModelIndex& ) const
 {
-  if ( !collection().contentMimeTypes().contains( KABC::Addressee::mimeType() ) )
+  if ( !d->collectionIsValid( collection() ) )
     return 1;
   return 4;
 }
@@ -80,7 +87,7 @@ QVariant KABCModel::data( const QModelIndex &index, int role ) const
   if ( index.row() >= rowCount() )
     return QVariant();
 
-  if ( !collection().contentMimeTypes().contains( KABC::Addressee::mimeType() ) ) {
+  if ( !d->collectionIsValid( collection() ) )
       if ( role == Qt::DisplayRole )
           // FIXME: i18n when strings unfreeze for 4.4
           return QString::fromLatin1( "This model can only handle contact folders. The current collection holds mimetypes: %1").arg(
@@ -160,7 +167,7 @@ QVariant KABCModel::headerData( int section, Qt::Orientation orientation, int ro
   if ( orientation != Qt::Horizontal )
     return QVariant();
 
-  if ( !collection().contentMimeTypes().contains( KABC::Addressee::mimeType() ) )
+  if ( !d->collectionIsValid( collection() ) )
       return QVariant();
 
   switch ( section ) {
