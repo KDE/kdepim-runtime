599e2021748b77e7cd19834ef7f712b6c8fd1176
Volker Krause <vkrause@kde.org>
2009-06-29 15:28:10 +0000

    compile with a clean checkout
    svn path=/branches/work/akonadi-ports/kdepim/akonadi/; revision=989149

--- akonadi/migration/kmail/kmailmigrator.cpp
+++ akonadi/migration/kmail/kmailmigrator.cpp
@@ -30,7 +30,7 @@
 using Akonadi::AgentManager;
 using Akonadi::AgentInstance;
 using Akonadi::AgentInstanceCreateJob;
-#include <akonadi/resources/mbox/settings.h>
+#include "settings.h"
 
 #include <KConfig>
 #include <KConfigGroup>
