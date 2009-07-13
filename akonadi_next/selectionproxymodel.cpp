KDE/kdepim-runtime

commit 88dc0b5be24a882f36fcc27718a87c078d99f24b

Map columns correctly in the selectionproxymodel.
svn path=/trunk/KDE/kdepim/akonadi/; revision=995710

akonadi_next/selectionproxymodel.cpp
@@ -893,6 +893,7 @@ QModelIndex SelectionProxyModel::mapToSource(const QModelIndex &proxyIndex) cons
     return QModelIndex();
 
   QModelIndex idx = d->m_map.value(proxyIndex.internalPointer());
+  idx = idx.sibling(proxyIndex.row(), proxyIndex.column());
   return idx;
 
 }

commit 1a00d2429c05e572980ac5d456a341b89feb517c

Use the correct row in mapToSource. Fixes bug introduced in r995710.
svn path=/trunk/KDE/kdepim/akonadi/; revision=995800

akonadi_next/selectionproxymodel.cpp
@@ -914,7 +914,7 @@ QModelIndex SelectionProxyModel::mapToSource(const QModelIndex &proxyIndex) cons
     return QModelIndex();
 
   QModelIndex idx = d->m_map.value(proxyIndex.internalPointer());
-  idx = idx.sibling(proxyIndex.row(), proxyIndex.column());
+  idx = idx.sibling(idx.row(), proxyIndex.column());
   return idx;
 
 }