80e226c8461a0712e9e1e19c2db6901cb3cebdec
Stephen Kelly <steveire@gmail.com>
2009-07-15 08:29:16 +0000

    Use amazing completion for matching contacts.
    svn path=/trunk/playground/pim/akonadi/; revision=996944

--- akonadi/example_mail/mailcomposer.cpp
+++ akonadi/example_mail/mailcomposer.cpp
@@ -27,6 +27,21 @@
 #include <QLabel>
 
 #include "emaillineedit.h"
+#include <QTreeView>
+#include "amazingcompleter.h"
+#include "amazingview.h"
+#include "amazingdelegate.h"
+#include "entitytreemodel.h"
+
+#include <akonadi/monitor.h>
+#include <akonadi/session.h>
+#include <akonadi/entitydisplayattribute.h>
+#include <akonadi/itemfetchscope.h>
+
+#include "contactsmodel.h"
+#include <descendantentitiesproxymodel.h>
+
+using namespace Akonadi;
 
 MailComposer::MailComposer(Akonadi::Session *session, QWidget *parent)
 {
@@ -34,7 +49,25 @@ MailComposer::MailComposer(Akonadi::Session *session, QWidget *parent)
 
   QLabel *toLabel = new QLabel(this);
   toLabel->setText("To:");
-  EmailLineEdit *emailLineEdit = new EmailLineEdit(session, this);
+
+  ItemFetchScope scope;
+  scope.fetchFullPayload( true ); // Need to have full item when adding it to the internal data structure
+  //   scope.fetchAttribute< CollectionChildOrderAttribute >();
+  scope.fetchAttribute< EntityDisplayAttribute >();
+
+  Monitor *monitor = new Monitor( this );
+  monitor->fetchCollection( true );
+  monitor->setItemFetchScope( scope );
+  monitor->setCollectionMonitored( Collection::root() );
+  monitor->setMimeTypeMonitored( "text/directory" );
+
+  ContactsModel *contactsModel = new ContactsModel( session, monitor, this);
+
+  DescendantEntitiesProxyModel *descProxyModel = new DescendantEntitiesProxyModel(this);
+  descProxyModel->setSourceModel(contactsModel);
+  descProxyModel->setHeaderSet(EntityTreeModel::ItemListHeaders);
+
+  QLineEdit *emailLineEdit = new QLineEdit(this);
 
   QLabel *subjectLabel = new QLabel(this);
   subjectLabel->setText("Subject:");
@@ -42,13 +75,27 @@ MailComposer::MailComposer(Akonadi::Session *session, QWidget *parent)
 
   QTextEdit *textEdit = new QTextEdit(this);
 
+  AmazingView *amazingView = new AmazingView(this);
+  amazingView->setItemDelegate(new AmazingContactItemDelegate(this));
+
   layout->addWidget(toLabel, 0, 0);
   layout->addWidget(emailLineEdit, 0, 1);
   layout->addWidget(subjectLabel, 1, 0);
   layout->addWidget(subjectLineEdit, 1, 1);
   layout->addWidget(textEdit, 2, 0, 1, 2);
+//   layout->addWidget(amazingView, 3, 0, 1, 2);
 
   this->setLayout(layout);
+
+  AmazingCompleter *completer = new AmazingCompleter(this);
+  completer->setModel(descProxyModel);
+  completer->setMatchingRole(Akonadi::EntityTreeModel::AmazingCompletionRole);
+  completer->setWidget(emailLineEdit);
+
+  connect(emailLineEdit, SIGNAL(textChanged(QString)), completer, SLOT(setCompletionPrefixString(QString)));
+
+  completer->setView(amazingView, AmazingCompleter::Popup);
+
 }
 
 
