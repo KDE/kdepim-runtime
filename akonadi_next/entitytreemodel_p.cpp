1b4485a968f31425d887c52996e222cb07712c58
Stephen Kelly <steveire@gmail.com>
2009-07-16 09:37:18 +0000

    Fix speeling. 'I' before 'E' except after 'C'
    svn path=/trunk/KDE/kdepim/akonadi/; revision=997682

--- akonadi_next/entitytreemodel_p.cpp
+++ akonadi_next/entitytreemodel_p.cpp
@@ -298,7 +298,7 @@ void EntityTreeModelPrivate::monitoredCollectionAdded( const Akonadi::Collection
 {
 
   // Some collection trees contain multiple mimetypes. Even though server side filtering ensures we
-  // only get the ones we're interested in from the job, we have to filter on collections recieved through signals too.
+  // only get the ones we're interested in from the job, we have to filter on collections received through signals too.
   if ( !m_mimeChecker.isWantedCollection( collection ) )
     return;
 
