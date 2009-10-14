5c09f9529f9873aa681f7a014972dc4700558966
Kevin Ottens <ervin@kde.org>
2009-10-14 14:34:11 +0000

    Add a proxy model which allows to color collections in red when their quota is above a given threshold.
    svn path=/trunk/KDE/kdepim/akonadi/; revision=1035204

---
+++ akonadi_next/quotacolorproxymodel.cpp
@@ -0,0 +1,124 @@
+/*
+    Copyright (c) 2009 Kevin Ottens <ervin@kde.org>
+
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "quotacolorproxymodel.h"
+
+#include <akonadi/entitytreemodel.h>
+#include <akonadi/collectionquotaattribute.h>
+
+#include <QtGui/QColor>
+
+using namespace Akonadi;
+
+/**
+ * @internal
+ */
+class QuotaColorProxyModel::Private
+{
+  public:
+    Private( QuotaColorProxyModel *parent )
+      : mParent( parent ), mThreshold( 100.0 ), mColor( Qt::red )
+    {
+    }
+
+    QuotaColorProxyModel *mParent;
+
+    qreal mThreshold;
+    QColor mColor;
+};
+
+QuotaColorProxyModel::QuotaColorProxyModel( QObject *parent )
+  : QSortFilterProxyModel( parent ),
+    d( new Private( this ) )
+{
+}
+
+QuotaColorProxyModel::~QuotaColorProxyModel()
+{
+  delete d;
+}
+
+void QuotaColorProxyModel::setWarningThreshold( qreal threshold )
+{
+  d->mThreshold = threshold;
+}
+
+qreal QuotaColorProxyModel::warningThreshold() const
+{
+  return d->mThreshold;
+}
+
+void QuotaColorProxyModel::setWarningColor( const QColor &color )
+{
+  d->mColor = color;
+}
+
+QColor QuotaColorProxyModel::warningColor() const
+{
+  return d->mColor;
+}
+
+QVariant QuotaColorProxyModel::data( const QModelIndex & index, int role) const
+{
+  if ( role == Qt::ForegroundRole ) {
+    const QModelIndex sourceIndex = mapToSource( index );
+    const QModelIndex rowIndex = sourceIndex.sibling( sourceIndex.row(), 0 );
+    Collection collection = sourceModel()->data( rowIndex, EntityTreeModel::CollectionRole ).value<Collection>();
+
+    if ( collection.isValid() && collection.hasAttribute<CollectionQuotaAttribute>() ) {
+      CollectionQuotaAttribute *quota = collection.attribute<CollectionQuotaAttribute>();
+
+      if ( quota->currentValue() > -1 && quota->maxValue() > 0 ) {
+        qreal percentage = ( 100.0 * quota->currentValue() ) / quota->maxValue();
+
+        if ( percentage >= d->mThreshold ) {
+          return d->mColor;
+        }
+      }
+    }
+  }
+
+  return QAbstractProxyModel::data( index, role );
+}
+
+bool QuotaColorProxyModel::dropMimeData( const QMimeData * data, Qt::DropAction action, int row, int column, const QModelIndex & parent )
+{
+  Q_ASSERT(sourceModel());
+  const QModelIndex sourceParent = mapToSource(parent);
+  return sourceModel()->dropMimeData(data, action, row, column, sourceParent);
+}
+
+QMimeData* QuotaColorProxyModel::mimeData( const QModelIndexList & indexes ) const
+{
+  Q_ASSERT(sourceModel());
+  QModelIndexList sourceIndexes;
+  foreach(const QModelIndex& index, indexes)
+    sourceIndexes << mapToSource(index);
+  return sourceModel()->mimeData(sourceIndexes);
+}
+
+QStringList QuotaColorProxyModel::mimeTypes() const
+{
+  Q_ASSERT(sourceModel());
+  return sourceModel()->mimeTypes();
+}
+
+#include "quotacolorproxymodel.moc"
+
