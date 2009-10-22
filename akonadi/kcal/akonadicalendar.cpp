7b89e812245ee78a9d3fb0d208bc97f973de90f3
Frank Osterfeld <frank.osterfeld@kdemail.net>
2009-10-22 13:07:26 +0000

    move method impls to the .cpp
    svn path=/branches/work/akonadi-ports/kdepim/akonadi/; revision=1038992

--- akonadi/kcal/akonadicalendar.cpp
+++ akonadi/kcal/akonadicalendar.cpp
@@ -56,6 +56,411 @@ using namespace Akonadi;
 using namespace KCal;
 using namespace KOrg;
 
+AkonadiCalendar::Private::Private( AkonadiCalendar *q )
+  : q( q )
+  , m_monitor( new Akonadi::Monitor() )
+  , m_session( new Akonadi::Session( QCoreApplication::instance()->applicationName().toUtf8() + QByteArray("-AkonadiCal-") + QByteArray::number(qrand()) ) )
+  , m_incidenceBeingChanged()
+{
+  m_monitor->itemFetchScope().fetchFullPayload();
+  m_monitor->itemFetchScope().setAncestorRetrieval( Akonadi::ItemFetchScope::Parent );
+  m_monitor->ignoreSession( m_session );
+
+  connect( m_monitor, SIGNAL(itemChanged(Akonadi::Item,QSet<QByteArray> )),
+           this, SLOT(itemChanged(Akonadi::Item,QSet<QByteArray> )) );
+  connect( m_monitor, SIGNAL(itemMoved(Akonadi::Item,Akonadi::Collection,Akonadi::Collection )),
+           this, SLOT(itemMoved(Akonadi::Item,Akonadi::Collection,Akonadi::Collection ) ) );
+  connect( m_monitor, SIGNAL(itemAdded(Akonadi::Item,Akonadi::Collection )),
+           this, SLOT(itemAdded(Akonadi::Item )) );
+  connect( m_monitor, SIGNAL(itemRemoved(Akonadi::Item )),
+           this, SLOT(itemRemoved(Akonadi::Item )) );
+  /*
+  connect( m_monitor, SIGNAL(itemLinked(const Akonadi::Item,Akonadi::Collection)),
+           this, SLOT(itemAdded(const Akonadi::Item,Akonadi::Collection)) );
+  connect( m_monitor, SIGNAL(itemUnlinked(Akonadi::Item,Akonadi::Collection )),
+           this, SLOT(itemRemoved(Akonadi::Item,Akonadi::Collection )) );
+  */
+}
+
+AkonadiCalendar::Private::~Private()
+{
+  delete m_monitor;
+  delete m_session;
+}
+
+bool AkonadiCalendar::Private::addIncidence( Incidence *incidence )
+{
+  kDebug();
+  Akonadi::CollectionDialog dlg;
+  dlg.setMimeTypeFilter( QStringList() << QString::fromLatin1( "text/calendar" ) );
+  if ( ! dlg.exec() ) {
+    return false;
+  }
+  const Akonadi::Collection collection = dlg.selectedCollection();
+  Q_ASSERT( collection.isValid() );
+  //Q_ASSERT( m_collectionMap.contains( collection.id() ) ); //we can add items to collections we don't show yet
+
+  Akonadi::Item item;
+  //the sub-mimetype of text/calendar as defined at kdepim/akonadi/kcal/kcalmimetypevisitor.cpp
+  item.setMimeType( QString::fromLatin1("application/x-vnd.akonadi.calendar.%1").arg(QLatin1String(incidence->type().toLower())) );
+  KCal::Incidence::Ptr incidencePtr( incidence ); //no clone() needed
+  item.setPayload<KCal::Incidence::Ptr>( incidencePtr );
+  Akonadi::ItemCreateJob *job = new Akonadi::ItemCreateJob( item, collection, m_session );
+  connect( job, SIGNAL( result( KJob* ) ), this, SLOT( createDone( KJob* ) ) );
+  return true;
+}
+
+bool AkonadiCalendar::Private::addIncidence( const Incidence::Ptr &incidence )
+{
+  kDebug();
+  Akonadi::CollectionDialog dlg; //PENDING(AKONADI_PORT) we really need a parent here
+  dlg.setMimeTypeFilter( QStringList() << QLatin1String( "text/calendar" ) );
+  if ( ! dlg.exec() ) {
+    return false;
+  }
+  const Akonadi::Collection collection = dlg.selectedCollection();
+  Q_ASSERT( collection.isValid() );
+  //Q_ASSERT( m_collectionMap.contains( collection.id() ) ); //we can add items to collections we don't show yet
+
+  Akonadi::Item item;
+  item.setPayload( incidence );
+  //the sub-mimetype of text/calendar as defined at kdepim/akonadi/kcal/kcalmimetypevisitor.cpp
+  item.setMimeType( QString::fromLatin1("application/x-vnd.akonadi.calendar.%1").arg(QLatin1String(incidence->type().toLower())) ); //PENDING(AKONADI_PORT) shouldn't be hardcoded?
+  Akonadi::ItemCreateJob *job = new Akonadi::ItemCreateJob( item, collection, m_session );
+  connect( job, SIGNAL( result( KJob* ) ), this, SLOT( createDone( KJob* ) ) );
+  return true;
+}
+
+bool AkonadiCalendar::Private::deleteIncidence( const Akonadi::Item &item )
+{
+  kDebug();
+  m_changes.removeAll( item.id() ); //abort changes to this incidence cause we will just delete it
+  Akonadi::ItemDeleteJob *job = new Akonadi::ItemDeleteJob( item, m_session );
+  connect( job, SIGNAL( result( KJob* ) ), this, SLOT( deleteDone( KJob* ) ) );
+  return true;
+}
+
+void AkonadiCalendar::Private::assertInvariants() const
+{
+}
+
+void AkonadiCalendar::Private::updateItem( const Akonadi::Item &item, UpdateMode mode ) {
+  assertInvariants();
+  const bool alreadyExisted = m_itemMap.contains( item.id() );
+  const Akonadi::Item::Id id = item.id();
+
+  Q_ASSERT( mode == DontCare || alreadyExisted == ( mode == AssertExists ) );
+
+  if ( alreadyExisted ) {
+    Q_ASSERT( item.storageCollectionId() == m_itemMap.value( id ).storageCollectionId() ); // there was once a bug that resulted in items forget their collectionId...
+    // update-only goes here
+  } else {
+    // new-only goes here
+  }
+
+  QDate typeSpecificDate;
+  if( const Event::Ptr e = Akonadi::event( item ) ) {
+    if ( !e->recurs() && !e->isMultiDay() ) {
+      typeSpecificDate = e->dtStart().date();
+    }
+  } else if( const Todo::Ptr t = Akonadi::todo( item ) ) {
+    if ( t->hasDueDate() ) {
+      typeSpecificDate = t->dtDue().date();
+    }
+  } else if( const Journal::Ptr j = Akonadi::journal( item ) ) {
+      typeSpecificDate = j->dtStart().date();
+  } else {
+    Q_ASSERT( false );
+    return;
+  }
+
+  const KCal::Incidence::Ptr incidence = Akonadi::incidence( item );
+  Q_ASSERT( incidence );
+
+  if ( alreadyExisted ) {
+    //TODO(AKONADI_PORT): for changed items, we should remove existing date entries (they might have changed)
+  }
+
+  if ( typeSpecificDate.isValid() )
+    m_itemsForDate.insert( typeSpecificDate.toString(), item );
+  m_itemsForDate.insert( incidence->dtStart().date().toString(), item );
+
+  m_itemMap.insert( id, item );
+
+  UnseenItem ui;
+  ui.collection = item.storageCollectionId();
+  ui.uid = incidence->uid();
+
+  //REVIEW(AKONADI_PORT)
+  //UIDs might be duplicated and thus not unique, so for now we assume that the relatedTo
+  // UID refers to an item in the same collection.
+  //this might break with virtual collections, so we might fall back to a global UID
+  //to akonadi item mapping, and pick just any item (or the first found, or whatever strategy makes sense)
+  //from the ones with the same UID
+  const QString parentUID = incidence->relatedToUid();
+  const bool hasParent = !parentUID.isEmpty();
+  UnseenItem parentItem;
+  QMap<UnseenItem,Akonadi::Item::Id>::const_iterator parentIt = m_uidToItemId.constEnd();
+  bool knowParent = false;
+  bool parentNotChanged = false;
+  if ( hasParent ) {
+    parentItem.collection = item.storageCollectionId();
+    parentItem.uid = parentUID;
+    QMap<UnseenItem,Akonadi::Item::Id>::const_iterator parentIt = m_uidToItemId.constFind( parentItem );
+    knowParent = parentIt != m_uidToItemId.constEnd();
+  }
+
+  if ( alreadyExisted ) {
+    Q_ASSERT( m_uidToItemId.value( ui ) == item.id() );
+    QHash<Akonadi::Item::Id,Akonadi::Item::Id>::Iterator oldParentIt = m_childToParent.find( id );
+    if ( oldParentIt != m_childToParent.constEnd() ) {
+      const Incidence::Ptr parentInc = Akonadi::incidence( m_itemMap.value( oldParentIt.value() ) );
+      Q_ASSERT( parentInc );
+      if ( parentInc->uid() != parentUID ) {
+        //parent changed, remove old entries
+        Akonadi::incidence( item )->setRelatedTo( 0 );
+        QVector<Akonadi::Item::Id>& l = m_parentToChildren[oldParentIt.value()];
+        l.erase( std::remove( l.begin(), l.end(), id ), l.end() );
+        m_childToParent.remove( id );
+      } else
+        parentNotChanged = true;
+    } else { //old parent not seen, maybe unseen?
+      QHash<Akonadi::Item::Id,UnseenItem>::Iterator oldUnseenParentIt = m_childToUnseenParent.find( id );
+      if ( oldUnseenParentIt != m_childToUnseenParent.constEnd() ) {
+        if ( oldUnseenParentIt.value().uid != parentUID ) {
+          //parent changed, remove old entries
+          QVector<Akonadi::Item::Id>& l = m_unseenParentToChildren[oldUnseenParentIt.value()];
+          l.erase( std::remove( l.begin(), l.end(), id ), l.end() );
+          m_childToUnseenParent.remove( id );
+        }
+        else
+          parentNotChanged = true;
+      }
+    }
+
+  } else {
+    m_uidToItemId.insert( ui, item.id() );
+
+    //check for already known children:
+    const QVector<Akonadi::Item::Id> orphanedChildren = m_unseenParentToChildren.value( ui );
+    if ( !orphanedChildren.isEmpty() )
+      m_parentToChildren.insert( id, orphanedChildren );
+    Q_FOREACH ( const Akonadi::Item::Id &cid, orphanedChildren )
+      m_childToParent.insert( cid, id );
+    m_unseenParentToChildren.remove( ui );
+    m_childToUnseenParent.remove( id );
+  }
+
+  if ( hasParent && !parentNotChanged ) {
+    if ( knowParent ) {
+      Q_ASSERT( !m_parentToChildren.value( parentIt.value() ).contains( id ) );
+      const Incidence::Ptr parentInc = Akonadi::incidence( m_itemMap.value( parentIt.value() ) );
+      Q_ASSERT( parentInc );
+      Akonadi::incidence( item )->setRelatedTo( parentInc.get() );
+      m_parentToChildren[parentIt.value()].push_back( id );
+      m_childToParent.insert( id, parentIt.value() );
+    } else {
+      m_childToUnseenParent.insert( id, parentItem );
+      m_unseenParentToChildren[parentItem].push_back( id );
+    }
+  }
+
+  if ( !alreadyExisted ) {
+    incidence->registerObserver( q );
+    q->notifyIncidenceAdded( item );
+  }
+  assertInvariants();
+}
+
+void AkonadiCalendar::Private::listingDone( KJob *job )
+{
+    kDebug();
+    Akonadi::ItemFetchJob *fetchjob = static_cast<Akonadi::ItemFetchJob*>( job );
+    if ( job->error() ) {
+        kWarning( 5250 ) << "Item query failed:" << job->errorString();
+        emit q->signalErrorMessage( job->errorString() );
+        return;
+    }
+    itemsAdded( fetchjob->items() );
+}
+
+void AkonadiCalendar::Private::agentCreated( KJob *job )
+{
+    kDebug();
+    Akonadi::AgentInstanceCreateJob *createjob = dynamic_cast<Akonadi::AgentInstanceCreateJob*>( job );
+    if ( createjob->error() ) {
+        kWarning( 5250 ) << "Agent create failed:" << createjob->errorString();
+        emit q->signalErrorMessage( createjob->errorString() );
+        return;
+    }
+    Akonadi::AgentInstance instance = createjob->instance();
+    //instance.setName( CalendarName );
+    QDBusInterface iface( QString::fromLatin1("org.freedesktop.Akonadi.Resource.%1").arg( instance.identifier() ), QLatin1String("/Settings") );
+    if( ! iface.isValid() ) {
+        kWarning( 5250 ) << "Failed to obtain D-Bus interface for remote configuration.";
+        emit q->signalErrorMessage( i18n("Failed to obtain D-Bus interface for remote configuration.") );
+        return;
+    }
+    QString path = createjob->property( "path" ).toString();
+    Q_ASSERT( ! path.isEmpty() );
+    iface.call(QLatin1String("setPath"), path);
+    instance.reconfigure();
+}
+
+void AkonadiCalendar::Private::createDone( KJob *job )
+{
+    kDebug();
+    if ( job->error() ) {
+        kWarning( 5250 ) << "Item create failed:" << job->errorString();
+        emit q->signalErrorMessage( job->errorString() );
+        return;
+    }
+    Akonadi::ItemCreateJob *createjob = static_cast<Akonadi::ItemCreateJob*>( job );
+    if ( m_collectionMap.contains( createjob->item().parentCollection().id() ) ) {
+      // yes, adding to an un-viewed collection happens
+      itemAdded( createjob->item() );
+    } else {
+      // FIXME show dialog indicating that the creation worked, but the incidence will
+      // not show, since the collection isn't
+      kWarning() << "Collection with id=" << createjob->item().parentCollection() << " not in m_collectionMap";
+    }
+}
+
+void AkonadiCalendar::Private::deleteDone( KJob *job )
+{
+    kDebug();
+    if ( job->error() ) {
+        kWarning( 5250 ) << "Item delete failed:" << job->errorString();
+        emit q->signalErrorMessage( job->errorString() );
+        return;
+    }
+    Akonadi::ItemDeleteJob *deletejob = static_cast<Akonadi::ItemDeleteJob*>( job );
+    itemsRemoved( deletejob->deletedItems() );
+}
+
+void AkonadiCalendar::Private::modifyDone( KJob *job )
+{
+    // we should probably update the revision number here,or internally in the Event
+    // itself when certain things change. need to verify with ical documentation.
+
+    assertInvariants();
+    Akonadi::ItemModifyJob *modifyjob = static_cast<Akonadi::ItemModifyJob*>( job );
+    if ( modifyjob->error() ) {
+        kWarning( 5250 ) << "Item modify failed:" << job->errorString();
+        emit q->signalErrorMessage( job->errorString() );
+        return;
+    }
+    Akonadi::Item item = modifyjob->item();
+    Q_ASSERT( item.hasPayload<KCal::Incidence::Ptr>() );
+    const KCal::Incidence::Ptr incidence = item.payload<KCal::Incidence::Ptr>();
+    Q_ASSERT( incidence );
+    const Akonadi::Item::Id id = item.id();
+    //kDebug()<<"Old storageCollectionId="<<m_itemMap[id]->m_item.storageCollectionId();
+    kDebug() << "Item modify done item id=" << id << "storageCollectionId=" << item.storageCollectionId();
+    updateItem( item, AssertExists );
+    q->notifyIncidenceChanged( item );
+    q->setModified( true );
+    emit q->calendarChanged();
+    assertInvariants();
+}
+
+void AkonadiCalendar::Private::itemChanged( const Akonadi::Item& item, const QSet<QByteArray>& )
+{
+    assertInvariants();
+    Q_ASSERT( item.isValid() );
+    const Akonadi::Item::Id id = item.id();
+    const Incidence::ConstPtr incidence = Akonadi::incidence( item );
+    if ( !incidence )
+      return;
+    kDebug() << "Item changed item id=" << id << "summary=" << incidence->summary() << "type=" << incidence->type() << "storageCollectionId=" << item.storageCollectionId();
+    updateItem( item, AssertExists );
+    q->notifyIncidenceChanged( item );
+    q->setModified( true );
+    emit q->calendarChanged();
+    assertInvariants();
+}
+
+void AkonadiCalendar::Private::itemMoved( const Akonadi::Item &item, const Akonadi::Collection& colSrc, const Akonadi::Collection& colDst )
+{
+    kDebug();
+    if( m_collectionMap.contains(colSrc.id()) && ! m_collectionMap.contains(colDst.id()) )
+        itemRemoved( item );
+    else if( m_collectionMap.contains(colDst.id()) && ! m_collectionMap.contains(colSrc.id()) )
+        itemAdded( item );
+}
+
+void AkonadiCalendar::Private::itemsAdded( const Akonadi::Item::List &items )
+{
+    kDebug();
+    assertInvariants();
+    foreach( const Akonadi::Item &item, items ) {
+        if ( !m_collectionMap.contains( item.parentCollection().id() ) )  // collection got deselected again in the meantime
+          continue;
+        Q_ASSERT( item.isValid() );
+        if ( !Akonadi::hasIncidence( item ) )
+          continue;
+        updateItem( item, AssertNew );
+        const KCal::Incidence::Ptr incidence = item.payload<KCal::Incidence::Ptr>();
+        kDebug() << "Add akonadi id=" << item.id() << "uid=" << incidence->uid() << "summary=" << incidence->summary() << "type=" << incidence->type();
+
+        incidence->registerObserver( q );
+        q->notifyIncidenceAdded( item );
+    }
+    q->setModified( true );
+    emit q->calendarChanged();
+    assertInvariants();
+}
+
+void AkonadiCalendar::Private::itemAdded( const Akonadi::Item &item )
+{
+    kDebug();
+    Q_ASSERT( item.isValid() );
+    if( ! m_itemMap.contains( item.id() ) ) {
+      itemsAdded( Akonadi::Item::List() << item );
+    }
+}
+
+void AkonadiCalendar::Private::itemsRemoved( const Akonadi::Item::List &items )
+{
+    assertInvariants();
+    //kDebug()<<items.count();
+    foreach(const Akonadi::Item& item, items) {
+        Q_ASSERT( item.isValid() );
+        Akonadi::Item ci( m_itemMap.take( item.id() ) );
+        Q_ASSERT( ci.hasPayload<KCal::Incidence::Ptr>() );
+        const KCal::Incidence::Ptr incidence = ci.payload<KCal::Incidence::Ptr>();
+        kDebug() << "Remove uid=" << incidence->uid() << "summary=" << incidence->summary() << "type=" << incidence->type();
+
+        if( const Event::Ptr e = dynamic_pointer_cast<Event>(incidence) ) {
+        if ( !e->recurs() ) {
+            m_itemsForDate.remove( e->dtStart().date().toString(), item );
+        }
+        } else if( const Todo::Ptr t = dynamic_pointer_cast<Todo>( incidence ) ) {
+          if ( t->hasDueDate() ) {
+            m_itemsForDate.remove( t->dtDue().date().toString(), item );
+          }
+        } else if( const Journal::Ptr j = dynamic_pointer_cast<Journal>( incidence ) ) {
+          m_itemsForDate.remove( j->dtStart().date().toString(), item );
+        } else {
+          Q_ASSERT(false);
+          continue;
+        }
+
+        //incidence->unregisterObserver( q );
+        q->notifyIncidenceDeleted( item );
+    }
+    q->setModified( true );
+    emit q->calendarChanged();
+    assertInvariants();
+}
+
+void AkonadiCalendar::Private::itemRemoved( const Akonadi::Item &item )
+{
+    kDebug();
+    itemsRemoved( Akonadi::Item::List() << item );
+}
+
+
 AkonadiCalendar::AkonadiCalendar( const KDateTime::Spec &timeSpec )
   : KOrg::CalendarBase( timeSpec )
   , d( new AkonadiCalendar::Private(this) )
