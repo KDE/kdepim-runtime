c6ab004dc6e877e84a0c5e3b65af154b96aa9e05
Laurent Montel <montel@kde.org>
2009-12-02 17:25:49 +0000

    Specify rights
    svn path=/branches/work/akonadi-ports/kdepim/akonadi/; revision=1057513

--- akonadi/resources/pop3/accountdialog.cpp
+++ akonadi/resources/pop3/accountdialog.cpp
@@ -151,6 +151,7 @@ void AccountDialog::setupWidgets()
   folderRequester->setMimeTypeFilter(
       QStringList() << QLatin1String( "message/rfc822" ) );
   folderRequester->setFrameStyle( QFrame::NoFrame );
+  folderRequester->setAccessRightsFilter( Akonadi::Collection::CanChangeItem | Akonadi::Collection::CanCreateItem | Akonadi::Collection::CanDeleteItem );
 
   connect( usePipeliningCheck, SIGNAL(clicked()),
            SLOT(slotPipeliningClicked()) );
