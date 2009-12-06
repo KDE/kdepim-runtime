d2e1b8de95b9fd03b9076a3a457d73f7068127b7
Volker Krause <vkrause@kde.org>
2009-12-06 16:06:16 +0000

    Prototype of a new account wizard. It consists of a generic loader and actual wizards based on ui files and Kross scripts. So far it supports setting up Akonadi resources and mail transports, LDAP accounts and identities are still to be done.
    Since the actual wizards do not contain any compiled code they can be
    very easily adapted for provider or site-specific setups and could even
    be distributed via GHNS.
    
    svn path=/branches/work/akonadi-ports/kdepim/akonadi/accountwizard/; revision=1059425

---
+++ accountwizard/page.h
@@ -0,0 +1,59 @@
+/*
+    Copyright (c) 2009 Volker Krause <vkrause@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <QtGui/QWidget>
+
+class KAssistantDialog;
+class KPageWidgetItem;
+
+class Page : public QWidget
+{
+  Q_OBJECT
+  public:
+    Page( KAssistantDialog *parent );
+
+    void setPageWidgetItem( KPageWidgetItem *item );
+
+    virtual void enterPageNext();
+    virtual void enterPageBack();
+    virtual void leavePageNext();
+    virtual void leavePageBack();
+
+  signals:
+    Q_SCRIPTABLE void pageEnteredNext();
+    Q_SCRIPTABLE void pageEnteredBack();
+    Q_SCRIPTABLE void pageLeftNext();
+    Q_SCRIPTABLE void pageLeftBack();
+
+  public slots:
+    Q_SCRIPTABLE void setValid( bool valid );
+
+  protected:
+    KPageWidgetItem *m_item;
+    KAssistantDialog *m_parent;
+
+  private:
+    friend class Dialog;
+    bool m_valid;
+};
+
+#endif // PAGE_H
