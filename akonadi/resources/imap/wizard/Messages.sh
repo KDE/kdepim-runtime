32b6553f649d753129984de40330c68fb03ead01
Laurent Montel <montel@kde.org>
2009-12-07 17:11:01 +0000

    Implement load po file.
    svn path=/branches/work/akonadi-ports/kdepim/akonadi/; revision=1059917

--- akonadi/resources/imap/wizard/Messages.sh
+++ akonadi/resources/imap/wizard/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT *.cpp -o $podir/imapwizard.pot
+$XGETTEXT *.cpp -o $podir/accountwizard_imap.pot
--- akonadi/resources/maildir/wizard/Messages.sh
+++ akonadi/resources/maildir/wizard/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT *.cpp -o $podir/maildirwizard.pot
+$XGETTEXT *.cpp -o $podir/accountwizard_maildir.pot
