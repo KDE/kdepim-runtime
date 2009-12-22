20ffda1a34320e03edda0c82b91838de2efff600
Tobias Koenig <tokoe@kde.org>
2009-12-22 16:41:55 +0000

    Add contact group property to OXA::Object
    svn path=/trunk/playground/pim/openxchange/; revision=1065149

---
+++ resources/openxchange/oxa/object.cpp
@@ -0,0 +1,113 @@
+/*
+    This file is part of oxaccess.
+
+    Copyright (c) 2009 Tobias Koenig <tokoe@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "object.h"
+
+using namespace OXA;
+
+Object::Object()
+  : mObjectId( -1 ), mFolderId( -1 )
+{
+}
+
+void Object::setObjectId( qlonglong id )
+{
+  mObjectId = id;
+}
+
+qlonglong Object::objectId() const
+{
+  return mObjectId;
+}
+
+void Object::setFolderId( qlonglong id )
+{
+  mFolderId = id;
+}
+
+qlonglong Object::folderId() const
+{
+  return mFolderId;
+}
+
+void Object::setLastModified( const QString &timeStamp )
+{
+  mLastModified = timeStamp;
+}
+
+QString Object::lastModified() const
+{
+  return mLastModified;
+}
+
+void Object::setModule( Folder::Module module )
+{
+  mModule = module;
+}
+
+Folder::Module Object::module() const
+{
+  return mModule;
+}
+
+void Object::setContact( const KABC::Addressee &contact )
+{
+  mModule = Folder::Contacts;
+  mContact = contact;
+}
+
+KABC::Addressee Object::contact() const
+{
+  return mContact;
+}
+
+void Object::setContactGroup( const KABC::ContactGroup &group )
+{
+  mModule = Folder::Contacts;
+  mContactGroup = group;
+}
+
+KABC::ContactGroup Object::contactGroup() const
+{
+  return mContactGroup;
+}
+
+void Object::setEvent( const KCal::Incidence::Ptr &event )
+{
+  mModule = Folder::Calendar;
+  mEvent = event;
+}
+
+KCal::Incidence::Ptr Object::event() const
+{
+  return mEvent;
+}
+
+void Object::setTask( const KCal::Incidence::Ptr &task )
+{
+  mModule = Folder::Tasks;
+  mTask = task;
+}
+
+KCal::Incidence::Ptr Object::task() const
+{
+  return mTask;
+}
---
+++ resources/openxchange/oxa/object.h
@@ -0,0 +1,81 @@
+/*
+    This file is part of oxaccess.
+
+    Copyright (c) 2009 Tobias Koenig <tokoe@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef OXA_OBJECT_H
+#define OXA_OBJECT_H
+
+#include "folder.h"
+
+#include <boost/shared_ptr.hpp>
+#include <kabc/addressee.h>
+#include <kabc/contactgroup.h>
+#include <kcal/incidence.h>
+
+#include <QtCore/QList>
+#include <QtCore/QString>
+
+namespace OXA {
+
+class Object
+{
+  public:
+    typedef QList<Object> List;
+
+    Object();
+
+    void setObjectId( qlonglong id );
+    qlonglong objectId() const;
+
+    void setFolderId( qlonglong id );
+    qlonglong folderId() const;
+
+    void setLastModified( const QString &timeStamp );
+    QString lastModified() const;
+
+    void setModule( Folder::Module module );
+    Folder::Module module() const;
+
+    void setContact( const KABC::Addressee &contact );
+    KABC::Addressee contact() const;
+
+    void setContactGroup( const KABC::ContactGroup &group );
+    KABC::ContactGroup contactGroup() const;
+
+    void setEvent( const KCal::Incidence::Ptr &event );
+    KCal::Incidence::Ptr event() const;
+
+    void setTask( const KCal::Incidence::Ptr &task );
+    KCal::Incidence::Ptr task() const;
+
+  private:
+    qlonglong mObjectId;
+    qlonglong mFolderId;
+    QString mLastModified;
+    Folder::Module mModule;
+    KABC::Addressee mContact;
+    KABC::ContactGroup mContactGroup;
+    KCal::Incidence::Ptr mEvent;
+    KCal::Incidence::Ptr mTask;
+};
+
+}
+
+#endif
