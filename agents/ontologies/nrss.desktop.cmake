750a3a0bbe856b5e7c4f5c2301a0fc5e58e73852
Volker Krause <vkrause@kde.org>
2010-01-08 10:49:19 +0000

    Remove the krss dependency on kdepim/runtime.
    svn path=/branches/work/akonadi-ports/kdepim/runtime/; revision=1071591

--- agents/ontologies/nrss.desktop.cmake
+++
@@ -1,8 +0,0 @@
-[Desktop Entry]
-Version=1.0
-Name=Nepomuk RSS Ontology
-Comment=The Nepomuk RSS Ontology extends the Nepomuk Information Element framework into the domain of RSS channels.
-URL=http://www.semanticdesktop.org/ontologies/2007/03/22/nrss#
-Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/nrss.rdfs
-MimeType=application/rdf+xml
-Type=Data
