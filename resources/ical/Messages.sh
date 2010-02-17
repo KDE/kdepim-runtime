e45fb27f8d116d59037c7c868848681c9befe958
Laurent Montel <montel@kde.org>
2010-02-17 22:09:13 +0000

    Fix extract messages
    svn path=/branches/KDE/4.4/kdepim/runtime/; revision=1091927

--- resources/ical/Messages.sh
+++ resources/ical/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp` -o $podir/akonadi_ical_resource.pot
+$EXTRACTRC `find . -name \*.kcfg -o -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/akonadi_ical_resource.pot
+rm -f rc.cpp
