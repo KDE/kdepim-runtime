76b2a995b813024ca2ab5e21c89e14591041e250
Casey Link <unnamedrambler@gmail.com>
2010-04-07 13:33:33 +0000

    remove the conversion between arbitrary numbers and enum types, this is taken care of in the imap dialog. corrects imap encryption saving issues. CCBUG: 233206
    svn path=/trunk/KDE/kdepim/runtime/; revision=1112152

--- resources/imap/imapaccount.cpp
+++ resources/imap/imapaccount.cpp
@@ -64,48 +64,8 @@ ImapAccount::ImapAccount( Settings *settings, QObject *parent )
   m_userName = settings->userName();
   m_subscriptionEnabled = settings->subscriptionEnabled();
 
-  switch ( settings->safety() ) {
-  case 1:
-    m_encryption = KIMAP::LoginJob::Unencrypted;
-    break;
-  case 2:
-    m_encryption = KIMAP::LoginJob::AnySslVersion;
-    break;
-  case 3:
-    m_encryption = KIMAP::LoginJob::TlsV1;
-    break;
-  default:
-    kFatal("Shouldn't happen...");
-  }
-
-  switch ( settings->authentication() ) {
-  case 0:
-    m_authentication = KIMAP::LoginJob::ClearText;
-    break;
-  case 1:
-    m_authentication = KIMAP::LoginJob::Login;
-    break;
-  case 2:
-    m_authentication = KIMAP::LoginJob::Plain;
-    break;
-  case 3:
-    m_authentication = KIMAP::LoginJob::CramMD5;
-    break;
-  case 4:
-    m_authentication = KIMAP::LoginJob::DigestMD5;
-    break;
-  case 5:
-    m_authentication = KIMAP::LoginJob::NTLM;
-    break;
-  case 6:
-    m_authentication = KIMAP::LoginJob::GSSAPI;
-    break;
-  case 7:
-    m_authentication = KIMAP::LoginJob::Anonymous;
-    break;
-  default:
-    kFatal("Shouldn't happen...");
-  }
+  m_encryption = (KIMAP::LoginJob::EncryptionMode) settings->safety();
+  m_authentication = (KIMAP::LoginJob::AuthenticationMode) settings->authentication();
 }
 
 ImapAccount::ImapAccount( QObject *parent )
