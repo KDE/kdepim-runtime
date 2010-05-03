0f37869f594439655b121e8d3481317524df9fd5
Stephen Kelly <steveire@gmail.com>
2010-05-03 16:05:16 +0000

    Add some docs.
    svn path=/trunk/KDE/kdepim/runtime/; revision=1122338

--- qml/kde/tests/qmlbreadcrumbnavigation/kmodelindexproxymapper.h
+++ qml/kde/tests/qmlbreadcrumbnavigation/kmodelindexproxymapper.h
@@ -29,15 +29,78 @@ class QModelIndex;
 class QItemSelection;
 class KModelIndexProxyMapperPrivate;
 
+/**
+ * @brief This class facilitates easy mapping of indexes and selections through proxy models.
+ *
+ * In a complex system of proxy models there can be a need to map indexes and selections between them,
+ * and sometimes to do so without knowledge of the path from one model to another.
+ *
+ * For example,
+ *
+ * @verbatim
+ *     Root model
+ *         |
+ *       /    \
+ *   Proxy 1   Proxy 3
+ *      |       |
+ *   Proxy 2   Proxy 4
+ * @endverbatim
+ *
+ * If there is a need to map indexes between proxy 2 and proxy 4, a KModelIndexProxyMapper can be created
+ * to facilitate mapping of indexes between them.
+ *
+ * @code
+ *   m_indexMapper = new KModelIndexProxyMapper(proxy2, proxy4, this);
+ *
+ *  ...
+ *
+ *   const QModelIndex proxy4Index = m_mapLeftToRight(proxy2->index(0, 0));
+ *   Q_ASSERT(proxy4Index.model() == proxy4);
+ * @endcode
+ *
+ * Note that the aim is to achieve black box connections so that there is no need for application code to
+ * know the structure of proxy models in the path between left and right and attempt to manually map them.
+ *
+ * @verbatim
+ *     Root model
+ *         |
+ *   ---------------
+ *   |  Black Box  |
+ *   ---------------
+ *      |       |
+ *   Proxy 2   Proxy 4
+ * @endverbatim
+ *
+ * @author Stephen Kelly <steveire@gmail.com>
+ *
+ */
 class KModelIndexProxyMapper : public QObject
 {
   Q_OBJECT
 public:
+  /**
+   * Constructor
+   */
   KModelIndexProxyMapper(QAbstractItemModel *leftModel, QAbstractItemModel *rightModel, QObject* parent = 0);
 
+  /**
+   * Maps the @p index from the left model to the right model.
+   */
   QModelIndex mapLeftToRight(const QModelIndex &index);
+
+  /**
+   * Maps the @p index from the right model to the left model.
+   */
   QModelIndex mapRightToLeft(const QModelIndex &index);
+
+  /**
+   * Maps the @p selection from the left model to the right model.
+   */
   QItemSelection mapSelectionLeftToRight(const QItemSelection &selection);
+
+  /**
+   * Maps the @p selection from the right model to the left model.
+   */
   QItemSelection mapSelectionRightToLeft(const QItemSelection &selection);
 
 private:
