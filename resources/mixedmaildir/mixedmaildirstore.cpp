acdda3b1108e0628e6a75bc71e627263368b3d71
Kevin Krammer <kevin.krammer@gmx.at>
2010-07-31 17:56:51 +0000

    Fix collection remote revision handling when compacting store
    svn path=/branches/KDE/4.5/kdepim/runtime/; revision=1157619

--- resources/mixedmaildir/mixedmaildirstore.cpp
+++ resources/mixedmaildir/mixedmaildirstore.cpp
@@ -608,6 +608,10 @@ void MixedMaildirStore::Private::fillMBoxCollectionDetails( const MBoxPtr &mbox,
   } else {
     collection.setRights( Collection::ReadOnly );
   }
+
+  if ( mbox->mRevision > 0 ) {
+    collection.setRemoteRevision( QString::number( mbox->mRevision ) );
+  }
 }
 
 void MixedMaildirStore::Private::fillMaildirCollectionDetails( const Maildir &md, Collection &collection )
@@ -861,7 +865,7 @@ void MixedMaildirStore::Private::updateContextHashes( const QString &oldPath, co
     if ( key == oldPath ) {
       key = newPath;
     } else if ( key.startsWith( oldSubDirPath ) ) {
-      if ( mboxPtr->hasIndexData() ) {
+      if ( mboxPtr->hasIndexData() || mboxPtr->mRevision > 0 ) {
         key.replace( oldSubDirPath, newSubDirPath );
       } else {
         // if there is no index data yet, just discard this context
@@ -869,7 +873,7 @@ void MixedMaildirStore::Private::updateContextHashes( const QString &oldPath, co
       }
     }
 
-    if ( !key.isEmpty() ){
+    if ( !key.isEmpty() ) {
       mboxPtr->updatePath( key );
       mboxes.insert( key, mboxPtr );
     }
@@ -2098,7 +2102,15 @@ bool MixedMaildirStore::Private::visit( FileStore::StoreCompactJob *job )
         kDebug() << "purge of" << mbox->mCollection.name() << "caused item move: oldRevision="
                 << revision << "(stored)," << mbox->mRevision << "(local)";
         revision = qMax( revision, mbox->mRevision ) + 1;
-        mbox->mCollection.setRemoteRevision( QString::number( revision ) );
+
+        const QString remoteRevision = QString::number( revision );
+
+        Collection collection = mbox->mCollection;
+        collection.attribute<FileStore::EntityCompactChangeAttribute>( Entity::AddIfMissing )->setRemoteRevision( remoteRevision );
+
+        q->notifyCollectionsProcessed( Collection::List() << collection );
+
+        mbox->mCollection.setRemoteRevision( remoteRevision );
         mbox->mRevision = revision;
       }
 
