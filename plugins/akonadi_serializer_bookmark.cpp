d3ec5e710d36547cda665f1492ac4445cab597a3
Volker Krause <vkrause@kde.org>
2010-08-11 06:44:01 +0000

    unbreak build
    svn path=/trunk/KDE/kdepim/runtime/; revision=1161903

--- plugins/akonadi_serializer_bookmark.cpp
+++ plugins/akonadi_serializer_bookmark.cpp
@@ -29,7 +29,9 @@
 
 using namespace Akonadi;
 
+#ifndef KIO_KBOOKMARK_METATYPE_DEFINED
 Q_DECLARE_METATYPE( KBookmark )
+#endif
 
 bool SerializerPluginBookmark::deserialize( Item& item, const QByteArray& label, QIODevice& data, int version )
 {
