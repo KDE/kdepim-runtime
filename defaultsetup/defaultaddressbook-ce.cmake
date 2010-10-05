99880db5ebd4d42c1cb3aa6c53e7cad984bc511b
Andreas Holzammer <andy@kdab.com>
2010-10-05 08:14:04 +0000

    - add default resources for wince
    svn path=/trunk/KDE/kdepim/runtime/; revision=1182621

---
+++ defaultsetup/defaultaddressbook-ce.cmake
@@ -0,0 +1,7 @@
+[Agent]
+Id=defaultaddressbook
+Type=akonadi_contacts_resource
+Name=Personal Contacts
+
+[Settings]
+Path[$e]=/My Documents/.local/share/contacts/
---
+++ defaultsetup/defaultcalendar-ce.cmake
@@ -0,0 +1,8 @@
+[Agent]
+Id=defaultcalendar
+Type=akonadi_ical_resource
+Name=Personal Calendar
+
+[Settings]
+Path[$e]=/My Documents/.local/share/korganizer/std.ics
+
