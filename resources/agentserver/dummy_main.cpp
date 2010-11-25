953aae71df01deb481be99059048370ffb7d1520
Volker Krause <vkrause@kde.org>
2010-11-25 09:40:57 +0000

    Add akonotes to the WinCE agent server.
    svn path=/trunk/KDE/kdepim/runtime/; revision=1200517

--- resources/agentserver/dummy_main.cpp
+++ resources/agentserver/dummy_main.cpp
@@ -22,6 +22,7 @@
 Q_IMPORT_PLUGIN(akonadi_ical_resource)
 Q_IMPORT_PLUGIN(akonadi_vcard_resource)
 Q_IMPORT_PLUGIN(akonadi_maildir_resource)
+Q_IMPORT_PLUGIN(akonadi_akonotes_resource)
 
 Q_IMPORT_PLUGIN(akonadi_serializer_mail)
 Q_IMPORT_PLUGIN(akonadi_serializer_addressee)
