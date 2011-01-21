601e0ceecd82963ee9b8945a097010c12dc506bd
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-01-21 11:09:20 +0100

    do that for wince
    Revert "Try to fix layout of PersonalPage on Maemo"
    
    This reverts commit e55b43237391e04ef21846c40c6dcb3b6ad5fc39.

--- accountwizard/personaldatapage.cpp
+++ accountwizard/personaldatapage.cpp
@@ -34,10 +34,29 @@
 
 #include <KDebug>
 
+#include <QScrollArea>
+
 PersonalDataPage::PersonalDataPage(Dialog* parent) :
   Page( parent ), mSetupManager( parent->setupManager() )
 {
-  ui.setupUi( this );
+#ifdef _WIN32_WCE
+  // for mobile ui we put the page into a scroll area in case it's too big
+  QVBoxLayout *layout = new QVBoxLayout;
+  layout->setMargin( 0 );
+  setLayout( layout );
+
+  QScrollArea *scrollArea = new QScrollArea( this );
+  scrollArea->setFrameShape( QFrame::NoFrame );
+  scrollArea->setWidgetResizable( true );
+  layout->addWidget( scrollArea );
+
+  QWidget *pageParent = new QWidget;
+  scrollArea->setWidget( pageParent );
+#else
+  QWidget *pageParent = this;
+#endif
+
+  ui.setupUi( pageParent );
 
   KPIMUtils::EmailValidator* emailValidator = new KPIMUtils::EmailValidator( this );
   ui.emailEdit->setValidator( emailValidator );
