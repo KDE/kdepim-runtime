fc61f33136716c627e2207506a3b752c3985b79c
Andre Heinecke <aheinecke@intevation.de>
2011-04-11 14:49:05 +0000

    Change ~ in Path to $HOME
        This commit should not change the location. On Windows
        ~ is not expanded by KDE, $HOME is expanded correctly making
        this change neccessary.
        $HOME is also used in the other defaults.

--- defaultsetup/defaultcalendar.cmake
+++ defaultsetup/defaultcalendar.cmake
@@ -4,5 +4,5 @@ Type=akonadi_ical_resource
 Name=Personal Calendar
 
 [Settings]
-Path[$e]=~/${KDE_DEFAULT_HOME}/share/apps/korganizer/std.ics
+Path[$e]=$HOME/${KDE_DEFAULT_HOME}/share/apps/korganizer/std.ics
 
