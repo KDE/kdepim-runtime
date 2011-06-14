a18477ae5902cc20e0ce54c961438ef2874a727f
Thiago Macieira <thiago@kde.org>
2011-06-14 17:36:28 +0200

    Protect the Add and Change item tasks from servers without UIDNEXT
    Copy the codepath from the Move Item Task code.
    
    This allows the IMAP agent to start for me.
    
    Review-Board: https://git.reviewboard.kde.org/r/101614/

--- resources/imap/changeitemtask.cpp
+++ resources/imap/changeitemtask.cpp
@@ -208,6 +208,11 @@ void ChangeItemTask::triggerSearchJob()
     search->addSearchCriteria( KIMAP::SearchJob::New );
 
     UidNextAttribute *uidNext = item().parentCollection().attribute<UidNextAttribute>();
+    if ( !uidNext ){
+      cancelTask( i18n("Could not determine the UID for the newly created message on the server") );
+      search->deleteLater();
+      return;
+    }
     KIMAP::ImapInterval interval( uidNext->uidNext() );
 
     search->addSearchCriteria( KIMAP::SearchJob::Uid, interval.toImapSequence() );
