297e34c90e202f4c80780c355359b280137d48c9
Alessandro Cosentino <cosenal@gmail.com>
2011-06-28 21:56:54 -0400

    base from tutorial

---
+++ krsslocal/krsslocalresource.cpp
@@ -0,0 +1,113 @@
+#include "krsslocalresource.h"
+
+#include "settings.h"
+#include "settingsadaptor.h"
+
+#include <QtDBus/QDBusConnection>
+
+using namespace Akonadi;
+
+krsslocalResource::krsslocalResource( const QString &id )
+  : ResourceBase( id )
+{
+  new SettingsAdaptor( Settings::self() );
+  QDBusConnection::sessionBus().registerObject( QLatin1String( "/Settings" ),
+                            Settings::self(), QDBusConnection::ExportAdaptors );
+
+  // TODO: you can put any resource specific initialization code here.
+}
+
+krsslocalResource::~krsslocalResource()
+{
+}
+
+void krsslocalResource::retrieveCollections()
+{
+  // TODO: this method is called when Akonadi wants to have all the
+  // collections your resource provides.
+  // Be sure to set the remote ID and the content MIME types
+}
+
+void krsslocalResource::retrieveItems( const Akonadi::Collection &collection )
+{
+  Q_UNUSED( collection );
+
+  // TODO: this method is called when Akonadi wants to know about all the
+  // items in the given collection. You can but don't have to provide all the
+  // data for each item, remote ID and MIME type are enough at this stage.
+  // Depending on how your resource accesses the data, there are several
+  // different ways to tell Akonadi when you are done.
+}
+
+bool krsslocalResource::retrieveItem( const Akonadi::Item &item, const QSet<QByteArray> &parts )
+{
+  Q_UNUSED( item );
+  Q_UNUSED( parts );
+
+  // TODO: this method is called when Akonadi wants more data for a given item.
+  // You can only provide the parts that have been requested but you are allowed
+  // to provide all in one go
+
+  return true;
+}
+
+void krsslocalResource::aboutToQuit()
+{
+  // TODO: any cleanup you need to do while there is still an active
+  // event loop. The resource will terminate after this method returns
+}
+
+void krsslocalResource::configure( WId windowId )
+{
+  Q_UNUSED( windowId );
+
+  // TODO: this method is usually called when a new resource is being
+  // added to the Akonadi setup. You can do any kind of user interaction here,
+  // e.g. showing dialogs.
+  // The given window ID is usually useful to get the correct
+  // "on top of parent" behavior if the running window manager applies any kind
+  // of focus stealing prevention technique
+  //
+  // If the configuration dialog has been accepted by the user by clicking Ok,
+  // the signal configurationDialogAccepted() has to be emitted, otherwise, if
+  // the user canceled the dialog, configurationDialogRejected() has to be emitted.
+}
+
+void krsslocalResource::itemAdded( const Akonadi::Item &item, const Akonadi::Collection &collection )
+{
+  Q_UNUSED( item );
+  Q_UNUSED( collection );
+
+  // TODO: this method is called when somebody else, e.g. a client application,
+  // has created an item in a collection managed by your resource.
+
+  // NOTE: There is an equivalent method for collections, but it isn't part
+  // of this template code to keep it simple
+}
+
+void krsslocalResource::itemChanged( const Akonadi::Item &item, const QSet<QByteArray> &parts )
+{
+  Q_UNUSED( item );
+  Q_UNUSED( parts );
+
+  // TODO: this method is called when somebody else, e.g. a client application,
+  // has changed an item managed by your resource.
+
+  // NOTE: There is an equivalent method for collections, but it isn't part
+  // of this template code to keep it simple
+}
+
+void krsslocalResource::itemRemoved( const Akonadi::Item &item )
+{
+  Q_UNUSED( item );
+
+  // TODO: this method is called when somebody else, e.g. a client application,
+  // has deleted an item managed by your resource.
+
+  // NOTE: There is an equivalent method for collections, but it isn't part
+  // of this template code to keep it simple
+}
+
+AKONADI_RESOURCE_MAIN( krsslocalResource )
+
+#include "krsslocalresource.moc"
---
+++ krsslocal/krsslocalresource.h
@@ -0,0 +1,31 @@
+#ifndef KRSSLOCALRESOURCE_H
+#define KRSSLOCALRESOURCE_H
+
+#include <akonadi/resourcebase.h>
+
+class krsslocalResource : public Akonadi::ResourceBase,
+                           public Akonadi::AgentBase::Observer
+{
+  Q_OBJECT
+
+  public:
+    krsslocalResource( const QString &id );
+    ~krsslocalResource();
+
+  public Q_SLOTS:
+    virtual void configure( WId windowId );
+
+  protected Q_SLOTS:
+    void retrieveCollections();
+    void retrieveItems( const Akonadi::Collection &col );
+    bool retrieveItem( const Akonadi::Item &item, const QSet<QByteArray> &parts );
+
+  protected:
+    virtual void aboutToQuit();
+
+    virtual void itemAdded( const Akonadi::Item &item, const Akonadi::Collection &collection );
+    virtual void itemChanged( const Akonadi::Item &item, const QSet<QByteArray> &parts );
+    virtual void itemRemoved( const Akonadi::Item &item );
+};
+
+#endif
