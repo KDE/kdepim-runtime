e01298b9a72caeb7068e5f70e94a1cc88293dffd
Martin Klapetek <martin.klapetek@gmail.com>
2011-08-29 22:23:11 +0200

    Contact feeder port to Nepomuk DMS
    REVIEW: 102084

--- agents/nepomuk_contact_feeder/nepomukcontactfeeder.h
+++ agents/nepomuk_contact_feeder/nepomukcontactfeeder.h
@@ -1,45 +1,46 @@
 /*
-    Copyright (c) 2007 Tobias Koenig <tokoe@kde.org>
-                  2008 Sebastian Trueg <trueg@kde.org>
-
-    This library is free software; you can redistribute it and/or modify it
-    under the terms of the GNU Library General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or (at your
-    option) any later version.
-
-    This library is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
-    License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to the
-    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-    02110-1301, USA.
-*/
+ *    Copyright (c) 2007 Tobias Koenig <tokoe@kde.org>
+ *                  2008 Sebastian Trueg <trueg@kde.org>
+ *                  2011 Martin Klapetek <martin.klapetek@gmail.com>
+ *
+ *    This library is free software; you can redistribute it and/or modify it
+ *    under the terms of the GNU Library General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or (at your
+ *    option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful, but WITHOUT
+ *    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ *    License for more details.
+ *
+ *    You should have received a copy of the GNU Library General Public License
+ *    along with this library; see the file COPYING.LIB.  If not, write to the
+ *    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *    02110-1301, USA.
+ */
 
 #ifndef AKONADI_NEPOMUK_CONTACT_FEEDER_H
 #define AKONADI_NEPOMUK_CONTACT_FEEDER_H
 
-#include <nepomukfeederagent.h>
-#include "contactlist.h"
+#include <nepomukfeederagentbase.h>
 #include <akonadi/agentbase.h>
 #include <akonadi/item.h>
 
 namespace Akonadi {
 
-class NepomukContactFeeder : public NepomukFeederAgent<NepomukFast::ContactList>
-{
-  Q_OBJECT
-  public:
-    NepomukContactFeeder( const QString &id );
-    ~NepomukContactFeeder();
+    class NepomukContactFeeder : public NepomukFeederAgentBase
+    {
+        Q_OBJECT
+    public:
+        NepomukContactFeeder( const QString &id );
+        ~NepomukContactFeeder();
 
-  private:
-    void updateItem( const Akonadi::Item &item, const QUrl &graphUri );
-    void updateContactItem( const Akonadi::Item &item, const QUrl& );
-    void updateGroupItem( const Akonadi::Item &item, const QUrl& );
-};
+    private:
+
+        void updateItem( const Akonadi::Item &item, Nepomuk::SimpleResource &res, Nepomuk::SimpleResourceGraph &graph );
+        void updateContactItem( const Akonadi::Item &item, Nepomuk::SimpleResource &res, Nepomuk::SimpleResourceGraph &graph );
+        void updateGroupItem( const Akonadi::Item &item, Nepomuk::SimpleResource &res, Nepomuk::SimpleResourceGraph &graph );
+    };
 
 }
 
