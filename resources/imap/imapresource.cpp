5d343b97144dbe2a371837111f950dfbdf1fe9cb
Sergio Martins <iamsergio@gmail.com>
2011-11-14 20:30:02 +0000

    React to network changes.
    setNeedsNetwork() wasn't being called.
    
    REVIEW: 103112

--- resources/imap/imapresource.cpp
+++ resources/imap/imapresource.cpp
@@ -148,6 +148,7 @@ ImapResource::ImapResource( const QString &id )
 
   Settings::self(); // make sure the D-Bus settings interface is up
   new ResourceAdaptor( this );
+  setNeedsNetwork( needsNetwork() );
 }
 
 ImapResource::~ImapResource()
