462e71398da5dc77a39d4e0eb9f65990023858bf
Grégory Oestreicher <greg@kamago.net>
2013-03-16 09:29:45 +0100

    Call emitResult() in one place for DavCollectionsFetchJob

--- resources/dav/common/davcollectionsfetchjob.cpp
+++ resources/dav/common/davcollectionsfetchjob.cpp
@@ -127,230 +127,221 @@ void DavCollectionsFetchJob::collectionsFetchFinished( KJob *job )
       doCollectionsFetch( mUrl.url() );
       return;
     }
+    else if ( !mSubJobSuccessful ) {
+      if ( DavUtils::httpRequestRetryable( responseCode ) )
+        mHasTemporaryError = true;
 
-    if ( DavUtils::httpRequestRetryable( responseCode ) )
-      mHasTemporaryError = true;
-
-    setError( UserDefinedError + responseCode );
-    setErrorText( i18n( "There was a problem with the request.\n"
-                        "%1 (%2).", davJob->errorString(), responseCode ) );
-
-    if ( mSubJobCount == 0 )
-      emitResult();
-    return;
+      setError( UserDefinedError + responseCode );
+      setErrorText( i18n( "There was a problem with the request.\n"
+                          "%1 (%2).", davJob->errorString(), responseCode ) );
+    }
   }
-
-  if ( !mSubJobSuccessful ) {
-    setError( 0 ); // nope, everything went fine if we're here
-    mSubJobSuccessful = true;
-    mHasTemporaryError = false;
-  }
-
-  // For use in the collectionDiscovered() signal
-  KUrl _jobUrl = mUrl.url();
-  _jobUrl.setUser( QString() );
-  const QString jobUrl = _jobUrl.prettyUrl();
-
-  //kDebug() << davJob->response().toString();
-
-  QByteArray resp( davJob->response().toByteArray() );
-  QBuffer buffer( &resp );
-  buffer.open( QIODevice::ReadOnly );
-
-  QXmlQuery xquery;
-  if ( !xquery.setFocus( &buffer ) ) {
-    setError( UserDefinedError );
-    setErrorText( i18n( "Error setting focus for XQuery" ) );
-    if ( mSubJobCount == 0 )
-      emitResult();
-    return;
-  }
-
-  xquery.setQuery( DavManager::self()->davProtocol( mUrl.protocol() )->collectionsXQuery() );
-  if ( !xquery.isValid() ) {
-    setError( UserDefinedError );
-    setErrorText( i18n( "Invalid XQuery submitted by DAV implementation" ) );
-    if ( mSubJobCount == 0 )
-      emitResult();
-    return;
-  }
-
-  QString responsesStr;
-  xquery.evaluateTo( &responsesStr );
-  responsesStr.prepend( "<responses>" );
-  responsesStr.append( "</responses>" );
-
-  QDomDocument document;
-  if ( !document.setContent( responsesStr, true ) ) {
-    setError( UserDefinedError );
-    setErrorText( i18n( "Invalid responses from backend" ) );
-    if ( mSubJobCount == 0 )
-      emitResult();
-    return;
-  }
-
-  /*
-   * Extract information from a document like the following:
-   *
-   * <responses>
-   *   <response xmlns="DAV:">
-   *     <href xmlns="DAV:">/caldav.php/test1.user/home/</href>
-   *     <propstat xmlns="DAV:">
-   *       <prop xmlns="DAV:">
-   *         <C:supported-calendar-component-set xmlns:C="urn:ietf:params:xml:ns:caldav">
-   *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VEVENT"/>
-   *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VTODO"/>
-   *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VJOURNAL"/>
-   *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VTIMEZONE"/>
-   *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VFREEBUSY"/>
-   *         </C:supported-calendar-component-set>
-   *         <resourcetype xmlns="DAV:">
-   *           <collection xmlns="DAV:"/>
-   *           <C:calendar xmlns:C="urn:ietf:params:xml:ns:caldav"/>
-   *           <C:schedule-calendar xmlns:C="urn:ietf:params:xml:ns:caldav"/>
-   *         </resourcetype>
-   *         <displayname xmlns="DAV:">Test1 User</displayname>
-   *         <current-user-privilege-set xmlns="DAV:">
-   *           <privilege xmlns="DAV:">
-   *             <read xmlns="DAV:"/>
-   *           </privilege>
-   *         </current-user-privilege-set>
-   *       </prop>
-   *       <status xmlns="DAV:">HTTP/1.1 200 OK</status>
-   *     </propstat>
-   *   </response>
-   * </responses>
-   */
-
-  const QDomElement responsesElement = document.documentElement();
-
-  QDomElement responseElement = DavUtils::firstChildElementNS( responsesElement, "DAV:", "response" );
-  while ( !responseElement.isNull() ) {
-
-    QDomElement propstatElement;
-
-    // check for the valid propstat, without giving up on first error
-    {
-      const QDomNodeList propstats = responseElement.elementsByTagNameNS( "DAV:", "propstat" );
-      for ( uint i = 0; i < propstats.length(); ++i ) {
-        const QDomElement propstatCandidate = propstats.item( i ).toElement();
-        const QDomElement statusElement = DavUtils::firstChildElementNS( propstatCandidate, "DAV:", "status" );
-        if ( statusElement.text().contains( "200" ) ) {
-          propstatElement = propstatCandidate;
-        }
-      }
+  else {
+    if ( !mSubJobSuccessful ) {
+      setError( 0 ); // nope, everything went fine if we're here
+      mSubJobSuccessful = true;
+      mHasTemporaryError = false;
     }
 
-    if ( propstatElement.isNull() ) {
-      responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
-      continue;
+    // For use in the collectionDiscovered() signal
+    KUrl _jobUrl = mUrl.url();
+    _jobUrl.setUser( QString() );
+    const QString jobUrl = _jobUrl.prettyUrl();
+
+    //kDebug() << davJob->response().toString();
+
+    QByteArray resp( davJob->response().toByteArray() );
+    QBuffer buffer( &resp );
+    buffer.open( QIODevice::ReadOnly );
+
+    QXmlQuery xquery;
+    if ( !xquery.setFocus( &buffer ) ) {
+      setError( UserDefinedError );
+      setErrorText( i18n( "Error setting focus for XQuery" ) );
     }
 
-    // extract url
-    const QDomElement hrefElement = DavUtils::firstChildElementNS( responseElement, "DAV:", "href" );
-    if ( hrefElement.isNull() ) {
-      responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
-      continue;
+    xquery.setQuery( DavManager::self()->davProtocol( mUrl.protocol() )->collectionsXQuery() );
+    if ( !xquery.isValid() ) {
+      setError( UserDefinedError );
+      setErrorText( i18n( "Invalid XQuery submitted by DAV implementation" ) );
     }
 
-    QString href = hrefElement.text();
-    if ( !href.endsWith( '/' ) )
-      href.append( '/' );
+    QString responsesStr;
+    xquery.evaluateTo( &responsesStr );
+    responsesStr.prepend( "<responses>" );
+    responsesStr.append( "</responses>" );
 
-    KUrl url = davJob->url();
-    url.setUser( QString() );
-    if ( href.startsWith( '/' ) ) {
-      // href is only a path, use request url to complete
-      url.setEncodedPath( href.toLatin1() );
-    } else {
-      // href is a complete url
-      KUrl tmpUrl( href );
-      url = tmpUrl;
+    QDomDocument document;
+    if ( !document.setContent( responsesStr, true ) ) {
+      setError( UserDefinedError );
+      setErrorText( i18n( "Invalid responses from backend" ) );
     }
 
-    // don't add this resource if it has already been detected
-    bool alreadySeen = false;
-    foreach ( const DavCollection &seen, mCollections ) {
-      kDebug() << seen.url() << url.prettyUrl();
-      if ( seen.url() == url.prettyUrl() )
-        alreadySeen = true;
-    }
-    if ( alreadySeen ) {
-      responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
-      continue;
-    }
+    if ( !error() ) {
+      /*
+       * Extract information from a document like the following:
+       *
+       * <responses>
+       *   <response xmlns="DAV:">
+       *     <href xmlns="DAV:">/caldav.php/test1.user/home/</href>
+       *     <propstat xmlns="DAV:">
+       *       <prop xmlns="DAV:">
+       *         <C:supported-calendar-component-set xmlns:C="urn:ietf:params:xml:ns:caldav">
+       *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VEVENT"/>
+       *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VTODO"/>
+       *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VJOURNAL"/>
+       *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VTIMEZONE"/>
+       *           <C:comp xmlns:C="urn:ietf:params:xml:ns:caldav" name="VFREEBUSY"/>
+       *         </C:supported-calendar-component-set>
+       *         <resourcetype xmlns="DAV:">
+       *           <collection xmlns="DAV:"/>
+       *           <C:calendar xmlns:C="urn:ietf:params:xml:ns:caldav"/>
+       *           <C:schedule-calendar xmlns:C="urn:ietf:params:xml:ns:caldav"/>
+       *         </resourcetype>
+       *         <displayname xmlns="DAV:">Test1 User</displayname>
+       *         <current-user-privilege-set xmlns="DAV:">
+       *           <privilege xmlns="DAV:">
+       *             <read xmlns="DAV:"/>
+       *           </privilege>
+       *         </current-user-privilege-set>
+       *       </prop>
+       *       <status xmlns="DAV:">HTTP/1.1 200 OK</status>
+       *     </propstat>
+       *   </response>
+       * </responses>
+       */
 
-    // extract display name
-    const QDomElement propElement = DavUtils::firstChildElementNS( propstatElement, "DAV:", "prop" );
-    const QDomElement displaynameElement = DavUtils::firstChildElementNS( propElement, "DAV:", "displayname" );
-    const QString displayName = displaynameElement.text();
+      const QDomElement responsesElement = document.documentElement();
 
-    // extract calendar color if provided
-    const QDomElement colorElement = DavUtils::firstChildElementNS( propElement, "http://apple.com/ns/ical/", "calendar-color" );
-    QColor color;
-    if ( !colorElement.isNull() ) {
-      QString colorValue = colorElement.text();
-      if ( QColor::isValidColor( colorValue ) )
-        color.setNamedColor( colorValue );
-    }
+      QDomElement responseElement = DavUtils::firstChildElementNS( responsesElement, "DAV:", "response" );
+      while ( !responseElement.isNull() ) {
 
-    // extract allowed content types
-    const DavCollection::ContentTypes contentTypes = DavManager::self()->davProtocol( mUrl.protocol() )->collectionContentTypes( propstatElement );
+        QDomElement propstatElement;
 
-    DavCollection collection( mUrl.protocol(), url.prettyUrl(), displayName, contentTypes );
-    if ( color.isValid() )
-      collection.setColor( color );
-
-    // extract privileges
-    const QDomElement currentPrivsElement = DavUtils::firstChildElementNS( propElement, "DAV:", "current-user-privilege-set" );
-    if ( currentPrivsElement.isNull() ) {
-      // Assume that we have all privileges
-      collection.setPrivileges( DavCollection::All );
-    } else {
-      QDomElement privElement = DavUtils::firstChildElementNS( currentPrivsElement, "DAV:", "privilege" );
-      DavCollection::Privileges privileges = DavCollection::None;
-      while ( !privElement.isNull() ) {
-        QDomElement child = privElement.firstChildElement();
-
-        while ( !child.isNull() ) {
-          const QString privname = child.localName();
-
-          if ( privname == "read" )
-            privileges |= DavCollection::Read;
-          else if ( privname == "write" )
-            privileges |= DavCollection::Write;
-          else if ( privname == "write-properties" )
-            privileges |= DavCollection::WriteProperties;
-          else if ( privname == "write-content" )
-            privileges |= DavCollection::WriteContent;
-          else if ( privname == "unlock" )
-            privileges |= DavCollection::Unlock;
-          else if ( privname == "read-acl" )
-            privileges |= DavCollection::ReadAcl;
-          else if ( privname == "read-current-user-privilege-set" )
-            privileges |= DavCollection::ReadCurrentUserPrivilegeSet;
-          else if ( privname == "write-acl" )
-            privileges |= DavCollection::WriteAcl;
-          else if ( privname == "bind" )
-            privileges |= DavCollection::Bind;
-          else if ( privname == "unbind" )
-            privileges |= DavCollection::Unbind;
-          else if ( privname == "all" )
-            privileges |= DavCollection::All;
-
-          child = child.nextSiblingElement();
+        // check for the valid propstat, without giving up on first error
+        {
+          const QDomNodeList propstats = responseElement.elementsByTagNameNS( "DAV:", "propstat" );
+          for ( uint i = 0; i < propstats.length(); ++i ) {
+            const QDomElement propstatCandidate = propstats.item( i ).toElement();
+            const QDomElement statusElement = DavUtils::firstChildElementNS( propstatCandidate, "DAV:", "status" );
+            if ( statusElement.text().contains( "200" ) ) {
+              propstatElement = propstatCandidate;
+            }
+          }
         }
 
-        privElement = DavUtils::nextSiblingElementNS( privElement, "DAV:", "privilege" );
+        if ( propstatElement.isNull() ) {
+          responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
+          continue;
+        }
+
+        // extract url
+        const QDomElement hrefElement = DavUtils::firstChildElementNS( responseElement, "DAV:", "href" );
+        if ( hrefElement.isNull() ) {
+          responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
+          continue;
+        }
+
+        QString href = hrefElement.text();
+        if ( !href.endsWith( '/' ) )
+          href.append( '/' );
+
+        KUrl url = davJob->url();
+        url.setUser( QString() );
+        if ( href.startsWith( '/' ) ) {
+          // href is only a path, use request url to complete
+          url.setEncodedPath( href.toLatin1() );
+        } else {
+          // href is a complete url
+          KUrl tmpUrl( href );
+          url = tmpUrl;
+        }
+
+        // don't add this resource if it has already been detected
+        bool alreadySeen = false;
+        foreach ( const DavCollection &seen, mCollections ) {
+          kDebug() << seen.url() << url.prettyUrl();
+          if ( seen.url() == url.prettyUrl() )
+            alreadySeen = true;
+        }
+        if ( alreadySeen ) {
+          responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
+          continue;
+        }
+
+        // extract display name
+        const QDomElement propElement = DavUtils::firstChildElementNS( propstatElement, "DAV:", "prop" );
+        const QDomElement displaynameElement = DavUtils::firstChildElementNS( propElement, "DAV:", "displayname" );
+        const QString displayName = displaynameElement.text();
+
+        // extract calendar color if provided
+        const QDomElement colorElement = DavUtils::firstChildElementNS( propElement, "http://apple.com/ns/ical/", "calendar-color" );
+        QColor color;
+        if ( !colorElement.isNull() ) {
+          QString colorValue = colorElement.text();
+          if ( QColor::isValidColor( colorValue ) )
+            color.setNamedColor( colorValue );
+        }
+
+        // extract allowed content types
+        const DavCollection::ContentTypes contentTypes = DavManager::self()->davProtocol( mUrl.protocol() )->collectionContentTypes( propstatElement );
+
+        DavCollection collection( mUrl.protocol(), url.prettyUrl(), displayName, contentTypes );
+        if ( color.isValid() )
+          collection.setColor( color );
+
+        // extract privileges
+        const QDomElement currentPrivsElement = DavUtils::firstChildElementNS( propElement, "DAV:", "current-user-privilege-set" );
+        if ( currentPrivsElement.isNull() ) {
+          // Assume that we have all privileges
+          collection.setPrivileges( DavCollection::All );
+        } else {
+          QDomElement privElement = DavUtils::firstChildElementNS( currentPrivsElement, "DAV:", "privilege" );
+          DavCollection::Privileges privileges = DavCollection::None;
+          while ( !privElement.isNull() ) {
+            QDomElement child = privElement.firstChildElement();
+
+            while ( !child.isNull() ) {
+              const QString privname = child.localName();
+
+              if ( privname == "read" )
+                privileges |= DavCollection::Read;
+              else if ( privname == "write" )
+                privileges |= DavCollection::Write;
+              else if ( privname == "write-properties" )
+                privileges |= DavCollection::WriteProperties;
+              else if ( privname == "write-content" )
+                privileges |= DavCollection::WriteContent;
+              else if ( privname == "unlock" )
+                privileges |= DavCollection::Unlock;
+              else if ( privname == "read-acl" )
+                privileges |= DavCollection::ReadAcl;
+              else if ( privname == "read-current-user-privilege-set" )
+                privileges |= DavCollection::ReadCurrentUserPrivilegeSet;
+              else if ( privname == "write-acl" )
+                privileges |= DavCollection::WriteAcl;
+              else if ( privname == "bind" )
+                privileges |= DavCollection::Bind;
+              else if ( privname == "unbind" )
+                privileges |= DavCollection::Unbind;
+              else if ( privname == "all" )
+                privileges |= DavCollection::All;
+
+              child = child.nextSiblingElement();
+            }
+
+            privElement = DavUtils::nextSiblingElementNS( privElement, "DAV:", "privilege" );
+          }
+          collection.setPrivileges( privileges );
+        }
+
+        kDebug() << url.prettyUrl() << "PRIVS: " << collection.privileges();
+        mCollections << collection;
+        emit collectionDiscovered( mUrl.protocol(), url.prettyUrl(), jobUrl );
+
+        responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
       }
-      collection.setPrivileges( privileges );
     }
-
-    kDebug() << url.prettyUrl() << "PRIVS: " << collection.privileges();
-    mCollections << collection;
-    emit collectionDiscovered( mUrl.protocol(), url.prettyUrl(), jobUrl );
-
-    responseElement = DavUtils::nextSiblingElementNS( responseElement, "DAV:", "response" );
   }
 
   if ( mSubJobCount == 0 )
