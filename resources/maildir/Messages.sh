45d15855fc418ca2124a950a7956c00bb1d32522
Burkhard Lück <lueck@hube-lueck.de>
2013-04-26 14:18:39 +0200

    extract messages from libmaildir folder

--- resources/maildir/Messages.sh
+++ resources/maildir/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui *.kcfg >> rc.cpp
-$XGETTEXT *.cpp -o $podir/akonadi_maildir_resource.pot
+$XGETTEXT libmaildir/*.cpp *.cpp -o $podir/akonadi_maildir_resource.pot
