68310be708e41ec78af34e28c3df3d4b4648b6f2
Àlex Fiestas <afiestas@kde.org>
2013-06-10 18:38:07 +0200

    Update getCredentials version

--- resources/getcredentialsjob.cpp
+++ resources/getcredentialsjob.cpp
@@ -1,24 +1,23 @@
 /*************************************************************************************
  *  Copyright (C) 2013 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
  *                                                                                   *
- *  This program is free software; you can redistribute it and/or                    *
- *  modify it under the terms of the GNU General Public License                      *
- *  as published by the Free Software Foundation; either version 2                   *
- *  of the License, or (at your option) any later version.                           *
+ *  This library is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU Lesser General Public                       *
+ *  License as published by the Free Software Foundation; either                     *
+ *  version 2 of the License, or (at your option) any later version.                 *
  *                                                                                   *
- *  This program is distributed in the hope that it will be useful,                  *
+ *  This library is distributed in the hope that it will be useful,                  *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
- *  GNU General Public License for more details.                                     *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ *  Library General Public License for more details.                                 *
  *                                                                                   *
- *  You should have received a copy of the GNU General Public License                *
- *  along with this program; if not, write to the Free Software                      *
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *  You should have received a copy of the GNU Library General Public License        *
+ *  along with this library; see the file COPYING.LIB.  If not, write to             *
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,             *
+ *  Boston, MA 02110-1301, USA.                                                      *
  *************************************************************************************/
 
 #include "getcredentialsjob.h"
-
-#include <QtCore/QDebug>
 
 #include <Accounts/Manager>
 #include <Accounts/Account>
@@ -26,10 +25,12 @@
 
 #include <SignOn/Identity>
 
+#include <KDebug>
+
 GetCredentialsJob::GetCredentialsJob(const Accounts::AccountId& id, QObject* parent)
 : KJob(parent)
 , m_id(id)
-, m_manager(new Accounts::Manager())
+, m_manager(new Accounts::Manager(this))
 {
 
 }
@@ -46,13 +47,27 @@ void GetCredentialsJob::setServiceType(const QString& serviceType)
 
 void GetCredentialsJob::getCredentials()
 {
-    Accounts::Account* acc = m_manager->account(m_id);
+    Accounts::Account *acc = m_manager->account(m_id);
+    if (!acc) {
+        setError(-1);
+        setErrorText("Could not find account");
+        emitResult();
+        return;
+    }
     Accounts::AccountService *service = new Accounts::AccountService(acc, m_manager->service(m_serviceType), this);
 
     Accounts::AuthData authData = service->authData();
     m_authData = authData.parameters();
     SignOn::Identity* identity = SignOn::Identity::existingIdentity(authData.credentialsId(), this);
 
+    if (!identity) {
+        setError(-1);
+        setErrorText("Could not find credentials");
+        emitResult();
+        return;
+    }
+
+    m_authData["AccountUsername"] = acc->value("username").toString();
     QPointer<SignOn::AuthSession> authSession = identity->createSession(authData.method());
 
     connect(authSession, SIGNAL(response(SignOn::SessionData)),
@@ -60,25 +75,24 @@ void GetCredentialsJob::getCredentials()
     connect(authSession, SIGNAL(error(SignOn::Error)),
             SLOT(sessionError(SignOn::Error)));
 
-    qDebug() << authData.parameters();
-    qDebug() << authData.mechanism();
+    kDebug() << authData.parameters();
+    kDebug() << authData.mechanism();
     authSession->process(authData.parameters(), authData.mechanism());
-}
-
-void GetCredentialsJob::info(const SignOn::IdentityInfo& info)
-{
 }
 
 void GetCredentialsJob::sessionResponse(const SignOn::SessionData& data)
 {
-    qDebug() << data.toMap();
+    kDebug() << data.toMap();
     m_sessionData = data;
     emitResult();
 }
 
 void GetCredentialsJob::sessionError(const SignOn::Error& error)
 {
-    qDebug() << error.message();
+    kDebug() << error.message();
+    setError(-1);
+    setErrorText(error.message());
+    emitResult();
 }
 
 Accounts::AccountId GetCredentialsJob::accountId() const
