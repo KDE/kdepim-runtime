3f89894a2cf4f8f1287d223f40c60d0690022b8f
Montel Laurent <montel@kde.org>
2013-12-07 09:52:38 +0100

    Revert "fixed windows build"
    CCMAIL: vonreth@kde.org
    
    Please test on linux too before to commit.
    We will release 4.12 in 2 days so please wait after that to test to fix
    
    This reverts commit b2398dd3bf7094311e37b09ff6bb85a7c612c0d5.

--- agents/newmailnotifier/newmailnotifieragent.cpp
+++ agents/newmailnotifier/newmailnotifieragent.cpp
@@ -231,7 +231,7 @@ bool NewMailNotifierAgent::enabledAgent() const
 
 void NewMailNotifierAgent::showConfigureDialog(qlonglong windowId)
 {
-    configure( reinterpret_cast<WId>(windowId));
+    configure( windowId );
 }
 
 void NewMailNotifierAgent::configure( WId windowId )
