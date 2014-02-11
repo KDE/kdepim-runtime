fc2b48d20c6ce2f5bd0d0059b94fba6fa67115c3
Montel Laurent <montel@kde.org>
2014-02-11 13:56:47 +0100

    Move code for folderarchive settings as discussed with david

---
+++ resources/folderarchivesettings/folderarchivesettingpage.h
@@ -0,0 +1,67 @@
+/*
+  Copyright (c) 2013, 2014 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef FOLDERARCHIVESETTINGPAGE_H
+#define FOLDERARCHIVESETTINGPAGE_H
+
+#include "folderarchiveaccountinfo.h"
+#include "folderarchivesettings_export.h"
+#include <QWidget>
+#include <QComboBox>
+
+class QCheckBox;
+namespace Akonadi {
+class CollectionRequester;
+}
+
+class FolderArchiveComboBox : public QComboBox
+{
+    Q_OBJECT
+public:
+    explicit FolderArchiveComboBox(QWidget *parent = 0);
+    ~FolderArchiveComboBox();
+
+    void setType(FolderArchiveAccountInfo::FolderArchiveType type);
+    FolderArchiveAccountInfo::FolderArchiveType type() const;
+
+private:
+    void initialize();
+};
+
+class FolderArchiveAccountInfo;
+class FOLDERARCHIVESETTINGS_EXPORT FolderArchiveSettingPage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit FolderArchiveSettingPage(const QString &instanceName, QWidget *parent=0);
+    ~FolderArchiveSettingPage();
+
+    void loadSettings();
+    void writeSettings();
+
+private Q_SLOTS:
+    void slotEnableChanged(bool enabled);
+
+private:
+    QString mInstanceName;
+    QCheckBox *mEnabled;
+    FolderArchiveComboBox *mArchiveNamed;
+    Akonadi::CollectionRequester *mArchiveFolder;
+    FolderArchiveAccountInfo *mInfo;
+};
+
+#endif // FOLDERARCHIVESETTINGPAGE_H
---
+++ resources/folderarchivesettings/folderarchiveutil.cpp
@@ -0,0 +1,45 @@
+/*
+  Copyright (c) 2013, 2014 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "folderarchiveutil.h"
+
+#include <KConfig>
+#include <KConfigGroup>
+
+using namespace FolderArchive;
+
+QString FolderArchiveUtil::groupConfigPattern()
+{
+    return QLatin1String("FolderArchiveAccount ");
+}
+
+QString FolderArchiveUtil::configFileName()
+{
+    return QLatin1String(QLatin1String("foldermailarchiverc"));
+}
+
+bool FolderArchiveUtil::resourceSupportArchiving(const QString &resource)
+{
+    KConfig config(FolderArchiveUtil::configFileName());
+    if (config.hasGroup(groupConfigPattern() + resource)) {
+        KConfigGroup grp = config.group(groupConfigPattern() + resource);
+        if (grp.readEntry("enabled", false) && (grp.readEntry("topLevelCollectionId", -1) > 0) ) {
+            return true;
+        }
+    }
+    return false;
+}
