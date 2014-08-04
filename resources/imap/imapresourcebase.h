82480fcca7dcd1c6e481943efb8f9d4a1585461a
Montel Laurent <montel@kde.org>
2014-08-04 13:03:35 +0200

    Fix "server side subscription"

--- resources/imap/imapresourcebase.h
+++ resources/imap/imapresourcebase.h
@@ -49,10 +49,10 @@ class Settings;
 
 class ImapResourceBase : public Akonadi::ResourceBase,
                          public Akonadi::AgentBase::ObserverV3,
-                         public Akonadi::AgentSearchInterface
+                        public Akonadi::AgentSearchInterface
 {
   Q_OBJECT
-
+  Q_CLASSINFO("D-Bus Interface", "org.kde.Akonadi.ImapResourceBase")
 protected:
   using Akonadi::AgentBase::Observer::collectionChanged;
 
