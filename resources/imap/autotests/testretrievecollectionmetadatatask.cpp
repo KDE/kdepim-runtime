d81c1e7547df209b83dcb49bca913118549e58a1
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-04-22 15:37:12 +0200

    compile

--- resources/imap/autotests/testretrievecollectionmetadatatask.cpp
+++ resources/imap/autotests/testretrievecollectionmetadatatask.cpp
@@ -255,7 +255,6 @@ private slots:
 
     QTest::newRow( "no capabilities" ) << collection << capabilities << scenario
                                        << callNames << rights << expectedAnnotations;
-                                         << callNames << rights << expectedAnnotations;
     }
 
     void shouldCollectionRetrieveMetadata()
