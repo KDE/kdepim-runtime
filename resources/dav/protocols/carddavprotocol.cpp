09282b0489a5f84b6173e99daf24e3dc5540ae78
Grégory Oestreicher <greg@kamago.net>
2015-08-31 22:34:30 +0200

    Don't decode URLs found in responses
    BUG: 351652
    FIXED-IN: 15.08.1

--- resources/dav/protocols/carddavprotocol.cpp
+++ resources/dav/protocols/carddavprotocol.cpp
@@ -116,9 +116,14 @@ QDomDocument CarddavProtocol::itemsReportQuery(const QStringList &urls) const
 
     foreach (const QString &url, urls) {
         QDomElement hrefElement = document.createElementNS(QStringLiteral("DAV:"), QStringLiteral("href"));
-        const KUrl pathUrl(url);
+         const QUrl pathUrl = QUrl::fromUserInput(url);
+         QString encodedUrl = QString::fromAscii(pathUrl.encodedPath());
+         if ( pathUrl.hasQuery() ) {
+             encodedUrl.append(QStringLiteral("?"));
+             encodedUrl.append(QString::fromAscii(pathUrl.encodedQuery()));
+         }
 
-        const QDomText textNode = document.createTextNode(pathUrl.encodedPathAndQuery());
+        const QDomText textNode = document.createTextNode(encodedUrl);
         hrefElement.appendChild(textNode);
 
         multigetElement.appendChild(hrefElement);
