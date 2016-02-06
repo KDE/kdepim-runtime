fe23c35545d66bbf8fe6cae461de96663d5cf089
Grégory Oestreicher <greg@kamago.net>
2016-02-06 23:56:27 +0100

    Build the caches as early as possible (before change replay at least)

--- resources/dav/resource/davgroupwareresource.cpp
+++ resources/dav/resource/davgroupwareresource.cpp
@@ -114,7 +114,8 @@ DavGroupwareResource::DavGroupwareResource(const QString &id)
 
     connect(this, &DavGroupwareResource::reloadConfiguration, this, &DavGroupwareResource::onReloadConfig);
 
-    scheduleCustomTask(this, "createInitialCache", QVariant(), ResourceBase::AfterChangeReplay);
+    scheduleCustomTask(this, "retrieveCollections", QVariant(), ResourceBase::Prepend);
+    scheduleCustomTask(this, "createInitialCache", QVariant(), ResourceBase::Prepend);
 }
 
 DavGroupwareResource::~DavGroupwareResource()
