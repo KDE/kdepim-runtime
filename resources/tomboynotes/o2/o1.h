3e2d10cf001d9552540a221e7b4cc60c6f0ac00f
Montel Laurent <montel@kde.org>
2016-08-04 20:46:49 +0200

    When we cancel create resource don't keep it in system

--- resources/tomboynotes/o2/o1.h
+++ resources/tomboynotes/o2/o1.h
@@ -48,7 +48,7 @@ public:
     void setAccessTokenUrl(const QUrl &value);
 
     /// Constructor.
-    explicit O1(QObject *parent = 0);
+    explicit O1(QObject *parent = Q_NULLPTR);
 
     /// Parse a URL-encoded response string.
     static QMap<QString, QString> parseResponse(const QByteArray &response);
