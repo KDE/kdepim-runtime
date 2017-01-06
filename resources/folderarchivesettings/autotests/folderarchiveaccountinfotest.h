2b34ae00330e88879d1b1792c59c867a7b013356
Montel Laurent <montel@kde.org>
2017-01-06 08:47:13 +0100

    GIT_SILENT: fix copyright

--- resources/folderarchivesettings/autotests/folderarchiveaccountinfotest.h
+++ resources/folderarchivesettings/autotests/folderarchiveaccountinfotest.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2014-2016 Laurent Montel <montel@kde.org>
+   Copyright (C) 2014-2017 Laurent Montel <montel@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
