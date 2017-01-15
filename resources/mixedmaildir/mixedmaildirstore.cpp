KDE/kdepim-runtime

commit e8a9d532884dc8af703bce75a437e44bc9af24c3

Port local mail resources to new retrieveItems() API
The Maildir, MBox and MixedMaildir resources are most affected by
the poor performance of ResourceBase::retrieveItem(). This change
ports those resources to the new retrieveItems() API which provides
batch retrieval of missing payload parts.

resources/mixedmaildir/mixedmaildirstore.cpp
@@ -1660,7 +1660,10 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
                                 scope.payloadParts().contains(MessagePart::Envelope);
 
     const bool fetchSingleItem = job->collection().remoteId().isEmpty();
-    const Collection collection = fetchSingleItem ? job->item().parentCollection() : job->collection();
+    const bool fetchItemsBatch = !job->items().isEmpty() && !job->item().isValid();
+    const Collection collection = fetchSingleItem ? job->item().parentCollection() :
+                                  fetchItemsBatch ? job->items().at(0).parentCollection() :
+                                  job->collection();
 
     QString path;
     QString errorText;
@@ -1675,7 +1678,7 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
 
     if (folderType == MBoxFolder) {
         MBoxHash::iterator findIt = mMBoxes.find(path);
-        if (findIt == mMBoxes.end() || !fetchSingleItem) {
+        if (findIt == mMBoxes.end() || (!fetchSingleItem || !fetchItemsBatch)) {
             MBoxPtr mbox = findIt != mMBoxes.end() ? findIt.value() : MBoxPtr(new MBoxContext);
             if (!mbox->load(path)) {
                 errorText = i18nc("@info:status", "Failed to load MBox folder %1", path);
@@ -1695,6 +1698,8 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
         Item::List items;
         if (fetchSingleItem) {
             items << job->item();
+        } else if (fetchItemsBatch) {
+            items = job->items();
         } else {
             listCollection(job, findIt.value(), collection, items);
         }
@@ -1734,6 +1739,8 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
         Item::List items;
         if (fetchSingleItem) {
             items << job->item();
+        } else if (fetchItemsBatch) {
+            items = job->items();
         } else {
             listCollection(job, mdPtr, collection, items);
         }

resources/shared/filestore/itemfetchjob.cpp
@@ -52,6 +52,15 @@ FileStore::ItemFetchJob::ItemFetchJob(const Item &item, FileStore::AbstractJobSe
     session->addJob(this);
 }
 
+FileStore::ItemFetchJob::ItemFetchJob(const Item::List &items, FileStore::AbstractJobSession *session)
+    : FileStore::Job(session), d(new Private())
+{
+    d->mItems = items;
+
+    session->addJob(this);
+}
+
+
 FileStore::ItemFetchJob::~ItemFetchJob()
 {
     delete d;

resources/shared/filestore/itemfetchjob.h
@@ -46,6 +46,8 @@ public:
 
     explicit ItemFetchJob(const Item &item, AbstractJobSession *session = Q_NULLPTR);
 
+    explicit ItemFetchJob(const Item::List &items, AbstractJobSession *session = Q_NULLPTR);
+
     virtual ~ItemFetchJob();
 
     Collection collection() const;

CMakeLists.txt
@@ -57,7 +57,7 @@ set(QT_REQUIRED_VERSION "5.5.0")
 
 set(KDEPIMRUNTIME_LIB_VERSION "${KDEPIM_RUNTIME_VERSION_NUMBER}")
 set(KDEPIMRUNTIME_LIB_SOVERSION "5")
-set(AKONADI_VERSION "5.3.40")
+set(AKONADI_VERSION "5.3.42")
 
 set(KCONTACTS_LIB_VERSION "5.3.40")
 set(KCALENDARCORE_LIB_VERSION "5.3.40")

resources/maildir/maildirresource.cpp
@@ -198,26 +198,32 @@ MaildirResource::~ MaildirResource()
     delete mSettings;
 }
 
-bool MaildirResource::retrieveItem(const Akonadi::Item &item, const QSet<QByteArray> &parts)
+bool MaildirResource::retrieveItems(const Akonadi::Item::List &items, const QSet<QByteArray> &parts)
 {
     Q_UNUSED(parts);
 
-    const Maildir md = maildirForCollection(item.parentCollection());
+    const Maildir md = maildirForCollection(items.at(0).parentCollection());
     if (!md.isValid()) {
         cancelTask(i18n("Unable to fetch item: The maildir folder \"%1\" is not valid.",
                         md.path()));
         return false;
     }
 
-    const QByteArray data = md.readEntry(item.remoteId());
-    KMime::Message *mail = new KMime::Message();
-    mail->setContent(KMime::CRLFtoLF(data));
-    mail->parse();
+    Akonadi::Item::List rv;
+    rv.reserve(items.count());
+    for (const Akonadi::Item &item : items) {
+        const QByteArray data = md.readEntry(item.remoteId());
+        KMime::Message *mail = new KMime::Message();
+        mail->setContent(KMime::CRLFtoLF(data));
+        mail->parse();
 
-    Item i(item);
-    i.setPayload(KMime::Message::Ptr(mail));
-    Akonadi::MessageFlags::copyMessageFlags(*mail, i);
-    itemRetrieved(i);
+        Item i(item);
+        i.setPayload(KMime::Message::Ptr(mail));
+        Akonadi::MessageFlags::copyMessageFlags(*mail, i);
+        rv.push_back(i);
+    }
+
+    itemsRetrieved(rv);
     return true;
 }
 

resources/maildir/maildirresource.h
@@ -50,7 +50,7 @@ public Q_SLOTS:
 protected Q_SLOTS:
     void retrieveCollections() Q_DECL_OVERRIDE;
     void retrieveItems(const Akonadi::Collection &col) Q_DECL_OVERRIDE;
-    bool retrieveItem(const Akonadi::Item &item, const QSet<QByteArray> &parts) Q_DECL_OVERRIDE;
+    bool retrieveItems(const Akonadi::Item::List &items, const QSet<QByteArray> &parts) Q_DECL_OVERRIDE;
 
 protected:
     virtual QString itemMimeType() const;

resources/mbox/mboxresource.cpp
@@ -149,7 +149,7 @@ void MboxResource::retrieveItems(const Akonadi::Collection &col)
     itemsRetrieved(items);
 }
 
-bool MboxResource::retrieveItem(const Akonadi::Item &item, const QSet<QByteArray> &parts)
+bool MboxResource::retrieveItems(const Akonadi::Item::List &items, const QSet<QByteArray> &parts)
 {
     Q_UNUSED(parts);
 
@@ -162,18 +162,23 @@ bool MboxResource::retrieveItem(const Akonadi::Item &item, const QSet<QByteArray
         return false;
     }
 
-    const QString rid = item.remoteId();
-    const quint64 offset = itemOffset(rid);
-    KMime::Message *mail = mMBox->readMessage(KMBox::MBoxEntry(offset));
-    if (!mail) {
-        Q_EMIT error(i18n("Failed to read message with uid '%1'.", rid));
-        return false;
-    }
+    Akonadi::Item::List rv;
+    rv.reserve(items.count());
+    for (const auto &item : items) {
+        const QString rid = item.remoteId();
+        const quint64 offset = itemOffset(rid);
+        KMime::Message *mail = mMBox->readMessage(KMBox::MBoxEntry(offset));
+        if (!mail) {
+            Q_EMIT error(i18n("Failed to read message with uid '%1'.", rid));
+            return false;
+        }
 
-    Item i(item);
-    i.setPayload(KMime::Message::Ptr(mail));
-    Akonadi::MessageFlags::copyMessageFlags(*mail, i);
-    itemRetrieved(i);
+        Item i(item);
+        i.setPayload(KMime::Message::Ptr(mail));
+        Akonadi::MessageFlags::copyMessageFlags(*mail, i);
+        rv.push_back(i);
+    }
+    itemsRetrieved(rv);
     return true;
 }
 

resources/mbox/mboxresource.h
@@ -37,7 +37,7 @@ public:
     ~MboxResource();
 
 protected Q_SLOTS:
-    bool retrieveItem(const Akonadi::Item &item, const QSet<QByteArray> &parts) Q_DECL_OVERRIDE;
+    bool retrieveItems(const Akonadi::Item::List &items, const QSet<QByteArray> &parts) Q_DECL_OVERRIDE;
     void retrieveItems(const Akonadi::Collection &col) Q_DECL_OVERRIDE;
 
 protected:

commit 3458ff99f88dfefae5b400e35a3e3dd129a32dec

FileStore::ItemFetchJob: fix confusion between requested items and result items
This fixes resources/mixedmaildir/autotests/itemdeletetest.

Regression from e8a9d532884d "Port local mail resources to new retrieveItems() API".

Differential Revision: https://phabricator.kde.org/D4139

resources/mixedmaildir/mixedmaildirstore.cpp
@@ -1659,10 +1659,10 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
     const bool includeHeaders = scope.payloadParts().contains(MessagePart::Header) ||
                                 scope.payloadParts().contains(MessagePart::Envelope);
 
-    const bool fetchSingleItem = job->collection().remoteId().isEmpty();
-    const bool fetchItemsBatch = !job->items().isEmpty() && !job->item().isValid();
-    const Collection collection = fetchSingleItem ? job->item().parentCollection() :
-                                  fetchItemsBatch ? job->items().at(0).parentCollection() :
+    const bool fetchItemsBatch = !job->requestedItems().isEmpty();
+    const bool fetchSingleItem = job->collection().remoteId().isEmpty() && !fetchItemsBatch;
+    const Collection collection = fetchItemsBatch ? job->requestedItems().at(0).parentCollection() :
+                                  fetchSingleItem ? job->item().parentCollection() :
                                   job->collection();
 
     QString path;
@@ -1699,7 +1699,7 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
         if (fetchSingleItem) {
             items << job->item();
         } else if (fetchItemsBatch) {
-            items = job->items();
+            items = job->requestedItems();
         } else {
             listCollection(job, findIt.value(), collection, items);
         }
@@ -1740,7 +1740,7 @@ bool MixedMaildirStore::Private::visit(FileStore::ItemFetchJob *job)
         if (fetchSingleItem) {
             items << job->item();
         } else if (fetchItemsBatch) {
-            items = job->items();
+            items = job->requestedItems();
         } else {
             listCollection(job, mdPtr, collection, items);
         }
@@ -2388,10 +2388,16 @@ void MixedMaildirStore::checkItemFetch(FileStore::ItemFetchJob *job, int &errorC
 {
     Q_UNUSED(errorCode);
     Q_UNUSED(errorText);
-    const bool fetchSingleItem = job->collection().remoteId().isEmpty();
-    if (fetchSingleItem) {
-        Collection coll = job->item().parentCollection();
-        Q_ASSERT(!coll.remoteId().isEmpty());
+    if (!job->requestedItems().isEmpty()) {
+        // Requesting items
+        for (const Item &item : job->requestedItems()) {
+            const Collection coll = item.parentCollection();
+            Q_ASSERT(!coll.remoteId().isEmpty());
+
+        }
+    } else {
+        // Requesting an entire collection
+        Q_ASSERT(!job->collection().remoteId().isEmpty());
     }
 }
 

resources/shared/filestore/itemfetchjob.cpp
@@ -30,7 +30,8 @@ class FileStore::ItemFetchJob::Private
 public:
     ItemFetchScope mFetchScope;
 
-    Item::List mItems;
+    Item::List mRequestedItems;
+    Item::List mResultItems;
 
     Collection mCollection;
     Item mItem;
@@ -55,7 +56,7 @@ FileStore::ItemFetchJob::ItemFetchJob(const Item &item, FileStore::AbstractJobSe
 FileStore::ItemFetchJob::ItemFetchJob(const Item::List &items, FileStore::AbstractJobSession *session)
     : FileStore::Job(session), d(new Private())
 {
-    d->mItems = items;
+    d->mRequestedItems = items;
 
     session->addJob(this);
 }
@@ -85,9 +86,14 @@ ItemFetchScope &FileStore::ItemFetchJob::fetchScope()
     return d->mFetchScope;
 }
 
+Item::List FileStore::ItemFetchJob::requestedItems() const
+{
+    return d->mRequestedItems;
+}
+
 Item::List FileStore::ItemFetchJob::items() const
 {
-    return d->mItems;
+    return d->mResultItems;
 }
 
 bool FileStore::ItemFetchJob::accept(FileStore::Job::Visitor *visitor)
@@ -97,7 +103,7 @@ bool FileStore::ItemFetchJob::accept(FileStore::Job::Visitor *visitor)
 
 void FileStore::ItemFetchJob::handleItemsReceived(const Item::List &items)
 {
-    d->mItems << items;
+    d->mResultItems << items;
 
     Q_EMIT itemsReceived(items);
 }

resources/shared/filestore/itemfetchjob.h
@@ -54,6 +54,8 @@ public:
 
     Item item() const;
 
+    Item::List requestedItems() const;
+
     void setFetchScope(const ItemFetchScope &fetchScope);
 
     ItemFetchScope &fetchScope();