20450ebcdceb53fd51a2a66db070f3dc09fb04f9
Montel Laurent <montel@kde.org>
2017-10-04 07:55:17 +0200

    Update copyright

--- resources/ews/configdialog.cpp
+++ resources/ews/configdialog.cpp
@@ -126,7 +126,7 @@ ConfigDialog::ConfigDialog(EwsResource *parentResource, EwsClient &client, WId w
     QIcon ewsIcon = QIcon::fromTheme(QStringLiteral("akonadi-ews"));
     mUi->aboutIconLabel->setPixmap(ewsIcon.pixmap(96, 96, QIcon::Normal, QIcon::On));
     mUi->aboutTextLabel->setText(i18nc("@info", "Akonadi Resource for Microsoft Exchange Web Services (EWS)"));
-    mUi->aboutCopyrightLabel->setText(i18nc("@info", "Copyright (c) Krzysztof Nowicki 2015-2016"));
+    mUi->aboutCopyrightLabel->setText(i18nc("@info", "Copyright (c) Krzysztof Nowicki 2015-2017"));
     mUi->aboutVersionLabel->setText(i18nc("@info", "Version %1", QString::fromLatin1(AKONADI_EWS_VERSION)));
     mUi->aboutLicenseLabel->setText(i18nc("@info", "Distributed under the GNU Library General Public License version 2.0 or later."));
     mUi->aboutUrlLabel->setText(QStringLiteral("<a href=\"https://github.com/KrissN/akonadi-ews\">https://github.com/KrissN/akonadi-ews</a>"));
