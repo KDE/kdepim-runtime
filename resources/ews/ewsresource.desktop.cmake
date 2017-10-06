3c64334f11704c4f7b2949ebfecb09fa9cf42ee6
l10n daemon script <scripty@kde.org>
2017-10-06 05:05:06 +0200

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- resources/ews/ewsresource.desktop.cmake
+++ resources/ews/ewsresource.desktop.cmake
@@ -1,11 +1,19 @@
 [Desktop Entry]
 Name=Microsoft Exchange Server (EWS)
 Name[ca]=Servidor Microsoft Exchange (EWS)
+Name[ca@valencia]=Servidor Microsoft Exchange (EWS)
 Name[nl]=Microsoft Exchange Server (EWS)
 Name[pt]=Servidor de Microsoft Exchange (EWS)
 Name[sv]=Microsoft Exchange-server (EWS)
+Name[uk]=Сервер Microsoft Exchange (EWS)
 Name[x-test]=xxMicrosoft Exchange Server (EWS)xx
 Comment=Provides access to mail, appointments tasks and contacts of a Microsoft Exchange server using EWS
+Comment[ca]=Proporciona accés al correu, tasques de cita i contactes emmagatzemats en un servidor Microsoft Exchange emprant EWS
+Comment[ca@valencia]=Proporciona accés al correu, tasques de cita i contactes emmagatzemats en un servidor Microsoft Exchange emprant EWS
+Comment[pt]=Oferece o acesso ao correio, compromissos, tarefas e contactos guardados num servidor Microsoft Exchange, usando o EWS
+Comment[sv]=Ger tillgång till e-post, möten, uppgifter och kontakter lagrade på en Microsoft Exchange-server med användning av EWS
+Comment[uk]=Надає доступ до пошти, записів зустрічей, завдань та контактів, які зберігаються на сервері Microsoft Exchange за допомогою EWS
+Comment[x-test]=xxProvides access to mail, appointments tasks and contacts of a Microsoft Exchange server using EWSxx
 Type=AkonadiResource
 Exec=akonadi_ews_resource
 Icon=akonadi-ews
