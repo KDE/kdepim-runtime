738d9f319c8a33417d849540de736ddfac4257f5
Laurent Montel <montel@kde.org>
2018-12-03 20:33:16 +0100

    Comment it

--- resources/dav/resource/searchdialog.cpp
+++ resources/dav/resource/searchdialog.cpp
@@ -97,7 +97,7 @@ QStringList SearchDialog::selection() const
     QStringList ret;
     ret.reserve(indexes.count());
     for (const QModelIndex &index : indexes) {
-        qCritical() << "SELECTED DATA: " << index.data(Qt::UserRole + 1).toString();
+        //qCritical() << "SELECTED DATA: " << index.data(Qt::UserRole + 1).toString();
         ret << index.data(Qt::UserRole + 1).toString();
     }
     return ret;
