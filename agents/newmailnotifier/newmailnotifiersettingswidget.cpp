be477b9e5644ca48cd313e4e6442fd3323a019ce
Laurent Montel <montel@kde.org>
2018-12-27 18:37:11 +0100

    GIT_SILENT: Update copyright

--- agents/newmailnotifier/newmailnotifiersettingswidget.cpp
+++ agents/newmailnotifier/newmailnotifiersettingswidget.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2013-2018 Laurent Montel <montel@kde.org>
+    Copyright (c) 2013-2019 Laurent Montel <montel@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
@@ -143,7 +143,7 @@ NewMailNotifierSettingsWidget::NewMailNotifierSettingsWidget(const KSharedConfig
         QStringLiteral(KDEPIM_RUNTIME_VERSION),
         i18n("Notify about new mails."),
         KAboutLicense::GPL_V2,
-        i18n("Copyright (C) 2013-2018 Laurent Montel"));
+        i18n("Copyright (C) 2013-2019 Laurent Montel"));
 
     aboutData.addAuthor(i18n("Laurent Montel"),
                         i18n("Maintainer"), QStringLiteral("montel@kde.org"));
