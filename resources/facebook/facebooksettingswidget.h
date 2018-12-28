667da0870c1c44bdb8aa8bc60b6bd861c90f2e3b
Laurent Montel <montel@kde.org>
2018-12-28 10:46:27 +0100

    GIT_SILENT: copyright

--- resources/facebook/facebooksettingswidget.h
+++ resources/facebook/facebooksettingswidget.h
@@ -1,6 +1,6 @@
 /*
  *    Copyright (C) 2017 Daniel Vrátil <dvratil@kde.org>
- *    Copyright (C) 2018 Laurent Montel <montel@kde.org>
+ *    Copyright (C) 2018-2019 Laurent Montel <montel@kde.org>
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
