3329edb0cc390589bcec32fdb8e1e4e8a64c57fd
Laurent Montel <montel@kde.org>
2019-06-07 07:53:06 +0200

    Add qfiledialog filter

--- resources/mbox/mboxconfig.cpp
+++ resources/mbox/mboxconfig.cpp
@@ -30,6 +30,7 @@ public:
     MBoxConfigBase(const KSharedConfigPtr &config, QWidget *parent, const QVariantList &args)
         : SingleFileResourceConfigBase<Settings>(config, parent, args)
     {
+        mWidget->setFilter(QStringLiteral("*.mbox|") + i18nc("Filedialog filter for *.mbox", "MBox file"));
         mWidget->addPage(i18n("Compact frequency"), new CompactPage(mSettings->path()));
         mWidget->addPage(i18n("Lock method"), new LockMethodPage());
     }
