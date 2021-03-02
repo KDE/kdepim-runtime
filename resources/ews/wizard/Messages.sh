7c6f4d221bb1573cc6c5f5280e97bf9566fb30fa
Carl Schwan <carl@carlschwan.eu>
2021-03-02 22:23:07 +0100

    Port ews wizard

--- resources/ews/wizard/Messages.sh
+++ resources/ews/wizard/Messages.sh
@@ -1,5 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/accountwizard_ews.pot
-touch $podir/accountwizard_ews.pot
-$XGETTEXT -kqsTr *.es -j -o $podir/accountwizard_ews.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o  $podir/accountwizard_ews.pot
