26af51b67c39a25e045428ffc920b63f046e6898
Carl Schwan <carl@carlschwan.eu>
2021-03-02 22:23:55 +0100

    Minor fixes in imap wizard

--- resources/imap/wizard/Messages.sh
+++ resources/imap/wizard/Messages.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: none
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/accountwizard_imap.pot
-$XGETTEXT -kqsTr *.es -j -o $podir/accountwizard_imap.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o  $podir/accountwizard_imap.pot
