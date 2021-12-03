71fc84196c0073f3b667d038985cbad13a26de16
Laurent Montel <montel@kde.org>
2021-12-03 06:48:24 +0100

    GIT_SILENT: add more license info

--- resources/kalarm/Messages.sh
+++ resources/kalarm/Messages.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name \*.kcfg -o -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find . \( ! -path "./kdepim-runtime/*" \) -a \( -name "*.cpp" -o -name "*.h" \)` -o $podir/akonadi_kalarm_resource.pot
 rm -f rc.cpp
