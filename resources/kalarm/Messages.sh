93ca42832e5e4e1f3196eff04a0bf6363902faec
David Jarvie <djarvie@kde.org>
2022-02-22 12:15:07 +0000

    Remove KAlarm Akonadi resource
    KAlarm no longer uses the Akonadi resource, which is no longer maintained

--- resources/kalarm/Messages.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-# SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: none
-$EXTRACTRC `find . -name \*.kcfg -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . \( ! -path "./kdepim-runtime/*" \) -a \( -name "*.cpp" -o -name "*.h" \)` -o $podir/akonadi_kalarm_resource.pot
-rm -f rc.cpp
