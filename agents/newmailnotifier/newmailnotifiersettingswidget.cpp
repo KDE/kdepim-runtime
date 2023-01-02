0192256601a42d0e6508bee41fab00845fc74615
Laurent Montel <montel@kde.org>
2023-01-02 06:50:42 +0100

    GIT_SILENT: update copyright

--- agents/newmailnotifier/newmailnotifiersettingswidget.cpp
+++ agents/newmailnotifier/newmailnotifiersettingswidget.cpp
@@ -149,7 +149,7 @@ NewMailNotifierSettingsWidget::NewMailNotifierSettingsWidget(const KSharedConfig
                                       QStringLiteral(KDEPIM_RUNTIME_VERSION),
                                       i18n("Notify about new mails."),
                                       KAboutLicense::GPL_V2,
-                                      i18n("Copyright (C) 2013-%1 Laurent Montel", QStringLiteral("2022")));
+                                      i18n("Copyright (C) 2013-%1 Laurent Montel", QStringLiteral("2023")));
 
     aboutData.setProductName(QByteArrayLiteral("Akonadi/New Mail Notifier"));
     aboutData.addAuthor(i18n("Laurent Montel"), i18n("Maintainer"), QStringLiteral("montel@kde.org"));
