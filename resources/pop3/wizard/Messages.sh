ff26c9637065cfca34740c62e7f29559f00c06ef
Laurent Montel <montel@kde.org>
2023-02-08 13:37:40 +0100

    Prepare to create pop3 wizard support

--- resources/pop3/wizard/Messages.sh
+++ resources/pop3/wizard/Messages.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: none
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/accountwizard_pop3.pot
-$XGETTEXT -kqsTr *.es -j -o $podir/accountwizard_pop3.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o  $podir/accountwizard_pop3.pot
