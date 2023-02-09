2fed3d5d62660a24b9229e93752c277a19ea47a1
Laurent Montel <montel@kde.org>
2023-02-09 07:54:06 +0100

    fix typo

--- resources/vcard/wizard/Messages.sh
+++ resources/vcard/wizard/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 # SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
 # SPDX-License-Identifier: CC0-1.0
-$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o  $podir/ccountwizard_vcard.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o  $podir/accountwizard_vcard.pot
