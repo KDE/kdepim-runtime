55b9b9804e7645af521a857fc97ee31c8cd5bc4f
Laurent Montel <montel@kde.org>
2023-06-17 08:22:32 +0200

    Move outside of kservices6 repo

--- resources/dav/resource/setupwizard.cpp
+++ resources/dav/resource/setupwizard.cpp
@@ -228,7 +228,7 @@ int CredentialsPage::nextId() const
     QString userName = field(QStringLiteral("credentialsUserName")).toString();
     if (userName.endsWith(QLatin1String("@yahoo.com"))) {
         const QString maybeYahooFile =
-            QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("kservices6/akonadi/davgroupware-providers/yahoo.desktop"));
+            QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("akonadi/davgroupware-providers/yahoo.desktop"));
 
         if (maybeYahooFile.isEmpty()) {
             return SetupWizard::W_ServerTypePage;
@@ -313,9 +313,8 @@ ServerTypePage::ServerTypePage(QWidget *parent)
     mProvidersCombo = new QComboBox(this);
     mProvidersCombo->setSizeAdjustPolicy(QComboBox::AdjustToContents);
 
-    const QStringList dirs = QStandardPaths::locateAll(QStandardPaths::GenericDataLocation,
-                                                       QStringLiteral("kservices6/akonadi/davgroupware-providers"),
-                                                       QStandardPaths::LocateDirectory);
+    const QStringList dirs =
+        QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, QStringLiteral("akonadi/davgroupware-providers"), QStandardPaths::LocateDirectory);
     const QStringList providers = KFileUtils::findAllUniqueFiles(dirs, QStringList{QStringLiteral("*.desktop")});
 
     QList<QPair<QString, QString>> offers;
