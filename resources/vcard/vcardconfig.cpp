15c0708ef489be287f49133cc4fd1d9dbe0aa4d3
Laurent Montel <montel@kde.org>
2024-08-16 13:33:01 +0200

    Add PimCommonActivities::ConfigureActivitiesWidget in vcardconfig

--- resources/vcard/vcardconfig.cpp
+++ resources/vcard/vcardconfig.cpp
@@ -4,17 +4,31 @@
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
+#include "config-kdepim-runtime.h"
 #include "settings.h"
 #include "singlefileresourceconfigbase.h"
-
+#if HAVE_ACTIVITY_SUPPORT
+#include <PimCommonActivities/ConfigureActivitiesWidget>
+#endif
 class VCardConfigBase : public SingleFileResourceConfigBase<SETTINGS_NAMESPACE::Settings>
 {
 public:
     VCardConfigBase(const KSharedConfigPtr &config, QWidget *parent, const QVariantList &list)
         : SingleFileResourceConfigBase(config, parent, list)
+#if HAVE_ACTIVITY_SUPPORT
+        , mConfigureActivitiesWidget(new PimCommonActivities::ConfigureActivitiesWidget(parent))
+#endif
     {
         mWidget->setFilter(QStringLiteral("%1 (*.vcf)").arg(i18nc("Filedialog filter for *.vcf", "vCard Address Book File")));
+#if HAVE_ACTIVITY_SUPPORT
+        mWidget->addPage(i18n("Activities"), mConfigureActivitiesWidget);
+#endif
     }
+
+private:
+#if HAVE_ACTIVITY_SUPPORT
+    PimCommonActivities::ConfigureActivitiesWidget *const mConfigureActivitiesWidget;
+#endif
 };
 
 class VCardConfig : public VCardConfigBase
