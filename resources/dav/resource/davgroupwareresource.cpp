eb9e3124d320148a3d22417b83e611af5a352518
Allen Winter <winter@kde.org>
2025-06-30 08:19:51 -0400

    Add davical icons to the setup wizard

--- resources/dav/resource/davgroupwareresource.cpp
+++ resources/dav/resource/davgroupwareresource.cpp
@@ -759,6 +759,9 @@ QString DavGroupwareResource::iconForDavUrl(const KDAV::DavUrl &davUrl)
         } else if (host.contains("egroupware"_L1)) {
             // egroupware is self-hosted
             icon = u"account-egroupware"_s;
+        } else if (host.contains("davical"_L1)) {
+            // davical is self-hosted
+            icon = u"account-davical"_s;
         } else if (host.contains("kopano"_L1)) {
             // Kopano is self-hosted
             icon = u"account-kopano"_s;
