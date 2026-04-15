fafcfd620066a12608dbe646db71f6372b64aecb
Kevin Ottens <ervin@kde.org>
2026-04-15 10:55:37 +0200

    Revert "Change base value of dav resource identifier to align to convention"
    This reverts commit 29152f7ff1338fb03a87c36c6bb5f9a21e2fb70f.
    
    This is actually creating problems in production, I should have caught
    it in review but this changes the key used. So it breaks existing
    installation, even worse it does so silently as the settings dialog seem
    to operate with the old key. This needs to be redone.

--- resources/dav/resource/davgroupwareresource.cpp
+++ resources/dav/resource/davgroupwareresource.cpp
@@ -104,7 +104,7 @@ DavGroupwareResource::DavGroupwareResource(const QString &id)
     changeRecorder()->itemFetchScope().setFetchTags(true);
 
     settings()->setAccountId(accountId());
-    settings()->setResourceIdentifier(config()->name());
+    settings()->setResourceIdentifier(identifier());
 
     connect(mFreeBusyHandler, &DavFreeBusyHandler::handlesFreeBusy, this, &DavGroupwareResource::onHandlesFreeBusy);
     connect(mFreeBusyHandler, &DavFreeBusyHandler::freeBusyRetrieved, this, &DavGroupwareResource::onFreeBusyRetrieved);
