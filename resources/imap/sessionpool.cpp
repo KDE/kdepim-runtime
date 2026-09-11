d7174f05113beb63e7c9e36d46972fc2b25e9e3f
Laurent Montel <montel@kde.org>
2026-09-11 13:22:41 +0200

    Use auto here it's a qint64 not a int

--- resources/imap/sessionpool.cpp
+++ resources/imap/sessionpool.cpp
@@ -276,7 +276,7 @@ void SessionPool::processPendingRequests()
 {
     if (!m_account) {
         // The connection to the server is lost; no point processing pending requests
-        for (int request : std::as_const(m_pendingRequests)) {
+        for (auto request : std::as_const(m_pendingRequests)) {
             Q_EMIT sessionRequestDone(request, nullptr, LoginFailError, i18n("Disconnected from server during login."));
         }
         return;
@@ -317,7 +317,7 @@ void SessionPool::onPasswordRequestDone(int resultType, const QString &password)
     if (!m_account) {
         // it looks like the connection was lost while we were waiting
         // for the password, we should fail all the pending requests and stop there
-        for (int request : std::as_const(m_pendingRequests)) {
+        for (auto request : std::as_const(m_pendingRequests)) {
             Q_EMIT sessionRequestDone(request, nullptr, LoginFailError, i18n("Disconnected from server during login."));
         }
         return;
